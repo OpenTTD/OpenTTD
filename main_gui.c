5d6c0a82204cc3ba04fe7a340260f59d10da29fa
tron <tron@openttd.org>
2004-11-11 23:23:42 +0000

    (svn r543) -Fix: [ 1064742 ] Editor Map-Menu wrong String The fix is a bit ugly, but this is due to limitations in the dropdown menu handling (the strings have to be contiguous)

--- main_gui.c
+++ main_gui.c
@@ -172,7 +172,8 @@ void MenuClickScenMap(int index)
 {
 	switch(index) {
 	case 0: ShowSmallMap(); break;
-	case 1: ShowTownDirectory(); break;
+	case 1: ShowExtraViewPortWindow(); break;
+	case 2: ShowTownDirectory(); break;
 	}
 }
 
@@ -884,7 +885,7 @@ static void ToolbarScenDateForward(Window *w)
 
 static void ToolbarScenMapTownDir(Window *w)
 {
-	PopupMainToolbMenu(w, 0x16A, 8 | (17<<8), STR_02DE_MAP_OF_WORLD, 2);
+	PopupMainToolbMenu(w, 0x16A, 8 | (17<<8), STR_02DE_MAP_OF_WORLD, 3);
 }
 
 static void ToolbarScenZoomIn(Window *w)
