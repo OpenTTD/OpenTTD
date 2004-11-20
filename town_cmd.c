578bde9aaef23287b43286878ea9c54b873e362c
pasky <pasky@openttd.org>
2004-11-20 02:36:10 +0000

    (svn r698) Merge a town growth fix from TTDPatch - in arctic and desert climate, require food (and possibly water) only if the population is more than 90, so that the town gets chance to actually accept it. (By Marcin?)

--- town_cmd.c
+++ town_cmd.c
@@ -1612,10 +1612,10 @@ static void UpdateTownGrowRate(Town *t)
 	}
 
 	if (_opt.landscape == LT_HILLY) {
-		if (GET_TILEHEIGHT(t->xy) >= _opt.snow_line &&	t->act_food == 0)
+ 		if (GET_TILEHEIGHT(t->xy) >= _opt.snow_line && t->act_food == 0 && t->population > 90)
 			return;
 	} else if (_opt.landscape == LT_DESERT) {
-		if (GetMapExtraBits(t->xy) == 1 && (t->act_food==0 || t->act_water==0))
+ 		if (GetMapExtraBits(t->xy) == 1 && (t->act_food==0 || t->act_water==0) && t->population > 60)
 			return;
 	}
 
