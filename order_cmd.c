453c85e61de28d36b903d8632f07e1dca7c65f07
dominik <dominik@openttd.org>
2004-12-18 21:11:45 +0000

    (svn r1163) Fix: [ 1087403 ] Crashed trains aren't reported to have too few orders any more

--- order_cmd.c
+++ order_cmd.c
@@ -375,7 +375,7 @@ int CheckOrders(Vehicle *v)
 	if (!_patches.order_review_system)	//User doesn't want things to be checked
 		return 0;
 
-	if ( (_patches.order_review_system == 1) && (v->vehstatus & VS_STOPPED) )
+	if ( (_patches.order_review_system == 1) && ((v->vehstatus & VS_STOPPED) || (v->vehstatus & VS_CRASHED)) )
 		return 0;
 
 	/* only check every 20 days, so that we don't flood the message log */
