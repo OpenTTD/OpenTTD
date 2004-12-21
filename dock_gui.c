8bf52f8c0f6c99d43f666ba5331048a4e3d3d166
dominik <dominik@openttd.org>
2004-12-21 18:30:51 +0000

    (svn r1206) Fix: Polished build docks window a bit.

--- dock_gui.c
+++ dock_gui.c
@@ -213,7 +213,8 @@ static void BuildDockStationWndProc(Window *w, WindowEvent *e)
 		if (_station_show_coverage)	SetTileSelectBigSize(-rad, -rad, 2 * rad, 2 * rad);
 		else SetTileSelectBigSize(0, 0, 0, 0);
 
-		DrawStationCoverageAreaText(2, 15, (uint)-1, rad);
+		DrawStringCentered(74, 17, STR_3066_COVERAGE_AREA_HIGHLIGHT, 0);
+		DrawStationCoverageAreaText(4, 50, (uint)-1, rad);
 	} break;
 
 	case WE_CLICK: {
@@ -244,14 +245,14 @@ static void BuildDockStationWndProc(Window *w, WindowEvent *e)
 static const Widget _build_dock_station_widgets[] = {
 {   WWT_CLOSEBOX,     7,     0,    10,     0,    13, STR_00C5,			STR_018B_CLOSE_WINDOW},
 {    WWT_CAPTION,     7,    11,   147,     0,    13, STR_3068_DOCK,	STR_018C_WINDOW_TITLE_DRAG_THIS},
-{      WWT_PANEL,     7,     0,   147,    14,    55, 0x0,						STR_NULL},
-{   WWT_CLOSEBOX,    14,    14,    73,    40,    50, STR_02DB_OFF,	STR_3065_DON_T_HIGHLIGHT_COVERAGE},
-{   WWT_CLOSEBOX,    14,    74,   133,    40,    50, STR_02DA_ON,		STR_3064_HIGHLIGHT_COVERAGE_AREA},
+{      WWT_PANEL,     7,     0,   147,    14,    74, 0x0,						STR_NULL},
+{   WWT_CLOSEBOX,    14,    14,    73,    30,    40, STR_02DB_OFF,	STR_3065_DON_T_HIGHLIGHT_COVERAGE},
+{   WWT_CLOSEBOX,    14,    74,   133,    30,    40, STR_02DA_ON,		STR_3064_HIGHLIGHT_COVERAGE_AREA},
 {   WIDGETS_END},
 };
 
 static const WindowDesc _build_dock_station_desc = {
-	-1, -1, 148, 56,
+	-1, -1, 148, 75,
 	WC_BUILD_STATION,WC_BUILD_TOOLBAR,
 	WDF_STD_TOOLTIPS | WDF_STD_BTN | WDF_DEF_WIDGET,
 	_build_dock_station_widgets,
