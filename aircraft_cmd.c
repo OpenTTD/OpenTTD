OpenTTD/OpenTTD

commit 3cb8f8efdd889b8bc26f3db9b35b3a8fbd764a11

- Fix: Ships and Aircrafts have to be build in depots owned by _current_player (hacked client protection) now all 4 types of vehicles have to be build in depots. Note: there is a wordaround for AI players since the AI handle aircraft building in a weird way
git-svn-id: http://svn.openttd.org/trunk@1730 6aa0318a-3be1-0310-93fa-89fd2396df07

aircraft_cmd.c
@@ -163,6 +163,11 @@ int32 CmdBuildAircraft(int x, int y, uint32 flags, uint32 p1, uint32 p2)
 
 	if (!IsEngineBuildable(p1, VEH_Aircraft)) return CMD_ERROR;
 
+	// Workaround: TODO: make AI players try to build planes in a hangar instead of just an airport tile.
+	if (!IsAircraftHangarTile((TileIndex)tile) && IS_HUMAN_PLAYER(_current_player)) return CMD_ERROR;
+
+	if (_map_owner[tile] != _current_player) return CMD_ERROR;
+
 	SET_EXPENSES_TYPE(EXPENSES_NEW_VEHICLES);
 
 	value = EstimateAircraftCost(p1);

ship_cmd.c
@@ -841,6 +841,10 @@ int32 CmdBuildShip(int x, int y, uint32 flags, uint32 p1, uint32 p2)
 
 	if (!IsEngineBuildable(p1, VEH_Ship)) return CMD_ERROR;
 
+	if (!IsShipDepotTile((TileIndex)tile)) return CMD_ERROR;
+
+	if (_map_owner[tile] != _current_player) return CMD_ERROR;
+
 	SET_EXPENSES_TYPE(EXPENSES_NEW_VEHICLES);
 
 	value = EstimateShipCost(p1);

commit 1c109afdaafe6d2ce6fe8fd34678d7f9a50d5fcd

(svn r1742) - Fix: fixed bug introduced in r1730 where AI players had problems when building aircrafts

aircraft_cmd.c
@@ -166,7 +166,7 @@ int32 CmdBuildAircraft(int x, int y, uint32 flags, uint32 p1, uint32 p2)
 	// Workaround: TODO: make AI players try to build planes in a hangar instead of just an airport tile.
 	if (!IsAircraftHangarTile((TileIndex)tile) && IS_HUMAN_PLAYER(_current_player)) return CMD_ERROR;
 
-	if (_map_owner[tile] != _current_player) return CMD_ERROR;
+	if (_map_owner[tile] != _current_player && IS_HUMAN_PLAYER(_current_player)) return CMD_ERROR;
 
 	SET_EXPENSES_TYPE(EXPENSES_NEW_VEHICLES);
 