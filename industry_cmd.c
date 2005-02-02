74df79331dfb23fc73426ac52ef5129586f0150b
truelight <truelight@openttd.org>
2005-02-02 19:08:34 +0000

    (svn r1773) -Fix: [ 1114950 ] Game crashed sometimes when there were no industries in the map

--- industry_cmd.c
+++ industry_cmd.c
@@ -1860,7 +1860,7 @@ void IndustryMonthlyLoop(void)
 	/* 3% chance that we start a new industry */
 	if (CHANCE16(3, 100)) {
 		MaybeNewIndustry(Random());
-	} else if (!_patches.smooth_economy) {
+	} else if (!_patches.smooth_economy && _total_industries > 0) {
 		i = GetIndustry(RandomRange(_total_industries));
 		MaybeCloseIndustry(i);
 	}
