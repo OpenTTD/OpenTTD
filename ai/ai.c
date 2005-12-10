be65f908c8ffb2cf1e9787f302acd4fb6dfafd8d
truelight <truelight@openttd.org>
2005-12-10 18:50:07 +0000

    (svn r3285) -Codechange: Tron was confused by a name, so let's make him happy, prefixed it with 'tmp', so now we know it is a temp char* :)

--- ai/ai.c
+++ ai/ai.c
@@ -99,7 +99,7 @@ int32 AI_DoCommand(uint tile, uint32 p1, uint32 p2, uint32 flags, uint procc)
 {
 	PlayerID old_lp;
 	int32 res = 0;
-	char *cmdtext = NULL;
+	char *tmp_cmdtext = NULL;
 
 	/* If you enable DC_EXEC with DC_QUERY_COST you are a really strange
 	 *   person.. should we check for those funny jokes?
@@ -107,20 +107,20 @@ int32 AI_DoCommand(uint tile, uint32 p1, uint32 p2, uint32 flags, uint procc)
 
 	/* The test already free _cmd_text in most cases, so let's backup the string, else we have a problem ;) */
 	if (_cmd_text != NULL)
-		cmdtext = strdup(_cmd_text);
+		tmp_cmdtext = strdup(_cmd_text);
 
 	/* First, do a test-run to see if we can do this */
 	res = DoCommandByTile(tile, p1, p2, flags & ~DC_EXEC, procc);
 	/* The command failed, or you didn't want to execute, or you are quering, return */
 	if ((CmdFailed(res)) || !(flags & DC_EXEC) || (flags & DC_QUERY_COST)) {
-		if (cmdtext != NULL)
-			free(cmdtext);
+		if (tmp_cmdtext != NULL)
+			free(tmp_cmdtext);
 		return res;
 	}
 
 	/* Recover _cmd_text */
-	if (cmdtext != NULL)
-		_cmd_text = cmdtext;
+	if (tmp_cmdtext != NULL)
+		_cmd_text = tmp_cmdtext;
 
 	/* If we did a DC_EXEC, and the command did not return an error, execute it
 	    over the network */
@@ -147,8 +147,8 @@ int32 AI_DoCommand(uint tile, uint32 p1, uint32 p2, uint32 flags, uint procc)
 	_local_player = old_lp;
 
 	/* Free the temp _cmd_text var */
-	if (cmdtext != NULL)
-		free(cmdtext);
+	if (tmp_cmdtext != NULL)
+		free(tmp_cmdtext);
 
 	return res;
 }
