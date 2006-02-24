494fd4ed3418a11a61c652a1d57573e1e6c22b14
tron <tron@openttd.org>
2006-02-24 20:52:32 +0000

    (svn r3669) Really wipe all data when making a void tile
    Thanks to Belugas, egladil and Rubidium for their inspiration and hard work, which all this stuff is based on

--- void.h
+++ void.h
@@ -3,4 +3,11 @@
 static inline void MakeVoid(TileIndex t)
 {
 	SetTileType(t, MP_VOID);
+	SetTileHeight(t, 0);
+	_m[t].m1 = 0;
+	_m[t].m2 = 0;
+	_m[t].m3 = 0;
+	_m[t].m4 = 0;
+	_m[t].m5 = 0;
+	_m[t].extra = 0;
 }
