OpenTTD/OpenTTD

commit c1d5ee853484e79377019264a0a38b0f066f66b6

- Fix: [Multistop] Check the status of the destination road stop instead of a station's first road stop. This only has effect with road vehicle queuing disabled.
git-svn-id: http://svn.openttd.org/trunk@3663 6aa0318a-3be1-0310-93fa-89fd2396df07

roadveh_cmd.c
@@ -983,17 +983,12 @@ static int RoadFindPathToDest(Vehicle *v, TileIndex tile, int enterdir)
 			/* Road depot */
 			bitmask |= _road_veh_fp_ax_or[GB(_m[tile].m5, 0, 2)];
 		}
-	} else if (IsTileType(tile, MP_STATION)) {
-		if (IsTileOwner(tile, OWNER_NONE) || IsTileOwner(tile, v->owner)) {
+	} else if (IsTileType(tile, MP_STATION) && IsRoadStationTile(tile)) {
+		if (IsTileOwner(tile, v->owner)) {
 			/* Our station */
-			const Station* st = GetStation(_m[tile].m2);
-			byte val = _m[tile].m5;
-			if (v->cargo_type != CT_PASSENGERS) {
-				if (IS_BYTE_INSIDE(val, 0x43, 0x47) && (_patches.roadveh_queue || st->truck_stops->status&3))
-					bitmask |= _road_veh_fp_ax_or[(val-0x43)&3];
-			} else {
-				if (IS_BYTE_INSIDE(val, 0x47, 0x4B) && (_patches.roadveh_queue || st->bus_stops->status&3))
-					bitmask |= _road_veh_fp_ax_or[(val-0x47)&3];
+			const RoadStop *rs = GetRoadStopByTile(tile, (v->cargo_type == CT_PASSENGERS) ? RS_BUS : RS_TRUCK);
+			if (rs != NULL && (_patches.roadveh_queue || GB(rs->status, 0, 2) != 0)) {
+				bitmask |= _road_veh_fp_ax_or[GetRoadStationDir(tile)];
 			}
 		}
 	}

commit 23b13d3f421d5013e2a3219ba6fa0fae9b5337d6

(svn r3681) - [Multistop] Check the RoadStop type before check its status. This fixes an assert introduced in r3663. Also fix the return type of GetRoadStopType().

roadveh_cmd.c
@@ -986,9 +986,12 @@ static int RoadFindPathToDest(Vehicle *v, TileIndex tile, int enterdir)
 	} else if (IsTileType(tile, MP_STATION) && IsRoadStationTile(tile)) {
 		if (IsTileOwner(tile, v->owner)) {
 			/* Our station */
-			const RoadStop *rs = GetRoadStopByTile(tile, (v->cargo_type == CT_PASSENGERS) ? RS_BUS : RS_TRUCK);
-			if (rs != NULL && (_patches.roadveh_queue || GB(rs->status, 0, 2) != 0)) {
-				bitmask |= _road_veh_fp_ax_or[GetRoadStationDir(tile)];
+			RoadStopType rstype = (v->cargo_type == CT_PASSENGERS) ? RS_BUS : RS_TRUCK;
+			if (GetRoadStopType(tile) == rstype) {
+				const RoadStop *rs = GetRoadStopByTile(tile, rstype);
+				if (rs != NULL && (_patches.roadveh_queue || GB(rs->status, 0, 2) != 0)) {
+					bitmask |= _road_veh_fp_ax_or[GetRoadStationDir(tile)];
+				}
 			}
 		}
 	}

station.h
@@ -196,7 +196,7 @@ uint GetStationPlatforms(const Station *st, TileIndex tile);
 void StationPickerDrawSprite(int x, int y, RailType railtype, int image);
 
 RoadStop * GetRoadStopByTile(TileIndex tile, RoadStopType type);
-static inline int GetRoadStopType(TileIndex tile)
+static inline RoadStopType GetRoadStopType(TileIndex tile)
 {
 	return (_m[tile].m5 < 0x47) ? RS_TRUCK : RS_BUS;
 }