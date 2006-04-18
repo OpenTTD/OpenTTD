35a21d80fc57791749a40bec9e7a66987421398e
celestar <celestar@openttd.org>
2006-04-18 13:55:11 +0000

    (svn r4468) -Fix: (FS#45) correct declaration of NORETURN for gcc (egladil)

--- stdafx.h
+++ stdafx.h
@@ -68,7 +68,7 @@
 
 // Stuff for GCC
 #if defined(__GNUC__)
-# define NORETURN
+# define NORETURN              __attribute((noreturn))
 # define FORCEINLINE inline
 # define CDECL
 //#include <alloca.h>
