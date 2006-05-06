0597c35fc142f4820e5900eb2e2f28422f351f62
peter1138 <peter1138@openttd.org>
2006-05-06 22:10:38 +0000

    (svn r4759) - Newstations: increase the number of station classes from 16 to 32. 16 was an arbitrary limit, 32 is the limit for dropdown lists...

--- newgrf_station.h
+++ newgrf_station.h
@@ -10,7 +10,7 @@
 typedef enum {
 	STAT_CLASS_DFLT,     ///< Default station class.
 	STAT_CLASS_WAYP,     ///< Waypoint class.
-	STAT_CLASS_MAX = 16, ///< Maximum number of classes.
+	STAT_CLASS_MAX = 32, ///< Maximum number of classes.
 } StationClassID;
 
 /* Station layout for given dimensions - it is a two-dimensional array
