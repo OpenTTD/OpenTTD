6c572f1947a68a9ecd4a822314e901c74cd4a963
glx <glx@openttd.org>
2006-05-16 16:30:40 +0000

    (svn r4889) - Codechange: added vehicle count in 'players' console command

--- console_cmds.c
+++ console_cmds.c
@@ -1148,7 +1148,12 @@ DEF_CONSOLE_CMD(ConPlayers)
 		if (!p->is_active) continue;
 
 		GetString(buffer, STR_00D1_DARK_BLUE + _player_colors[p->index]);
-		IConsolePrintF(8, "#:%d(%s) Company Name: '%s'  Year Founded: %d  Money: %d  Loan: %d  Value: %" OTTD_PRINTF64 "d", p->index + 1, buffer, _network_player_info[p->index].company_name, p->inaugurated_year + MAX_YEAR_BEGIN_REAL, p->player_money, p->current_loan, CalculateCompanyValue(p));
+		IConsolePrintF(8, "#:%d(%s) Company Name: '%s'  Year Founded: %d  Money: %d  Loan: %d  Value: %" OTTD_PRINTF64 "d  (T:%d, R:%d, P:%d, S:%d)",
+			p->index + 1, buffer, _network_player_info[p->index].company_name, p->inaugurated_year + MAX_YEAR_BEGIN_REAL, p->player_money, p->current_loan, CalculateCompanyValue(p),
+			/* trains      */ _network_player_info[p->index].num_vehicle[0],
+			/* lorry + bus */ _network_player_info[p->index].num_vehicle[1] + _network_player_info[p->index].num_vehicle[2],
+			/* planes      */ _network_player_info[p->index].num_vehicle[3],
+			/* ships       */ _network_player_info[p->index].num_vehicle[4]);
 	}
 
 	return true;
