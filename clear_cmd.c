94f1f758258e00d15550c8944009c8a4ed9226ec
truelight <truelight@openttd.org>
2006-08-21 09:22:44 +0000

    (svn r6032) -Fix: when a farm is gone, remove the fields when it is full grown and should be harvest (not before that)

--- clear_cmd.c
+++ clear_cmd.c
@@ -680,7 +680,7 @@ static void TileLoop_Clear(TileIndex tile)
 				SetClearCounter(tile, 0);
 			}
 
-			if (GetIndustryIndexOfField(tile) == INVALID_INDUSTRY) {
+			if (GetIndustryIndexOfField(tile) == INVALID_INDUSTRY && GetFieldType(tile) >= 7) {
 				/* This farmfield is no longer farmfield, so make it grass again */
 				MakeClear(tile, CLEAR_GRASS, 0);
 			} else {
