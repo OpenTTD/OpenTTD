de610df608a5e87f1bcde3c0e7e660457a44296f
Darkvater <darkvater@openttd.org>
2006-09-03 22:27:13 +0000

    (svn r6367) -Codechange: Speed up the animated cursors a bit so they move once in a while  at least.

--- table/animcursors.h
+++ table/animcursors.h
@@ -24,45 +24,45 @@
 /** Animated cursor elements for demolishion
  */
 static const CursorID _demolish_animcursor[] = {
-	ANIM_CURSOR_LINE(0x2C0, 29)
-	ANIM_CURSOR_LINE(0x2C1, 29)
-	ANIM_CURSOR_LINE(0x2C2, 29)
-	ANIM_CURSOR_LINE(0x2C3, 29)
+	ANIM_CURSOR_LINE(0x2C0, 8)
+	ANIM_CURSOR_LINE(0x2C1, 8)
+	ANIM_CURSOR_LINE(0x2C2, 8)
+	ANIM_CURSOR_LINE(0x2C3, 8)
 	ANIM_CURSOR_END()
 };
 
 /** Animated cursor elements for lower land
  */
 static const CursorID _lower_land_animcursor[] = {
-	ANIM_CURSOR_LINE(0x2BB, 29)
-	ANIM_CURSOR_LINE(0x2BC, 29)
-	ANIM_CURSOR_LINE(0x2BD, 98)
+	ANIM_CURSOR_LINE(0x2BB, 10)
+	ANIM_CURSOR_LINE(0x2BC, 10)
+	ANIM_CURSOR_LINE(0x2BD, 29)
 	ANIM_CURSOR_END()
 };
 
 /** Animated cursor elements for raise land
  */
 static const CursorID _raise_land_animcursor[] = {
-	ANIM_CURSOR_LINE(0x2B8, 29)
-	ANIM_CURSOR_LINE(0x2B9, 29)
-	ANIM_CURSOR_LINE(0x2BA, 98)
+	ANIM_CURSOR_LINE(0x2B8, 10)
+	ANIM_CURSOR_LINE(0x2B9, 10)
+	ANIM_CURSOR_LINE(0x2BA, 29)
 	ANIM_CURSOR_END()
 };
 
 /** Animated cursor elements for the goto icon
  */
-static const CursorID _pick_station_animcursor[] = {
-	ANIM_CURSOR_LINE(0x2CC, 29)
-	ANIM_CURSOR_LINE(0x2CD, 29)
-	ANIM_CURSOR_LINE(0x2CE, 98)
+static const CursorID _order_goto_animcursor[] = {
+	ANIM_CURSOR_LINE(0x2CC, 10)
+	ANIM_CURSOR_LINE(0x2CD, 10)
+	ANIM_CURSOR_LINE(0x2CE, 29)
 	ANIM_CURSOR_END()
 };
 
 /** Animated cursor elements for the build signal icon
  */
 static const CursorID _build_signals_animcursor[] = {
-	ANIM_CURSOR_LINE(0x50C, 29)
-	ANIM_CURSOR_LINE(0x50D, 29)
+	ANIM_CURSOR_LINE(0x50C, 20)
+	ANIM_CURSOR_LINE(0x50D, 20)
 	ANIM_CURSOR_END()
 };
 
@@ -74,6 +74,6 @@ static const CursorID * const _animcursors[] = {
 	_demolish_animcursor,
 	_lower_land_animcursor,
 	_raise_land_animcursor,
-	_pick_station_animcursor,
+	_order_goto_animcursor,
 	_build_signals_animcursor
 };
