c5ea9b0824a07f81c697cb5d8852199c4107b844
KUDr <kudr@openttd.org>
2006-11-21 16:24:13 +0000

    (svn r7227) -Fix: [YAPF] Bridge YAPF Penalty Incorrect. The penalty for upward slope was incorrectly applied on bridge exit. (Danny)

--- yapf/yapf_costbase.hpp
+++ yapf/yapf_costbase.hpp
@@ -8,10 +8,21 @@ struct CYapfCostBase {
 
 	FORCEINLINE static bool stSlopeCost(TileIndex tile, Trackdir td)
 	{
-		if (IsDiagonalTrackdir(td) && !IsTunnelTile(tile)) {
-			uint tile_slope = GetTileSlope(tile, NULL) & 0x0F;
-			if ((c_upwards_slopes[tile_slope] & TrackdirToTrackdirBits(td)) != 0) {
+		if (IsDiagonalTrackdir(td)) {
+			if (IsBridgeTile(tile) && IsBridgeRamp(tile)) {
+				// it is bridge ramp, check if we are entering the bridge
+				if (GetBridgeRampDirection(tile) != TrackdirToExitdir(td)) return false; // no, we are living it, no penalty
+				// we are entering the bridge
+				// if the tile slope is downwards, then bridge ramp has not upward slope
+				uint tile_slope = GetTileSlope(tile, NULL) & 0x0F;
+				if ((c_upwards_slopes[tile_slope] & TrackdirToTrackdirBits(ReverseTrackdir(td))) != 0) return false; // tile under ramp goes down, no penalty
+				// tile under ramp isn't going down, so ramp must go up
 				return true;
+			} else {
+				// not bridge ramp
+				if (IsTunnelTile(tile)) return false; // tunnel entry/exit doesn't slope
+				uint tile_slope = GetTileSlope(tile, NULL) & 0x0F;
+				if ((c_upwards_slopes[tile_slope] & TrackdirToTrackdirBits(td)) != 0) return true; // slopes uphill => apply penalty
 			}
 		}
 		return false;
