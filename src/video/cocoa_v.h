19bdb49e5bc34ab4434f3dc5a28ac7666f19cc33
bjarni <bjarni@openttd.org>
2007-01-15 00:58:18 +0000

    (svn r8141) -Fix r8128: [OSX] now OSX can compile again.   Note: this is again another not so nice workaround as Objective C fails to include C++ code in the headers     We should move to Objective C++ as soon as possible, but such a change is not done overnight and     this quick fix will allow OSX to compile until the prober solution is done

--- src/video/cocoa_v.h
+++ src/video/cocoa_v.h
@@ -4,6 +4,13 @@
 #define VIDEO_COCOA_H
 
 #include "../hal.h"
+
+#ifndef __cplusplus
+/* Really ugly workaround
+ * It should be solved right as soon as possible */
+typedef uint32 SpriteID;
+#endif //__cplusplus
+
 #include "../gfx.h"
 
 #ifdef __cplusplus
