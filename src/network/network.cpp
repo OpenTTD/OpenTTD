c1b898392ea3aa03c89d1222ad9558eb02ac25b6
glx <glx@openttd.org>
2007-04-25 17:40:06 +0000

    (svn r9716) -Add: server_lang in [network] section of openttd.cfg, so dedicated servers can have the little lang flag.

--- src/network/network.cpp
+++ src/network/network.cpp
@@ -1395,11 +1395,13 @@ void NetworkStartUp()
 		byte cl_max = _network_game_info.clients_max;
 		byte cp_max = _network_game_info.companies_max;
 		byte sp_max = _network_game_info.spectators_max;
+		byte s_lang = _network_game_info.server_lang;
 
 		memset(&_network_game_info, 0, sizeof(_network_game_info));
 		_network_game_info.clients_max = cl_max;
 		_network_game_info.companies_max = cp_max;
 		_network_game_info.spectators_max = sp_max;
+		_network_game_info.server_lang = s_lang;
 	}
 
 
