196ddae1db750e9e502bf972352f2cee747100aa
rubidium <rubidium@openttd.org>
2007-05-15 13:38:43 +0000

    (svn r9843) -Fix (r9838): tell the newgrfs that we now support proper FIFO loading.

--- src/newgrf.cpp
+++ src/newgrf.cpp
@@ -4213,7 +4213,7 @@ static void InitializeGRFSpecial()
 	                   |                                        (1 << 0x05)  // resolutionwidth
 	                   |                                        (1 << 0x06)  // resolutionheight
 	                   |                                        (0 << 0x07)  // newindustries
-	                   |                                        (0 << 0x08)  // fifoloading
+	                   |         ((_patches.improved_load ? 1 : 0) << 0x08)  // fifoloading
 	                   |                                        (0 << 0x09)  // townroadbranchprob
 	                   |                                        (0 << 0x0A)  // tempsnowline
 	                   |                                        (1 << 0x0B)  // newcargo
