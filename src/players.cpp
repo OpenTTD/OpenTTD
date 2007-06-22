cc6ee1840a4ba53ffe5b5f7975b1ddb93b0e393e
truelight <truelight@openttd.org>
2007-06-22 22:14:42 +0000

    (svn r10284) -Fix [FS#899]: in the unlucky event 2 clients join at the same time to start a new company while there is only one spot left, all clients became spectator. Now only make either one of the joining clients spectator (much more game-friendly ;))

--- src/players.cpp
+++ src/players.cpp
@@ -840,12 +840,10 @@ CommandCost CmdPlayerCtrl(TileIndex tile, uint32 flags, uint32 p1, uint32 p2)
 				NetworkClientInfo *ci = &_network_client_info[cid];
 				ci->client_playas = PLAYER_SPECTATOR;
 				NetworkUpdateClientInfo(ci->client_index);
-			} else
-#endif /* ENABLE_NETWORK */
-			{
+			} else if (_local_player == PLAYER_SPECATOR) {
 				_network_playas = PLAYER_SPECTATOR;
-				SetLocalPlayer(PLAYER_SPECTATOR);
 			}
+#endif /* ENABLE_NETWORK */
 			break;
 		}
 
