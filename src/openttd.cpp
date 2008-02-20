e95a85315ce8aeb40b1dd28cf6f5607032be9a7b
smatz <smatz@openttd.org>
2008-02-20 16:24:56 +0000

    (svn r12195) -Fix [FS#1787](r12100): there can be oil rigs at map borders, do not set water class for them

--- src/openttd.cpp
+++ src/openttd.cpp
@@ -2364,7 +2364,9 @@ bool AfterLoadGame()
 
 	if (CheckSavegameVersion(87)) {
 		for (TileIndex t = 0; t < map_size; t++) {
-			if (!IsTileType(t, MP_VOID) && (TileX(t) == 0 || TileY(t) == 0 || TileX(t) == MapMaxX() - 1 || TileY(t) == MapMaxY() - 1)) {
+			/* skip oil rigs at borders! */
+			if ((IsTileType(t, MP_WATER) || IsBuoyTile(t)) &&
+					(TileX(t) == 0 || TileY(t) == 0 || TileX(t) == MapMaxX() - 1 || TileY(t) == MapMaxY() - 1)) {
 				/* Some version 86 savegames have wrong water class at map borders (under buoy, or after removing buoy).
 				 * This conversion has to be done before buoys with invalid owner are removed. */
 				SetWaterClass(t, WATER_CLASS_SEA);
