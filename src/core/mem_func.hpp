03854bf27ccf8edb0d34a1fbf8964785d6b8fd70
skidd13 <skidd13@openttd.org>
2008-06-14 16:41:03 +0000

    (svn r13517) -Add: remaining used typesafe versions of the mem* functions from <string.h>

--- src/core/mem_func.hpp
+++ src/core/mem_func.hpp
@@ -22,6 +22,46 @@ FORCEINLINE void MemCpyT(T *destination, const T *source, uint num = 1)
 }
 
 /**
+ * Type-safe version of memmove().
+ *
+ * @param destination Pointer to the destination buffer
+ * @param source Pointer to the source buffer
+ * @param num number of items to be copied. (!not number of bytes!)
+ */
+template <typename T>
+FORCEINLINE void MemMoveT(T *destination, const T *source, uint num = 1)
+{
+	memmove(destination, source, num * sizeof(T));
+}
+
+/**
+ * Type-safe version of memset().
+ *
+ * @param ptr Pointer to the destination buffer
+ * @param value Value to be set
+ * @param num number of items to be set (!not number of bytes!)
+ */
+template <typename T>
+FORCEINLINE void MemSetT(T *ptr, int value, uint num = 1)
+{
+	memset(ptr, value, num * sizeof(T));
+}
+
+/**
+ * Type-safe version of memcmp().
+ *
+ * @param ptr1 Pointer to the first buffer
+ * @param ptr2 Pointer to the second buffer
+ * @param num Number of items to compare. (!not number of bytes!)
+ * @return an int value indicating the relationship between the content of the two buffers
+ */
+template <typename T>
+FORCEINLINE int MemCmpT(const T *ptr1, const T *ptr2, uint num = 1)
+{
+	return memcmp(ptr1, ptr2, num * sizeof(T));
+}
+
+/**
  * Type safe memory reverse operation.
  *  Reverse a block of memory in steps given by the
  *  type of the pointers.
