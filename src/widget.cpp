ebe9ae2b1755c619953a591c35613e7315954817
belugas <belugas@openttd.org>
2008-08-11 16:31:36 +0000

    (svn r14040) -Codechange:Remove a hard coded value that is not even representative, since captions have their own encoded colours in string.

--- src/widget.cpp
+++ src/widget.cpp
@@ -479,7 +479,7 @@ void Window::DrawWidgets() const
 				GfxFillRect(r.left + 2, r.top + 2, r.right - 2, r.bottom - 2, _colour_gradient[_player_colors[this->caption_color]][4]);
 			}
 
-			DrawStringCenteredTruncated(r.left + 2, r.right - 2, r.top + 2, wi->data, 0x84);
+			DrawStringCenteredTruncated(r.left + 2, r.right - 2, r.top + 2, wi->data, TC_FROMSTRING);
 			break;
 
 		case WWT_DROPDOWN: {
