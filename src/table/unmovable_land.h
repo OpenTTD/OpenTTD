f520a702e0279fffaa893496452d2864b32ca705
belugas <belugas@openttd.org>
2008-08-21 19:12:24 +0000

    (svn r14123) -Codechange: a few magical number to sprite name conversions

--- src/table/unmovable_land.h
+++ src/table/unmovable_land.h
@@ -15,47 +15,47 @@ static const DrawTileSeqStruct _unmovable_display_nothing[] = {
 };
 
 static const DrawTileSeqStruct _unmovable_display_datas_8[] = {
-	TILE_SEQ_LINE(20, 0xA34 | (1 << PALETTE_MODIFIER_COLOR))
+	TILE_SEQ_LINE(20, SPR_MEDIUMHQ_NORTH_WALL | (1 << PALETTE_MODIFIER_COLOR))
 	TILE_SEQ_END()
 };
 
 static const DrawTileSeqStruct _unmovable_display_datas_9[] = {
-	TILE_SEQ_LINE(20, 0xA36 | (1 << PALETTE_MODIFIER_COLOR))
+	TILE_SEQ_LINE(20, SPR_MEDIUMHQ_EAST_WALL  | (1 << PALETTE_MODIFIER_COLOR))
 	TILE_SEQ_END()
 };
 
 static const DrawTileSeqStruct _unmovable_display_datas_10[] = {
-	TILE_SEQ_LINE(20, 0xA38 | (1 << PALETTE_MODIFIER_COLOR))
+	TILE_SEQ_LINE(20, SPR_MEDIUMHQ_WEST_WALL  | (1 << PALETTE_MODIFIER_COLOR))
 	TILE_SEQ_END()
 };
 
 static const DrawTileSeqStruct _unmovable_display_datas_12[] = {
-	TILE_SEQ_LINE(50, 0xA3B | (1 << PALETTE_MODIFIER_COLOR))
+	TILE_SEQ_LINE(50, SPR_LARGEHQ_NORTH_BUILD | (1 << PALETTE_MODIFIER_COLOR))
 	TILE_SEQ_END()
 };
 
 static const DrawTileSeqStruct _unmovable_display_datas_13[] = {
-	TILE_SEQ_LINE(50, 0xA3D | (1 << PALETTE_MODIFIER_COLOR))
+	TILE_SEQ_LINE(50, SPR_LARGEHQ_EAST_BUILD  | (1 << PALETTE_MODIFIER_COLOR))
 	TILE_SEQ_END()
 };
 
 static const DrawTileSeqStruct _unmovable_display_datas_14[] = {
-	TILE_SEQ_LINE(50, 0xA3F | (1 << PALETTE_MODIFIER_COLOR))
+	TILE_SEQ_LINE(50, SPR_LARGEHQ_WEST_BUILD  | (1 << PALETTE_MODIFIER_COLOR))
 	TILE_SEQ_END()
 };
 
 static const DrawTileSeqStruct _unmovable_display_datas_16[] = {
-	TILE_SEQ_LINE(60, 0xA42 | (1 << PALETTE_MODIFIER_COLOR))
+	TILE_SEQ_LINE(60, SPR_HUGEHQ_NORTH_BUILD  | (1 << PALETTE_MODIFIER_COLOR))
 	TILE_SEQ_END()
 };
 
 static const DrawTileSeqStruct _unmovable_display_datas_17[] = {
-	TILE_SEQ_LINE(60, 0xA44 | (1 << PALETTE_MODIFIER_COLOR))
+	TILE_SEQ_LINE(60, SPR_HUGEHQ_EAST_BUILD   | (1 << PALETTE_MODIFIER_COLOR))
 	TILE_SEQ_END()
 };
 
 static const DrawTileSeqStruct _unmovable_display_datas_18[] = {
-	TILE_SEQ_LINE(60, 0xA46 | (1 << PALETTE_MODIFIER_COLOR))
+	TILE_SEQ_LINE(60, SPR_HUGEHQ_WEST_BUILD   | (1 << PALETTE_MODIFIER_COLOR))
 	TILE_SEQ_END()
 };
 
@@ -65,26 +65,30 @@ static const DrawTileSeqStruct _unmovable_display_datas_18[] = {
 #define TILE_SPRITE_LINE(img, dtss) { {img | (1 << PALETTE_MODIFIER_COLOR), PAL_NONE}, dtss },
 
 static const DrawTileSprites _unmovable_display_datas[] = {
-	TILE_SPRITE_LINE(0xA2B, _unmovable_display_nothing)
-	TILE_SPRITE_LINE(0xA2C, _unmovable_display_nothing)
-	TILE_SPRITE_LINE(0xA2D, _unmovable_display_nothing)
-	TILE_SPRITE_LINE(0xA2E, _unmovable_display_nothing)
-	TILE_SPRITE_LINE(0xA2F, _unmovable_display_nothing)
-	TILE_SPRITE_LINE(0xA30, _unmovable_display_nothing)
-	TILE_SPRITE_LINE(0xA31, _unmovable_display_nothing)
-	TILE_SPRITE_LINE(0xA32, _unmovable_display_nothing)
-	TILE_SPRITE_LINE(0xA33, _unmovable_display_datas_8)
-	TILE_SPRITE_LINE(0xA35, _unmovable_display_datas_9)
-	TILE_SPRITE_LINE(0xA37, _unmovable_display_datas_10)
-	TILE_SPRITE_LINE(0xA39, _unmovable_display_nothing)
-	TILE_SPRITE_LINE(0xA3A, _unmovable_display_datas_12)
-	TILE_SPRITE_LINE(0xA3C, _unmovable_display_datas_13)
-	TILE_SPRITE_LINE(0xA3E, _unmovable_display_datas_14)
-	TILE_SPRITE_LINE(0xA40, _unmovable_display_nothing)
-	TILE_SPRITE_LINE(0xA41, _unmovable_display_datas_16)
-	TILE_SPRITE_LINE(0xA43, _unmovable_display_datas_17)
-	TILE_SPRITE_LINE(0xA45, _unmovable_display_datas_18)
-	TILE_SPRITE_LINE(0xA47, _unmovable_display_nothing)
+	TILE_SPRITE_LINE(SPR_TINYHQ_NORTH,         _unmovable_display_nothing)
+	TILE_SPRITE_LINE(SPR_TINYHQ_EAST,          _unmovable_display_nothing)
+	TILE_SPRITE_LINE(SPR_TINYHQ_WEST,          _unmovable_display_nothing)
+	TILE_SPRITE_LINE(SPR_TINYHQ_SOUTH,         _unmovable_display_nothing)
+
+	TILE_SPRITE_LINE(SPR_SMALLHQ_NORTH,        _unmovable_display_nothing)
+	TILE_SPRITE_LINE(SPR_SMALLHQ_EAST,         _unmovable_display_nothing)
+	TILE_SPRITE_LINE(SPR_SMALLHQ_WEST,         _unmovable_display_nothing)
+	TILE_SPRITE_LINE(SPR_SMALLHQ_SOUTH,        _unmovable_display_nothing)
+
+	TILE_SPRITE_LINE(SPR_MEDIUMHQ_NORTH,       _unmovable_display_datas_8)
+	TILE_SPRITE_LINE(SPR_MEDIUMHQ_EAST,        _unmovable_display_datas_9)
+	TILE_SPRITE_LINE(SPR_MEDIUMHQ_WEST,        _unmovable_display_datas_10)
+	TILE_SPRITE_LINE(SPR_MEDIUMHQ_SOUTH,       _unmovable_display_nothing)
+
+	TILE_SPRITE_LINE(SPR_LARGEHQ_NORTH_GROUND, _unmovable_display_datas_12)
+	TILE_SPRITE_LINE(SPR_LARGEHQ_EAST_GROUND,  _unmovable_display_datas_13)
+	TILE_SPRITE_LINE(SPR_LARGEHQ_WEST_GROUND,  _unmovable_display_datas_14)
+	TILE_SPRITE_LINE(SPR_LARGEHQ_SOUTH,        _unmovable_display_nothing)
+
+	TILE_SPRITE_LINE(SPR_HUGEHQ_NORTH_GROUND,  _unmovable_display_datas_16)
+	TILE_SPRITE_LINE(SPR_HUGEHQ_EAST_GROUND,   _unmovable_display_datas_17)
+	TILE_SPRITE_LINE(SPR_HUGEHQ_WEST_GROUND,   _unmovable_display_datas_18)
+	TILE_SPRITE_LINE(SPR_HUGEHQ_SOUTH,         _unmovable_display_nothing)
 };
 
 #undef TILE_SPRITE_LINE
