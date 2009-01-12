2c89bfbc8167c8808be71d08c918d52d92c5300d
truebrain <truebrain@openttd.org>
2009-01-12 19:31:00 +0000

    (svn r15038) -Fix (r15027): loading older savegames failed

--- src/company_cmd.cpp
+++ src/company_cmd.cpp
@@ -51,7 +51,7 @@ DEFINE_OLD_POOL_GENERIC(Company, Company)
 Company::Company(uint16 name_1, bool is_ai) : name_1(name_1), location_of_HQ(INVALID_TILE), is_ai(is_ai)
 {
 	for (uint j = 0; j < 4; j++) this->share_owners[j] = COMPANY_SPECTATOR;
-	this->is_noai = true;
+	this->is_noai = false;
 }
 
 Company::~Company()
