90381d5d636ed376c8ca8d2ec1124ec25e63553c
smatz <smatz@openttd.org>
2009-01-20 23:43:19 +0000

    (svn r15180) -Fix [FS#2560](r8498): old aircraft have to be updated after updating airport_type of oil rigs

--- src/saveload/afterload.cpp
+++ src/saveload/afterload.cpp
@@ -588,6 +588,10 @@ bool AfterLoadGame()
 		}
 	}
 
+	/* In version 2.2 of the savegame, we have new airports, so status of all aircraft is reset.
+	 * This has to be called after the oilrig airport_type update above ^^^ ! */
+	if (CheckSavegameVersionOldStyle(2, 2)) UpdateOldAircraft();
+
 	/* In version 6.1 we put the town index in the map-array. To do this, we need
 	 *  to use m2 (16bit big), so we need to clean m2, and that is where this is
 	 *  all about ;) */
