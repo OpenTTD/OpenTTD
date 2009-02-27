dbec2ec6289dfdae6f289485a97aea46ff169bb6
yexo <yexo@openttd.org>
2009-02-27 22:26:06 +0000

    (svn r15593) -Change: the background of the the waypoint sign is now in the company color.

--- src/viewport.cpp
+++ src/viewport.cpp
@@ -1215,7 +1215,7 @@ static void ViewportAddSigns(DrawPixelInfo *dpi)
 
 static void AddWaypoint(const Waypoint *wp, StringID str, uint16 width)
 {
-	AddStringToDraw(wp->sign.left + 1, wp->sign.top + 1, str, wp->index, 0, (wp->deleted ? 0xE : 11), width);
+	AddStringToDraw(wp->sign.left + 1, wp->sign.top + 1, str, wp->index, 0, (wp->deleted ? 0xE : _company_colours[wp->owner]), width);
 }
 
 
