e4a90f9688de9942ee98fb37254168ca06deff14
glx <glx@openttd.org>
2009-03-31 22:55:38 +0000

    (svn r15907) -Fix (r15841): some strings in save/load dialog were drawn with a 2 pixel offset

--- src/misc_gui.cpp
+++ src/misc_gui.cpp
@@ -1588,7 +1588,7 @@ public:
 		for (uint pos = this->vscroll.pos; pos < _fios_items.Length(); pos++) {
 			const FiosItem *item = _fios_items.Get(pos);
 
-			DrawString(widg->left + 4, widg->right - 2, y, item->title, _fios_colours[item->type]);
+			DrawString(widg->left + 2, widg->right - 2, y, item->title, _fios_colours[item->type]);
 			y += 10;
 			if (y >= this->vscroll.cap * 10 + widg->top + 1) break;
 		}
