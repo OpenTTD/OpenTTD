8f831ceebc054abc9d9abf1591bb0ebea9e4b5ab
rubidium <rubidium@openttd.org>
2009-04-01 02:58:38 +0000

    (svn r15909) -Fix (r15892): Apple, PLEASE do not fill your API with #if !__LP64__. Especially when another part of your API depends on whether __LP64__ is defined and thus making it non-trivial to silence the warnings from the undefinedness of __LP64__ in #if !__LP64__...

--- src/os/macosx/osx_stdafx.h
+++ src/os/macosx/osx_stdafx.h
@@ -5,6 +5,10 @@
 #ifndef MACOS_STDAFX_H
 #define MACOS_STDAFX_H
 
+/* We need to include this first as that "depends" on the compiler's setting
+ * of __LP64__. So before we define __LP64__ so it can be used. */
+#include <sys/cdefs.h>
+
 /* __LP64__ only exists in 10.5 and higher */
 #if defined(__APPLE__) && !defined(__LP64__)
 #	define __LP64__ 0
