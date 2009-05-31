75a818a32540ee05a244aae368fb84bf08b1ffc7
rubidium <rubidium@openttd.org>
2009-05-31 11:04:56 +0000

    (svn r16477) -Fix [FS#2936] (r12650): loading indicator when 'unload' in and 'no loading' is off was pointing to the wrong direction.

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -948,7 +948,7 @@ uint8 CalcPercentVehicleFilled(const Vehicle *v, StringID *colour)
 		max += v->cargo_cap;
 		if (v->cargo_cap != 0 && colour != NULL) {
 			unloading += HasBit(v->vehicle_flags, VF_CARGO_UNLOADING) ? 1 : 0;
-			loading |= !(u->current_order.GetUnloadType() & OUFB_UNLOAD) && st->goods[v->cargo_type].days_since_pickup != 255;
+			loading |= !(u->current_order.GetLoadType() & OLFB_NO_LOAD) && st->goods[v->cargo_type].days_since_pickup != 255;
 			cars++;
 		}
 	}
