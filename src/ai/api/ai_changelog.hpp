330c8d6a57c847d621c629ac709a811db01d51d1
yexo <yexo@openttd.org>
2009-08-20 13:36:48 +0000

    (svn r17237) -Update: AI changelog

--- src/ai/api/ai_changelog.hpp
+++ src/ai/api/ai_changelog.hpp
@@ -3,19 +3,9 @@
 /** @file ai_changelog.hpp Lists all changes / additions to the API.
  *
  * Only new / renamed / deleted api functions will be listed here. A list of
- * bug fixes can be found in the normal changelog.
- *
- * \b 0.9.0
- *
- * Scheduled API removals:
- * \li AIOrder::ChangeOrder
- * \li AISign::GetMaxSignID
- * \li AITile::GetHeight
- * \li AIBaseStation::WAYPOINT_INVALID
- * \li AISubsidy::SourceIsTown
- * \li AISubsidy::GetSource
- * \li AISubsidy::DestinationIsTown
- * \li AISubsidy::GetDestination
+ * bug fixes can be found in the normal changelog. Note that removed API
+ * functions may still be available if you return an older API version
+ * in GetAPIVersion() in info.nut.
  *
  * \b 0.8.0
  *
@@ -29,6 +19,17 @@
  * \li AISubsidy::GetDestinationType
  * \li AISubsidy::GetDestinationIndex
  *
+ * API removals:
+ * \li AIOrder::ChangeOrder
+ * \li AISign::GetMaxSignID
+ * \li AITile::GetHeight
+ * \li AIBaseStation::WAYPOINT_INVALID
+ * \li AISubsidy::SourceIsTown
+ * \li AISubsidy::GetSource
+ * \li AISubsidy::DestinationIsTown
+ * \li AISubsidy::GetDestination
+ * \li AIWaypoint::WAYPOINT_INVALID
+ *
  * Other changes:
  * \li The GetName / SetName / GetLocation functions were moved from AIStation
  *     and AIWaypoint to AIBaseStation, but you can still use AIStation.GetName
@@ -36,12 +37,7 @@
  * \li The GetConstructionDate function was moved from AIStation to
  *     AIBaseStation, but can still be used as AIStation.GetConstructionDate
  * \li WaypointID was replaced by StationID. All WaypointIDs from previous
- *     savegames are invalid
- * \li WAYPOINT_INVALID is now deprecated, use STATION_INVALID instead
- * \li AISubsidy::GetSource and AISubsidy::GetDestination return STATION_INVALID
- *     for awarded subsidies. They are deprecated as well
- * \li AIs can create subclasses of API classes and use API constants as part
- *     of their own constants
+ *     savegames are invalid. Use STATION_INVALID instead of WAYPOINT_INVALID
  * \li AIVehicleList_Station now also works for waypoints
  *
  * \b 0.7.3
@@ -53,6 +49,14 @@
  * \li AICompany::SetPresidentGender
  * \li AIEngine::GetDesignDate
  * \li AIStation::GetConstructionDate
+ *
+ * Other changes:
+ * \li AIs are now killed when they execute a DoCommand or Sleep at a time
+ *     they are not allowed to do so.
+ * \li When the API requests a string as parameter you can give ever squirrel
+ *     type and it'll be converted to a string
+ * \li AIs can create subclasses of API classes and use API constants as part
+ *     of their own constants
  *
  * \b 0.7.2
  *
