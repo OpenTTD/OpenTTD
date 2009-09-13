5de9cc46b50b8e677395543c10b26b59c860212b
rubidium <rubidium@openttd.org>
2009-09-13 17:58:00 +0000

    (svn r17531) -Fix: wrong @file for ai_basestation.hpp

--- src/ai/api/ai_basestation.hpp
+++ src/ai/api/ai_basestation.hpp
@@ -7,7 +7,7 @@
  * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OpenTTD. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/** @file ai_waypoint.hpp Everything to query and build waypoints. */
+/** @file ai_basestation.hpp Base for stations/waypoint handling. */
 
 #ifndef AI_BASESTATION_HPP
 #define AI_BASESTATION_HPP
