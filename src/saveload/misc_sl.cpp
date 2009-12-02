429f3993f8dc380780a23a6f46420797a9a8a11c
rubidium <rubidium@openttd.org>
2009-12-02 18:14:34 +0000

    (svn r18384) -Fix (r18351): attempt to 'work around' MSVC's IMO stupid warning

--- src/saveload/misc_sl.cpp
+++ src/saveload/misc_sl.cpp
@@ -52,7 +52,7 @@ void ResetViewportAfterLoadGame()
 	w->viewport->dest_scrollpos_y = _saved_scrollpos_y;
 
 	ViewPort *vp = w->viewport;
-	vp->zoom = min<ZoomLevel>(_saved_scrollpos_zoom, ZOOM_LVL_MAX);
+	vp->zoom = (ZoomLevel)min(_saved_scrollpos_zoom, ZOOM_LVL_MAX);
 	vp->virtual_width = ScaleByZoom(vp->width, vp->zoom);
 	vp->virtual_height = ScaleByZoom(vp->height, vp->zoom);
 
