879668811c88f153e4f9e867146e7dd813377bc6
rubidium <rubidium@openttd.org>
2010-01-15 22:21:50 +0000

    (svn r18818) -Fix [FS#3526] (r18766): some train acceleration type conversions were missed

--- src/engine_gui.cpp
+++ src/engine_gui.cpp
@@ -161,7 +161,7 @@ static StringID GetTrainEngineInfoString(const Engine *e)
 	} else {
 		SetDParam(5, CT_INVALID);
 	}
-	return (_settings_game.vehicle.train_acceleration_model != TAM_ORIGINAL && e->u.rail.railtype != RAILTYPE_MAGLEV) ? STR_ENGINE_PREVIEW_COST_WEIGHT_SPEED_POWER_MAX_TE : STR_ENGINE_PREVIEW_COST_WEIGHT_SPEED_POWER;
+	return (_settings_game.vehicle.train_acceleration_model != TAM_ORIGINAL && GetRailTypeInfo(e->u.rail.railtype)->acceleration_type != 2) ? STR_ENGINE_PREVIEW_COST_WEIGHT_SPEED_POWER_MAX_TE : STR_ENGINE_PREVIEW_COST_WEIGHT_SPEED_POWER;
 }
 
 static StringID GetAircraftEngineInfoString(const Engine *e)
