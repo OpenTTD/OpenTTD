f1458df1caed833434303de9ae59f864404e4d07
peter1138 <peter1138@openttd.org>
2010-02-03 08:02:07 +0000

    (svn r18990) -Codechange: [NewGRF] Add rail type map bounds checking to RailType[Change|Reserve]Info().

--- src/newgrf.cpp
+++ src/newgrf.cpp
@@ -2494,6 +2494,11 @@ static ChangeInfoResult RailTypeChangeInfo(uint id, int numinfo, int prop, ByteR
 
 	extern RailtypeInfo _railtypes[RAILTYPE_END];
 
+	if (id + numinfo > RAILTYPE_END) {
+		grfmsg(1, "RailTypeChangeInfo: Rail type %u is invalid, max %u, ignoring", id + numinfo, RAILTYPE_END);
+		return CIR_INVALID_ID;
+	}
+
 	for (int i = 0; i < numinfo; i++) {
 		RailType rt = _cur_grffile->railtype_map[id + i];
 		if (rt == INVALID_RAILTYPE) return CIR_INVALID_ID;
@@ -2588,6 +2593,11 @@ static ChangeInfoResult RailTypeChangeInfo(uint id, int numinfo, int prop, ByteR
 static ChangeInfoResult RailTypeReserveInfo(uint id, int numinfo, int prop, ByteReader *buf)
 {
 	ChangeInfoResult ret = CIR_SUCCESS;
+
+	if (id + numinfo > RAILTYPE_END) {
+		grfmsg(1, "RailTypeReserveInfo: Rail type %u is invalid, max %u, ignoring", id + numinfo, RAILTYPE_END);
+		return CIR_INVALID_ID;
+	}
 
 	for (int i = 0; i < numinfo; i++) {
 		switch (prop) {
