8c4c691448e29fdbfcee274720ac4e4612f0812a
peter1138 <peter1138@openttd.org>
2010-04-03 20:25:23 +0000

    (svn r19556) -Fix (r19434): Incorrect speed limit reported for rail depots with original acceleration model.

--- src/rail_cmd.cpp
+++ src/rail_cmd.cpp
@@ -2629,10 +2629,12 @@ static void GetTileDesc_Track(TileIndex tile, TileDesc *td)
 
 		case RAIL_TILE_DEPOT:
 			td->str = STR_LAI_RAIL_DESCRIPTION_TRAIN_DEPOT;
-			if (td->rail_speed > 0) {
-				td->rail_speed = min(td->rail_speed, 61);
-			} else {
-				td->rail_speed = 61;
+			if (_settings_game.vehicle.train_acceleration_model != AM_ORIGINAL) {
+				if (td->rail_speed > 0) {
+					td->rail_speed = min(td->rail_speed, 61);
+				} else {
+					td->rail_speed = 61;
+				}
 			}
 			break;
 
