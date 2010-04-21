bc81039cb8a5791055a101517280f71a65c05fa2
rubidium <rubidium@openttd.org>
2010-04-21 12:36:00 +0000

    (svn r19687) -Fix: desync when joining the game because of using the wrong variable

--- src/train_cmd.cpp
+++ src/train_cmd.cpp
@@ -412,7 +412,7 @@ int Train::GetCurrentMaxSpeed() const
 				int st_max_speed = 120;
 
 				int delta_v = this->cur_speed / (distance_to_go + 1);
-				if (this->max_speed > (this->cur_speed - delta_v)) {
+				if (max_speed > (this->cur_speed - delta_v)) {
 					st_max_speed = this->cur_speed - (delta_v / 10);
 				}
 
