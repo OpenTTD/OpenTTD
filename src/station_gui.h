c4e2a49225c1b07d6a43465560d891af3cf9c887
smatz <smatz@openttd.org>
2010-06-12 21:21:44 +0000

    (svn r19968) -Fix [FS#3876](r19955): the 'list of ships' button opened list of aircraft and vice versa

--- src/station_gui.h
+++ src/station_gui.h
@@ -29,8 +29,8 @@ enum StationViewWidgets {
 	SVW_RENAME     =  6, ///< 'Rename' button
 	SVW_TRAINS     =  7, ///< List of scheduled trains button
 	SVW_ROADVEHS,        ///< List of scheduled road vehs button
-	SVW_PLANES,          ///< List of scheduled planes button
 	SVW_SHIPS,           ///< List of scheduled ships button
+	SVW_PLANES,          ///< List of scheduled planes button
 };
 
 /** Types of cargo to display for station coverage. */
