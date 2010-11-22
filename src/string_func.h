3cf9f175d8d3744196f8401a4f9388d0b5a326a8
rubidium <rubidium@openttd.org>
2010-11-22 17:59:11 +0000

    (svn r21295) -Fix [FS#4259]: FreeBSD introduced strndup as well (MicRO)

--- src/string_func.h
+++ src/string_func.h
@@ -277,12 +277,12 @@ static inline bool IsWhitespace(WChar c)
 }
 
 /* Needed for NetBSD version (so feature) testing */
-#ifdef __NetBSD__
+#if defined(__NetBSD__) || defined(__FreeBSD__)
 #include <sys/param.h>
 #endif
 
 /* strndup is a GNU extension */
-#if defined(_GNU_SOURCE) || (defined(__NetBSD_Version__) && 400000000 <= __NetBSD_Version__)
+#if defined(_GNU_SOURCE) || (defined(__NetBSD_Version__) && 400000000 <= __NetBSD_Version__) || (defined(__FreeBSD_version) && 720000 <= __FreeBSD_version)
 #	undef DEFINE_STRNDUP
 #else
 #	define DEFINE_STRNDUP
