f2cc73bb517b0145455bc216f9729ad4ee878e1e
terkhen <terkhen@openttd.org>
2010-11-27 22:42:24 +0000

    (svn r21341) -Add: Use the complete industry name instead of only the town when sorting industries by name.

--- src/industry_gui.cpp
+++ src/industry_gui.cpp
@@ -1128,13 +1128,13 @@ protected:
 		static char buf_cache[96];
 		static char buf[96];
 
-		SetDParam(0, (*a)->town->index);
-		GetString(buf, STR_TOWN_NAME, lastof(buf));
+		SetDParam(0, (*a)->index);
+		GetString(buf, STR_INDUSTRY_NAME, lastof(buf));
 
 		if (*b != last_industry) {
 			last_industry = *b;
-			SetDParam(0, (*b)->town->index);
-			GetString(buf_cache, STR_TOWN_NAME, lastof(buf_cache));
+			SetDParam(0, (*b)->index);
+			GetString(buf_cache, STR_INDUSTRY_NAME, lastof(buf_cache));
 		}
 
 		return strcmp(buf, buf_cache);
