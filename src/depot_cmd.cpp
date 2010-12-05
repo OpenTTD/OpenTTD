091263317e7eb8a882813a11f6da53058c266df5
rubidium <rubidium@openttd.org>
2010-12-05 22:24:04 +0000

    (svn r21409) -Codechange: limit depot name by amount of characters, not bytes

--- src/depot_cmd.cpp
+++ src/depot_cmd.cpp
@@ -53,7 +53,7 @@ CommandCost CmdRenameDepot(TileIndex tile, DoCommandFlag flags, uint32 p1, uint3
 	bool reset = StrEmpty(text);
 
 	if (!reset) {
-		if (strlen(text) >= MAX_LENGTH_DEPOT_NAME_BYTES) return CMD_ERROR;
+		if (Utf8StringLength(text) >= MAX_LENGTH_DEPOT_NAME_CHARS) return CMD_ERROR;
 		if (!IsUniqueDepotName(text)) return_cmd_error(STR_ERROR_NAME_MUST_BE_UNIQUE);
 	}
 
