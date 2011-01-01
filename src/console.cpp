62bc55cf7be5afc1c70bdd358e2d2748163f2881
smatz <smatz@openttd.org>
2011-01-01 16:35:32 +0000

    (svn r21686) -Cleanup: remove unused constants and comment used ones

--- src/console.cpp
+++ src/console.cpp
@@ -20,13 +20,8 @@
 
 #include <stdarg.h>
 
-#define ICON_BUFFER 79
-#define ICON_HISTORY_SIZE 20
-#define ICON_LINE_HEIGHT 12
-#define ICON_RIGHT_BORDERWIDTH 10
-#define ICON_BOTTOM_BORDERWIDTH 12
-#define ICON_MAX_ALIAS_LINES 40
-#define ICON_TOKEN_COUNT 20
+static const uint ICON_TOKEN_COUNT = 20;     ///< Maximum number of tokens in one command
+static const uint ICON_MAX_ALIAS_LINES = 40; ///< Maximum number of commands executed by one alias
 
 /* console parser */
 IConsoleCmd   *_iconsole_cmds;    ///< list of registered commands
