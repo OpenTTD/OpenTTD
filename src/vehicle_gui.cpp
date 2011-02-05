3bb7f7ebe4d097fe6061db5abf3bdc4fc10f9353
frosch <frosch@openttd.org>
2011-02-05 23:10:31 +0000

    (svn r21991) -Fix [FS#4466]: The share/copy-orders-cursor was not updated to refer to the new vehicle when it got autoreplaced/-renewed.

--- src/vehicle_gui.cpp
+++ src/vehicle_gui.cpp
@@ -1105,8 +1105,16 @@ static inline void ChangeVehicleWindow(WindowClass window_class, VehicleID from_
 {
 	Window *w = FindWindowById(window_class, from_index);
 	if (w != NULL) {
+		/* Update window_number */
 		w->window_number = to_index;
 		if (w->viewport != NULL) w->viewport->follow_vehicle = to_index;
+
+		/* Update vehicle drag data */
+		if (_thd.window_class == window_class && _thd.window_number == (WindowNumber)from_index) {
+			_thd.window_number = to_index;
+		}
+
+		/* Notify the window */
 		w->InvalidateData();
 	}
 }
