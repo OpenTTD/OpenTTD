b1873526635efa1b6fb2e2ba636f0d3368a9389e
rubidium <rubidium@openttd.org>
2011-02-11 13:38:34 +0000

    (svn r22058) -Fix (r22050): removing broken orders happened before the "front engine" conversion was done for road vehicles, so road vehicles didn't have orders anymore

--- src/saveload/vehicle_sl.cpp
+++ src/saveload/vehicle_sl.cpp
@@ -289,12 +289,6 @@ void AfterLoadVehicles(bool part_of_load)
 					}
 				}
 			}
-
-			/* In some old savegames there might be some "crap" stored. */
-			if (IsSavegameVersionBefore(160) && !v->IsPrimaryVehicle()) {
-				v->current_order.Free();
-				v->unitnumber = 0;
-			}
 		}
 	}
 
@@ -333,6 +327,16 @@ void AfterLoadVehicles(bool part_of_load)
 					rv->SetArticulatedPart();
 				} else {
 					NOT_REACHED();
+				}
+			}
+		}
+
+		if (IsSavegameVersionBefore(160)) {
+			/* In some old savegames there might be some "crap" stored. */
+			FOR_ALL_VEHICLES(v) {
+				if (!v->IsPrimaryVehicle()) {
+					v->current_order.Free();
+					v->unitnumber = 0;
 				}
 			}
 		}
