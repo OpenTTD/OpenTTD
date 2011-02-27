8074f123d524d404cd60f7ff6edd533962668efc
smatz <smatz@openttd.org>
2011-02-27 13:59:20 +0000

    (svn r22155) -Codechange: one comment in saveload/station_sl.cpp was wrong

--- src/saveload/station_sl.cpp
+++ src/saveload/station_sl.cpp
@@ -7,7 +7,7 @@
  * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OpenTTD. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/** @file station_sl.cpp Code handling saving and loading of economy data */
+/** @file station_sl.cpp Code handling saving and loading of stations. */
 
 #include "../stdafx.h"
 #include "../station_base.h"
