75b87c554f140be0b94358d66deeffed116621b2
rubidium <rubidium@openttd.org>
2011-04-22 16:09:46 +0000

    (svn r22371) -Fix [FS#4596]: make sure saving has completely and utterly finished before starting a new one. Otherwise you could start a save, which would be marked as done by the previous save stopping and then yet another save could be started... and that could create a deadlock

--- src/saveload/saveload.cpp
+++ src/saveload/saveload.cpp
@@ -2420,6 +2420,9 @@ void WaitTillSaved()
 	_save_thread->Join();
 	delete _save_thread;
 	_save_thread = NULL;
+
+	/* Make sure every other state is handled properly as well. */
+	ProcessAsyncSaveFinish();
 }
 
 /**
