e95840b6a89d83f9908f751a70c58a87709bd372
rubidium <rubidium@openttd.org>
2011-06-04 13:38:54 +0000

    (svn r22536) -Fix [FS#4632]: documentation omission regarding admin protocol

--- src/network/core/tcp_admin.h
+++ src/network/core/tcp_admin.h
@@ -340,6 +340,7 @@ protected:
 	 * uint64  Money.
 	 * uint64  Loan.
 	 * uint64  Income.
+	 * uint16  Delivered cargo (this quarter).
 	 * uint64  Company value (last quarter).
 	 * uint16  Performance (last quarter).
 	 * uint16  Delivered cargo (last quarter).
