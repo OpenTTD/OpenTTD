39eaf0e10d89188470878e9c89430d5352b1c009
rubidium <rubidium@openttd.org>
2011-09-09 20:28:48 +0000

    (svn r22913) -Fix [OSX]: make splash work again and make it work without needing to bundle it first

--- src/os/macosx/splash.h
+++ src/os/macosx/splash.h
@@ -12,16 +12,8 @@
 #ifndef SPLASH_H
 #define SPLASH_H
 
-#define SPLASH_IMAGE_FILE		"splash.png"
+#define SPLASH_IMAGE_FILE "splash.png"
 
-#ifdef __cplusplus
-extern "C" {
-#endif //__cplusplus
-
-	void DisplaySplashImage();
-
-#ifdef __cplusplus
-}
-#endif //__cplusplus
+void DisplaySplashImage();
 
 #endif /* SPLASH_H */
