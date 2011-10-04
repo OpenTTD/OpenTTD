4284b8abb39b0397de1bdec500221c10278ad1fc
rubidium <rubidium@openttd.org>
2011-10-04 20:06:23 +0000

    (svn r22991) [1.1] -Backport from trunk: - Fix: Old TTO/TTD savegames could get non-stop via orders upon savegame loading, even when those orders did not exist back then. This 'conversion' feature is something for TTDPatch and old OpenTTD savegames [FS#4716] (r22914) - Fix: The icon would (almost) never be shown for SDL builds [FS#4617] (r22910) - Fix: The name of the heightmap glitches in the 'play heightmap' window (r22902) - Fix: Locks would be incorrectly assembled (r22108)

--- src/table/water_land.h
+++ src/table/water_land.h
@@ -20,32 +20,34 @@ struct WaterDrawTileStruct {
 };
 
 #define BEGIN(image) { 0, 0, 0, 0, 0, 0, image }
-#define END(y) { 0x80, y, 0, 0, 0, 0, 0 }
+
+/** Constructor macro for a terminating WaterDrawTileStruct entry in an array */
+#define TILE_SEQ_END() { 0x80, 0, 0, 0, 0, 0, 0 }
 
 static const WaterDrawTileStruct _shipdepot_display_seq_1[] = {
 	BEGIN(0xFDD),
 	{ 0, 15, 0, 16, 1, 0x14, 0xFE8 | (1 << PALETTE_MODIFIER_COLOUR) },
-	END(0)
+	TILE_SEQ_END()
 };
 
 static const WaterDrawTileStruct _shipdepot_display_seq_2[] = {
 	BEGIN(0xFDD),
 	{ 0,  0, 0, 16, 1, 0x14, 0xFEA },
 	{ 0, 15, 0, 16, 1, 0x14, 0xFE6 | (1 << PALETTE_MODIFIER_COLOUR) },
-	END(0)
+	TILE_SEQ_END()
 };
 
 static const WaterDrawTileStruct _shipdepot_display_seq_3[] = {
 	BEGIN(0xFDD),
 	{ 15, 0, 0, 1, 0x10, 0x14, 0xFE9 | (1 << PALETTE_MODIFIER_COLOUR) },
-	END(0)
+	TILE_SEQ_END()
 };
 
 static const WaterDrawTileStruct _shipdepot_display_seq_4[] = {
 	BEGIN(0xFDD),
 	{  0, 0, 0, 1, 16, 0x14, 0xFEB },
 	{ 15, 0, 0, 1, 16, 0x14, 0xFE7 | (1 << PALETTE_MODIFIER_COLOUR) },
-	END(0)
+	TILE_SEQ_END()
 };
 
 static const WaterDrawTileStruct * const _shipdepot_display_seq[] = {
@@ -59,84 +61,84 @@ static const WaterDrawTileStruct _lock_display_seq_0[] = {
 	BEGIN(1),
 	{ 0,   0, 0, 0x10, 1, 0x14, 0 + 1 },
 	{ 0, 0xF, 0, 0x10, 1, 0x14, 4 + 1 },
-	END(0)
+	TILE_SEQ_END()
 };
 
 static const WaterDrawTileStruct _lock_display_seq_1[] = {
 	BEGIN(0),
 	{   0, 0, 0, 1, 0x10, 0x14, 0 },
 	{ 0xF, 0, 0, 1, 0x10, 0x14, 4 },
-	END(0)
+	TILE_SEQ_END()
 };
 
 static const WaterDrawTileStruct _lock_display_seq_2[] = {
 	BEGIN(2),
 	{ 0,   0, 0, 0x10, 1, 0x14, 0 + 2 },
 	{ 0, 0xF, 0, 0x10, 1, 0x14, 4 + 2 },
-	END(0)
+	TILE_SEQ_END()
 };
 
 static const WaterDrawTileStruct _lock_display_seq_3[] = {
 	BEGIN(3),
 	{   0, 0, 0, 1, 0x10, 0x14, 0 + 3 },
 	{ 0xF, 0, 0, 1, 0x10, 0x14, 4 + 3 },
-	END(0)
+	TILE_SEQ_END()
 };
 
 static const WaterDrawTileStruct _lock_display_seq_0b[] = {
 	BEGIN(0xFDD),
 	{ 0,   0, 0, 0x10, 1, 0x14, 8 + 1 },
 	{ 0, 0xF, 0, 0x10, 1, 0x14, 12 + 1 },
-	END(0)
+	TILE_SEQ_END()
 };
 
 static const WaterDrawTileStruct _lock_display_seq_1b[] = {
 	BEGIN(0xFDD),
 	{   0, 0, 0, 0x1, 0x10, 0x14, 8 },
 	{ 0xF, 0, 0, 0x1, 0x10, 0x14, 12 },
-	END(0)
+	TILE_SEQ_END()
 };
 
 static const WaterDrawTileStruct _lock_display_seq_2b[] = {
 	BEGIN(0xFDD),
 	{ 0,   0, 0, 0x10, 1, 0x14, 8 + 2 },
 	{ 0, 0xF, 0, 0x10, 1, 0x14, 12 + 2 },
-	END(0)
+	TILE_SEQ_END()
 };
 
 static const WaterDrawTileStruct _lock_display_seq_3b[] = {
 	BEGIN(0xFDD),
 	{   0, 0, 0, 1, 0x10, 0x14, 8 + 3 },
 	{ 0xF, 0, 0, 1, 0x10, 0x14, 12 + 3 },
-	END(0)
+	TILE_SEQ_END()
 };
 
 static const WaterDrawTileStruct _lock_display_seq_0t[] = {
 	BEGIN(0xFDD),
 	{ 0,   0, 0, 0x10, 1, 0x14, 16 + 1 },
 	{ 0, 0xF, 0, 0x10, 1, 0x14, 20 + 1 },
-	END(8)
+	TILE_SEQ_END()
 };
 
 static const WaterDrawTileStruct _lock_display_seq_1t[] = {
 	BEGIN(0xFDD),
 	{   0, 0, 0, 0x1, 0x10, 0x14, 16 },
 	{ 0xF, 0, 0, 0x1, 0x10, 0x14, 20 },
-	END(8)
+	TILE_SEQ_END()
 };
 
 static const WaterDrawTileStruct _lock_display_seq_2t[] = {
 	BEGIN(0xFDD),
 	{ 0,   0, 0, 0x10, 1, 0x14, 16 + 2 },
 	{ 0, 0xF, 0, 0x10, 1, 0x14, 20 + 2 },
-	END(8)
+	TILE_SEQ_END()
 };
 
 static const WaterDrawTileStruct _lock_display_seq_3t[] = {
 	BEGIN(0xFDD),
 	{   0, 0, 0, 1, 0x10, 0x14, 16 + 3 },
 	{ 0xF, 0, 0, 1, 0x10, 0x14, 20 + 3 },
-	END(8)
+	TILE_SEQ_END()
 };
 
 static const WaterDrawTileStruct * const _lock_display_seq[] = {
@@ -157,4 +159,4 @@ static const WaterDrawTileStruct * const _lock_display_seq[] = {
 };
 
 #undef BEGIN
-#undef END
+#undef TILE_SEQ_END
