a057b8c6349cf03f8d38acf56697d2e72cdc2380
truebrain <truebrain@openttd.org>
2012-01-01 10:50:38 +0000

    (svn r23699) -Fix [FS#4928] (r23630): too much copy/pasting only allowed a silly low amount of goals to be created.

--- src/goal_base.h
+++ src/goal_base.h
@@ -17,7 +17,7 @@
 #include "goal_type.h"
 #include "core/pool_type.hpp"
 
-typedef Pool<Goal, GoalID, 1, MAX_COMPANIES> GoalPool;
+typedef Pool<Goal, GoalID, 1, 256> GoalPool;
 extern GoalPool _goal_pool;
 
 /** Struct about subsidies, offered and awarded */
