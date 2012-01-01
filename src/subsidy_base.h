39f2d75e0315d42472ba645fbfdd40a350e69e54
truebrain <truebrain@openttd.org>
2012-01-01 10:50:46 +0000

    (svn r23700) -Fix: the size of the Subsidies pool used a random macro, which didn't really make sense in the grand scheme of things

--- src/subsidy_base.h
+++ src/subsidy_base.h
@@ -17,7 +17,7 @@
 #include "subsidy_type.h"
 #include "core/pool_type.hpp"
 
-typedef Pool<Subsidy, SubsidyID, 1, MAX_COMPANIES> SubsidyPool;
+typedef Pool<Subsidy, SubsidyID, 1, 256> SubsidyPool;
 extern SubsidyPool _subsidy_pool;
 
 /** Struct about subsidies, offered and awarded */
