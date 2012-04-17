6d5f864e25680e8f0816f452b75425562edfb5ae
michi_cc <michi_cc@openttd.org>
2012-04-17 21:39:09 +0000

    (svn r24148) -Add [FS#4907]: South Korean and South African currencies. (PaulC)

--- src/currency.cpp
+++ src/currency.cpp
@@ -53,6 +53,8 @@ static const CurrencySpec origin_currency_specs[NUM_CURRENCY] = {
 	{    4, "", CF_NOEURO, "R$" NBSP,      "",               0, STR_GAME_OPTIONS_CURRENCY_BRL    }, ///< brazil real
 	{   31, "", 2011,      "",             NBSP "EEK",       1, STR_GAME_OPTIONS_CURRENCY_EEK    }, ///< estonian krooni
 	{    4, "", 2014,      "",             NBSP "Lt",        1, STR_GAME_OPTIONS_CURRENCY_LTL    }, ///< lithuanian litas
+	{ 1850, "", CF_NOEURO, "\xE2\x82\xA9", "",               0, STR_GAME_OPTIONS_CURRENCY_KRW    }, ///< south korean won
+	{   13, "", CF_NOEURO, "R" NBSP,       "",               0, STR_GAME_OPTIONS_CURRENCY_ZAR    }, ///< south african rand
 	{    1, "", CF_NOEURO, "",             "",               2, STR_GAME_OPTIONS_CURRENCY_CUSTOM }, ///< custom currency
 };
 
