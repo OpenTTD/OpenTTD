5bfed864a08dce717e2658786948e6659872b8d4
rubidium <rubidium@openttd.org>
2012-05-09 19:11:09 +0000

    (svn r24219) -Fix [FS#5152]: immediately do the cargo payment on vehicle crashes instead of when they are cleared

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -200,6 +200,9 @@ uint Vehicle::Crash(bool flooded)
 	SetWindowDirty(WC_VEHICLE_DETAILS, this->index);
 	SetWindowDirty(WC_VEHICLE_DEPOT, this->tile);
 
+	delete this->cargo_payment;
+	this->cargo_payment = NULL;
+
 	return RandomRange(pass + 1); // Randomise deceased passengers.
 }
 
