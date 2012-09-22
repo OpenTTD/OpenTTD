d00dfaff4c0646ba72d15e3e3d485cba7d440efc
alberth <alberth@openttd.org>
2012-09-22 19:16:04 +0000

    (svn r24550) -Codechange: Remove unused panel widget in the group gui (Juanjo).

--- src/group_gui.cpp
+++ src/group_gui.cpp
@@ -60,7 +60,6 @@ static const NWidgetPart _nested_group_widgets[] = {
 				NWidget(WWT_PANEL, COLOUR_GREY), SetFill(1, 1), EndContainer(),
 				NWidget(WWT_PUSHIMGBTN, COLOUR_GREY, WID_GL_REPLACE_PROTECTION), SetFill(0, 1),
 						SetDataTip(SPR_GROUP_REPLACE_OFF_TRAIN, STR_GROUP_REPLACE_PROTECTION_TOOLTIP),
-				NWidget(WWT_PANEL, COLOUR_GREY), SetFill(0, 1), EndContainer(),
 			EndContainer(),
 		EndContainer(),
 		/* right part */
