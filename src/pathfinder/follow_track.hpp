962a7c36307e16753d9c0c4c0c0983771709a240
frosch <frosch@openttd.org>
2012-10-13 08:20:40 +0000

    (svn r24587) -Codechange: Improve coding style (Juanjo)

--- src/pathfinder/follow_track.hpp
+++ src/pathfinder/follow_track.hpp
@@ -86,13 +86,13 @@ struct CFollowTrackT
 		m_railtypes = railtype_override;
 	}
 
-	inline static TransportType TT() {return Ttr_type_;}
-	inline static bool IsWaterTT() {return TT() == TRANSPORT_WATER;}
-	inline static bool IsRailTT() {return TT() == TRANSPORT_RAIL;}
-	inline bool IsTram() {return IsRoadTT() && HasBit(RoadVehicle::From(m_veh)->compatible_roadtypes, ROADTYPE_TRAM);}
-	inline static bool IsRoadTT() {return TT() == TRANSPORT_ROAD;}
-	inline static bool Allow90degTurns() {return T90deg_turns_allowed_;}
-	inline static bool DoTrackMasking() {return IsRailTT() && Tmask_reserved_tracks;}
+	inline static TransportType TT() { return Ttr_type_; }
+	inline static bool IsWaterTT() { return TT() == TRANSPORT_WATER; }
+	inline static bool IsRailTT() { return TT() == TRANSPORT_RAIL; }
+	inline bool IsTram() { return IsRoadTT() && HasBit(RoadVehicle::From(m_veh)->compatible_roadtypes, ROADTYPE_TRAM); }
+	inline static bool IsRoadTT() { return TT() == TRANSPORT_ROAD; }
+	inline static bool Allow90degTurns() { return T90deg_turns_allowed_; }
+	inline static bool DoTrackMasking() { return IsRailTT() && Tmask_reserved_tracks; }
 
 	/** Tests if a tile is a road tile with a single tramtrack (tram can reverse) */
 	inline DiagDirection GetSingleTramBit(TileIndex tile)
