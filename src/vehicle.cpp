2770a24f9f662c702ed6f3b469950d0e65d8423a
frosch <frosch@openttd.org>
2013-08-06 17:31:16 +0000

    (svn r25695) -Fix [FS#5700] (r24882, r25648): Vehicle::MarkDirty must be called for the front engine.

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -856,7 +856,7 @@ static void RunVehicleDayProc()
 
 				/* After a vehicle trigger, the graphics and properties of the vehicle could change.
 				 * Note: MarkDirty also invalidates the palette, which is the meaning of bit 1. So, nothing special there. */
-				if (callback != 0) v->MarkDirty();
+				if (callback != 0) v->First()->MarkDirty();
 
 				if (callback & ~3) ErrorUnknownCallbackResult(v->GetGRFID(), CBID_VEHICLE_32DAY_CALLBACK, callback);
 			}
