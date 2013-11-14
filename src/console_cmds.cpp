a31b46ba840f1478666001283200b8176314b1f8
zuu <zuu@openttd.org>
2013-11-14 22:50:16 +0000

    (svn r26000) -Add: Optional filter parameter to the 'content state' console command, to limit the content list to only content where the name match the filter

--- src/console_cmds.cpp
+++ src/console_cmds.cpp
@@ -1741,12 +1741,12 @@ DEF_CONSOLE_CMD(ConContent)
 	}
 
 	if (argc <= 1) {
-		IConsoleHelp("Query, select and download content. Usage: 'content update|upgrade|select [all|id]|unselect [all|id]|state|download'");
+		IConsoleHelp("Query, select and download content. Usage: 'content update|upgrade|select [all|id]|unselect [all|id]|state [filter]|download'");
 		IConsoleHelp("  update: get a new list of downloadable content; must be run first");
 		IConsoleHelp("  upgrade: select all items that are upgrades");
 		IConsoleHelp("  select: select a specific item given by its id or 'all' to select all. If no parameter is given, all selected content will be listed");
 		IConsoleHelp("  unselect: unselect a specific item given by its id or 'all' to unselect all");
-		IConsoleHelp("  state: show the download/select state of all downloadable content");
+		IConsoleHelp("  state: show the download/select state of all downloadable content. Optionally give a filter string");
 		IConsoleHelp("  download: download all content you've selected");
 		return true;
 	}
@@ -1793,6 +1793,7 @@ DEF_CONSOLE_CMD(ConContent)
 	if (strcasecmp(argv[1], "state") == 0) {
 		IConsolePrintF(CC_WHITE, "id, type, state, name");
 		for (ConstContentIterator iter = _network_content_client.Begin(); iter != _network_content_client.End(); iter++) {
+			if (argc > 2 && strcasestr((*iter)->name, argv[2]) == NULL) continue;
 			OutputContentState(*iter);
 		}
 		return true;
