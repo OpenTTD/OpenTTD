aa5a570b0de70b8b49da6905085e1e0cbe325d11
rubidium <rubidium@openttd.org>
2014-04-26 16:55:30 +0000

    (svn r26518) -Change: make genworld messages on the dedicated server disable-able by using -dnet=0

--- src/genworld.cpp
+++ src/genworld.cpp
@@ -101,7 +101,7 @@ static void _GenerateWorld(void *)
 	try {
 		_generating_world = true;
 		_modal_progress_work_mutex->BeginCritical();
-		if (_network_dedicated) DEBUG(net, 0, "Generating map, please wait...");
+		if (_network_dedicated) DEBUG(net, 1, "Generating map, please wait...");
 		/* Set the Random() seed to generation_seed so we produce the same map with the same seed */
 		if (_settings_game.game_creation.generation_seed == GENERATE_NEW_SEED) _settings_game.game_creation.generation_seed = _settings_newgame.game_creation.generation_seed = InteractiveRandom();
 		_random.SetSeed(_settings_game.game_creation.generation_seed);
@@ -198,7 +198,7 @@ static void _GenerateWorld(void *)
 
 		ShowNewGRFError();
 
-		if (_network_dedicated) DEBUG(net, 0, "Map generated, starting game");
+		if (_network_dedicated) DEBUG(net, 1, "Map generated, starting game");
 		DEBUG(desync, 1, "new_map: %08x", _settings_game.game_creation.generation_seed);
 
 		if (_debug_desync_level > 0) {
