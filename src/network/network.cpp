e64249446d2792c2d681f37e10a5a5a26d7151b2
rubidium <rubidium@openttd.org>
2014-05-11 13:07:36 +0000

    (svn r26577) -Fix [FS#6001]: [Network] Client of non-dedicated server was not correctly put into the first company for all state variables

--- src/network/network.cpp
+++ src/network/network.cpp
@@ -711,7 +711,7 @@ static void NetworkInitGameInfo()
 	/* There should be always space for the server. */
 	assert(NetworkClientInfo::CanAllocateItem());
 	NetworkClientInfo *ci = new NetworkClientInfo(CLIENT_ID_SERVER);
-	ci->client_playas = _network_dedicated ? COMPANY_SPECTATOR : _local_company;
+	ci->client_playas = _network_dedicated ? COMPANY_SPECTATOR : COMPANY_FIRST;
 
 	strecpy(ci->client_name, _settings_client.network.client_name, lastof(ci->client_name));
 }
