d733c7d196fc7077b97117fc3cad7a909f737bba
rubidium <rubidium@openttd.org>
2014-08-16 10:20:15 +0000

    (svn r26736) -Fix [Script]: infinite loop (by scripts; always returning the same item) when sorted by item in descending order by oddly constructed FindNext that uses forward iterators. Simply use the same code as the ascending item sort but use the reverse iterator instead of the forward. Also do this same trick for the descending value sort even though I couldn't trigger the problem there

--- src/script/api/script_list.cpp
+++ src/script/api/script_list.cpp
@@ -151,9 +151,9 @@ public:
  */
 class ScriptListSorterValueDescending : public ScriptListSorter {
 private:
-	ScriptList::ScriptListBucket::iterator bucket_iter;    ///< The iterator over the list to find the buckets.
-	ScriptList::ScriptItemList *bucket_list;               ///< The current bucket list we're iterator over.
-	ScriptList::ScriptItemList::iterator bucket_list_iter; ///< The iterator over the bucket list.
+	ScriptList::ScriptListBucket::reverse_iterator bucket_iter;    ///< The iterator over the list to find the buckets.
+	ScriptList::ScriptItemList *bucket_list;                       ///< The current bucket list we're iterator over.
+	ScriptList::ScriptItemList::reverse_iterator bucket_list_iter; ///< The iterator over the bucket list.
 
 public:
 	/**
@@ -172,13 +172,11 @@ public:
 		this->has_no_more_items = false;
 
 		/* Go to the end of the bucket-list */
-		this->bucket_iter = this->list->buckets.begin();
-		for (size_t i = this->list->buckets.size(); i > 1; i--) this->bucket_iter++;
+		this->bucket_iter = this->list->buckets.rbegin();
 		this->bucket_list = &(*this->bucket_iter).second;
 
 		/* Go to the end of the items in the bucket */
-		this->bucket_list_iter = this->bucket_list->begin();
-		for (size_t i = this->bucket_list->size(); i > 1; i--) this->bucket_list_iter++;
+		this->bucket_list_iter = this->bucket_list->rbegin();
 		this->item_next = *this->bucket_list_iter;
 
 		int32 item_current = this->item_next;
@@ -203,18 +201,15 @@ public:
 			return;
 		}
 
-		if (this->bucket_list_iter == this->bucket_list->begin()) {
-			if (this->bucket_iter == this->list->buckets.begin()) {
+		this->bucket_list_iter++;
+		if (this->bucket_list_iter == this->bucket_list->rend()) {
+			this->bucket_iter++;
+			if (this->bucket_iter == this->list->buckets.rend()) {
 				this->bucket_list = NULL;
 				return;
 			}
-			this->bucket_iter--;
 			this->bucket_list = &(*this->bucket_iter).second;
-			/* Go to the end of the items in the bucket */
-			this->bucket_list_iter = this->bucket_list->begin();
-			for (size_t i = this->bucket_list->size(); i > 1; i--) this->bucket_list_iter++;
-		} else {
-			this->bucket_list_iter--;
+			this->bucket_list_iter = this->bucket_list->rbegin();
 		}
 		this->item_next = *this->bucket_list_iter;
 	}
@@ -315,7 +310,7 @@ public:
  */
 class ScriptListSorterItemDescending : public ScriptListSorter {
 private:
-	ScriptList::ScriptListMap::iterator item_iter; ///< The iterator over the items in the map.
+	ScriptList::ScriptListMap::reverse_iterator item_iter; ///< The iterator over the items in the map.
 
 public:
 	/**
@@ -333,8 +328,7 @@ public:
 		if (this->list->items.empty()) return 0;
 		this->has_no_more_items = false;
 
-		this->item_iter = this->list->items.begin();
-		for (size_t i = this->list->items.size(); i > 1; i--) this->item_iter++;
+		this->item_iter = this->list->items.rbegin();
 		this->item_next = (*this->item_iter).first;
 
 		int32 item_current = this->item_next;
@@ -352,12 +346,12 @@ public:
 	 */
 	void FindNext()
 	{
-		if (this->item_iter == this->list->items.end()) {
+		if (this->item_iter == this->list->items.rend()) {
 			this->has_no_more_items = true;
 			return;
 		}
-		this->item_iter--;
-		if (this->item_iter != this->list->items.end()) item_next = (*this->item_iter).first;
+		this->item_iter++;
+		if (this->item_iter != this->list->items.rend()) item_next = (*this->item_iter).first;
 	}
 
 	int32 Next()
