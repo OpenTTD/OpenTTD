79ca66eb673c710432d1f4e51af70730a09a82c8
rubidium <rubidium@openttd.org>
2014-09-19 20:06:51 +0000

    (svn r26853) -Cleanup [Squirrel]: remove some stuff that we never did and especially never should use

--- src/3rdparty/squirrel/include/sqstdsystem.h
+++
@@ -1,7 +0,0 @@
-/*	see copyright notice in squirrel.h */
-#ifndef _SQSTD_SYSTEMLIB_H_
-#define _SQSTD_SYSTEMLIB_H_
-
-SQInteger sqstd_register_systemlib(HSQUIRRELVM v);
-
-#endif /* _SQSTD_SYSTEMLIB_H_ */
