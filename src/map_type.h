e3b53fb67dfd8a64ef9576504fea1145608b73f6
planetmaker <planetmaker@openttd.org>
2015-01-26 10:27:51 +0000

    (svn r27126) -Fix [FS#6218] (r26873): Reduce memory footprint of map array by shuffling its variables

--- src/map_type.h
+++ src/map_type.h
@@ -19,8 +19,8 @@
 struct Tile {
 	byte   type;        ///< The type (bits 4..7), bridges (2..3), rainforest/desert (0..1)
 	byte   height;      ///< The height of the northern corner.
-	byte   m1;          ///< Primarily used for ownership information
 	uint16 m2;          ///< Primarily used for indices to towns, industries and stations
+	byte   m1;          ///< Primarily used for ownership information
 	byte   m3;          ///< General purpose
 	byte   m4;          ///< General purpose
 	byte   m5;          ///< General purpose
