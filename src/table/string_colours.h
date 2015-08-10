d818e1779c9ef9770e93c502b90e16caff7254aa
alberth <alberth@openttd.org>
2015-08-10 10:04:14 +0000

    (svn r27378) -Codechange: Fix indentation of the table values, allow Doxygen to link to the TextColour enum.

--- src/table/string_colours.h
+++ src/table/string_colours.h
@@ -9,23 +9,23 @@
 
 /** @file string_colours.h The colour translation of GRF's strings. */
 
-/** Colour mapping for the TextColours. */
+/** Colour mapping for #TextColour. */
 static const byte _string_colourmap[17] = {
-		150, // TC_BLUE
-		 12, // TC_SILVER
-		189, // TC_GOLD
-		184, // TC_RED
-		174, // TC_PURPLE
-		 30, // TC_LIGHT_BROWN
-		195, // TC_ORANGE
-		209, // TC_GREEN
-		 68, // TC_YELLOW
-		 95, // TC_DARK_GREEN
-		 79, // TC_CREAM
-		116, // TC_BROWN
-		 15, // TC_WHITE
-		152, // TC_LIGHT_BLUE
-		  6, // TC_GREY
-		133, // TC_DARK_BLUE
-		  1, // TC_BLACK
+	150, // TC_BLUE
+	 12, // TC_SILVER
+	189, // TC_GOLD
+	184, // TC_RED
+	174, // TC_PURPLE
+	 30, // TC_LIGHT_BROWN
+	195, // TC_ORANGE
+	209, // TC_GREEN
+	 68, // TC_YELLOW
+	 95, // TC_DARK_GREEN
+	 79, // TC_CREAM
+	116, // TC_BROWN
+	 15, // TC_WHITE
+	152, // TC_LIGHT_BLUE
+	  6, // TC_GREY
+	133, // TC_DARK_BLUE
+	  1, // TC_BLACK
 };
