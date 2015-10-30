22e6a2590d3955e86af609e2a1f103b663da25c8
frosch <frosch@openttd.org>
2015-10-30 16:18:03 +0000

    (svn r27418) -Fix [FS#6329] [FS#6379]: Desync due to incorrect storage of segments with different railtype in the YAPF cache. (JGR)

--- src/pathfinder/follow_track.hpp
+++ src/pathfinder/follow_track.hpp
@@ -144,7 +144,12 @@ struct CFollowTrackT
 			 * missing road bit, or inability to connect the
 			 * different bits due to slopes. */
 			if (IsRoadTT() && !IsTram() && TryReverse()) return true;
-			m_err = EC_NO_WAY;
+
+			/* CanEnterNewTile already set a reason.
+			 * Do NOT overwrite it (important for example for EC_RAIL_TYPE).
+			 * Only set a reason if CanEnterNewTile was not called */
+			if (m_new_td_bits == TRACKDIR_BIT_NONE) m_err = EC_NO_WAY;
+
 			return false;
 		}
 		if (!Allow90degTurns()) {
