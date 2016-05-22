13a86497ea74e5f5a3cdfba838f826f7841fd6a4
frosch <frosch@openttd.org>
2016-05-22 10:10:17 +0000

    (svn r27572) -Fix [FS#6442]: Convenience savegame bump to distinguish 1.6 savegames from 1.5 savegames.

--- src/saveload/saveload.cpp
+++ src/saveload/saveload.cpp
@@ -261,9 +261,10 @@
  *  191   26646
  *  192   26700
  *  193   26802
- *  194   26881   1.5.x, 1.6.x
+ *  194   26881   1.5.x, 1.6.0
+ *  195   27572   1.6.x
  */
-extern const uint16 SAVEGAME_VERSION = 194; ///< Current savegame version of OpenTTD.
+extern const uint16 SAVEGAME_VERSION = 195; ///< Current savegame version of OpenTTD.
 
 SavegameType _savegame_type; ///< type of savegame we are loading
 
