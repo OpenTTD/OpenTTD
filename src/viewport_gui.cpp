ae815ecf1b64b411c4fa3723cce92099d49d1b1e
frosch <frosch@openttd.org>
2017-02-05 17:54:46 +0000

    (svn r27743) -Fix [FS#6537]: Extra viewports did not center on the selected tile. (hackalittlebit)

--- src/viewport_gui.cpp
+++ src/viewport_gui.cpp
@@ -71,7 +71,7 @@ public:
 			pt.x = w->viewport->scrollpos_x + w->viewport->virtual_width / 2;
 			pt.y = w->viewport->scrollpos_y + w->viewport->virtual_height / 2;
 		} else {
-			pt = RemapCoords(TileX(tile) * TILE_SIZE + TILE_SIZE / 2, TileY(tile) * TILE_SIZE + TILE_SIZE / 2, TileHeight(tile));
+			pt = RemapCoords(TileX(tile) * TILE_SIZE + TILE_SIZE / 2, TileY(tile) * TILE_SIZE + TILE_SIZE / 2, TilePixelHeight(tile));
 		}
 
 		this->viewport->scrollpos_x = pt.x - this->viewport->virtual_width / 2;
