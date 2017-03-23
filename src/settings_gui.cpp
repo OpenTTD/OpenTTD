c5660b58d97044cd3bbb80a0a03102415020b385
peter1138 <peter1138@openttd.org>
2017-03-23 18:07:04 +0000

    (svn r27819) -Fix (r26990): Update viewport sign dimensions when changing GUI zoom level.

--- src/settings_gui.cpp
+++ src/settings_gui.cpp
@@ -36,6 +36,8 @@
 #include "textfile_gui.h"
 #include "stringfilter_type.h"
 #include "querystring_gui.h"
+#include "signs_func.h"
+#include "station_func.h"
 
 #include <vector>
 
@@ -532,6 +534,9 @@ struct GameOptionsWindow : Window {
 				_gui_zoom = (ZoomLevel)(ZOOM_LVL_OUT_4X - index);
 				UpdateCursorSize();
 				LoadStringWidthTable();
+				UpdateAllSignVirtCoords();
+				UpdateAllTownVirtCoords();
+				UpdateAllStationVirtCoords();
 				break;
 
 			case WID_GO_BASE_GRF_DROPDOWN:
