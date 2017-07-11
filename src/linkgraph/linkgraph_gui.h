3eae9aba797360e2f1e3ff16fa42876db587502d
frosch <frosch@openttd.org>
2017-07-11 19:34:21 +0000

    (svn r27887) -Cleanup: Remove unused/unimplemented prototype (adf88)

--- src/linkgraph/linkgraph_gui.h
+++ src/linkgraph/linkgraph_gui.h
@@ -77,7 +77,6 @@ protected:
 
 	Point GetStationMiddle(const Station *st) const;
 
-	void DrawForwBackLinks(Point pta, StationID sta, Point ptb, StationID stb) const;
 	void AddLinks(const Station *sta, const Station *stb);
 	void DrawLinks(const DrawPixelInfo *dpi) const;
 	void DrawStationDots(const DrawPixelInfo *dpi) const;
