ad0f527726686c9246428d06d1b4214902e8a51e
Charles Pigott <charlespigott@googlemail.com>
2018-04-12 21:31:35 +0100

    Add: List recent news messages in crashlog output

--- src/news_gui.h
+++ src/news_gui.h
@@ -12,7 +12,11 @@
 #ifndef NEWS_GUI_H
 #define NEWS_GUI_H
 
+#include "news_type.h"
+
 void ShowLastNewsMessage();
 void ShowMessageHistory();
+
+extern NewsItem *_oldest_news;
 
 #endif /* NEWS_GUI_H */
