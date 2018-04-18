637a594416156feb530826033f1dd8790ff910fd
Peter Nelson <peter1138@openttd.org>
2018-04-18 17:34:49 +0100

    Add: {PUSH_COLOUR} and {POP_COLOUR} control codes to handle switching colours. This replaces the internal SCC_PREVIOUS_COLOUR swap.

--- src/table/strgen_tables.h
+++ src/table/strgen_tables.h
@@ -60,7 +60,8 @@ static const CmdStruct _cmd_structs[] = {
 	{"GRAY",              EmitSingleChar, SCC_GRAY,               0, -1, C_DONTCOUNT},
 	{"DKBLUE",            EmitSingleChar, SCC_DKBLUE,             0, -1, C_DONTCOUNT},
 	{"BLACK",             EmitSingleChar, SCC_BLACK,              0, -1, C_DONTCOUNT},
-	{"PREVIOUS_COLOUR",   EmitSingleChar, SCC_PREVIOUS_COLOUR,    0, -1, C_DONTCOUNT},
+	{"PUSH_COLOUR",       EmitSingleChar, SCC_PUSH_COLOUR,        0, -1, C_DONTCOUNT},
+	{"POP_COLOUR",        EmitSingleChar, SCC_POP_COLOUR,         0, -1, C_DONTCOUNT},
 
 	{"REV",               EmitSingleChar, SCC_REVISION,           0, -1, C_NONE}, // openttd revision string
 
