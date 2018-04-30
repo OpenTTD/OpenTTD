fa87212a76f168e28d2439cb658fbfe388c19036
Matthijs Kooijman <matthijs@stdin.nl>
2018-04-30 12:54:00 +0200

    Fix: Some spelling errors in printed messages

--- src/core/backup_type.hpp
+++ src/core/backup_type.hpp
@@ -53,7 +53,7 @@ struct Backup {
 		{
 			/* We cannot assert here, as missing restoration is 'normal' when exceptions are thrown.
 			 * Exceptions are especially used to abort world generation. */
-			DEBUG(misc, 0, "%s:%d: Backupped value was not restored!", this->file, this->line);
+			DEBUG(misc, 0, "%s:%d: Backed-up value was not restored!", this->file, this->line);
 			this->Restore();
 		}
 	}
