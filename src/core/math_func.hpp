9d75600ac0137907d5c5ec4f0ff475e2efa2f596
btzy <bernard.14916@gmail.com>
2019-01-12 16:28:43 +0800

    Fix: Round up deltas for smooth scrolling, so target will be reached

--- src/core/math_func.hpp
+++ src/core/math_func.hpp
@@ -346,6 +346,23 @@ static inline int RoundDivSU(int a, uint b)
 	}
 }
 
+/**
+ * Computes (a / b) rounded away from zero.
+ * @param a Numerator
+ * @param b Denominator
+ * @return Quotient, rounded away from zero
+ */
+static inline int DivAwayFromZero(int a, uint b)
+{
+	const int _b = static_cast<int>(b);
+	if (a > 0) {
+		return (a + _b - 1) / _b;
+	} else {
+		/* Note: Behaviour of negative numerator division is truncation toward zero. */
+		return (a - _b + 1) / _b;
+	}
+}
+
 uint32 IntSqrt(uint32 num);
 
 #endif /* MATH_FUNC_HPP */
