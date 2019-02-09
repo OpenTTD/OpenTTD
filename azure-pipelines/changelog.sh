4d5d21be7668ae90d6f3c916ad754b719b2f2af5
Patric Stout <truebrain@openttd.org>
2019-02-09 23:34:06 +0100

    Fix: [AzurePipelines] always list the full changelog since last stable

--- azure-pipelines/changelog.sh
+++ azure-pipelines/changelog.sh
@@ -2,13 +2,9 @@
 
 tag=$(git describe --tags 2>/dev/null)
 
-# If we are a tag, show the part of the changelog that matches the tag.
-# In case of a stable, also show all betas and RCs.
+# If we are a tag, show the part of the changelog till (but excluding) the last stable
 if [ -n "$tag" ]; then
-    grep="."
-    if [ "$(echo $tag | grep '-')" = "" ]; then
-            grep='^[0-9]\.[0-9]\.[0-9][^-]'
-    fi
+    grep='^[0-9]\.[0-9]\.[0-9][^-]'
     next=$(cat changelog.txt | grep '^[0-9]' | awk 'BEGIN { show="false" } // { if (show=="true") print $0; if ($1=="'$tag'") show="true"} ' | grep "$grep" | head -n1 | sed 's/ .*//')
     cat changelog.txt | awk 'BEGIN { show="false" } /^[0-9].[0-9].[0-9]/ { if ($1=="'$next'") show="false"; if ($1=="'$tag'") show="true";} // { if (show=="true") print $0 }'
     exit 0
