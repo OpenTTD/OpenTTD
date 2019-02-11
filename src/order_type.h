f5792279766902a28a4d254ded2a6a4d900c8298
Peter Nelson <peter1138@openttd.org>
2019-02-11 00:29:44 +0000

    Change: Increase maximum number of orders from 64000 to ~16.7m.

--- src/order_type.h
+++ src/order_type.h
@@ -15,7 +15,7 @@
 #include "core/enum_type.hpp"
 
 typedef byte VehicleOrderID;  ///< The index of an order within its current vehicle (not pool related)
-typedef uint16 OrderID;
+typedef uint32 OrderID;
 typedef uint16 OrderListID;
 typedef uint16 DestinationID;
 
@@ -25,7 +25,7 @@ static const VehicleOrderID INVALID_VEH_ORDER_ID = 0xFF;
 static const VehicleOrderID MAX_VEH_ORDER_ID     = INVALID_VEH_ORDER_ID - 1;
 
 /** Invalid order (sentinel) */
-static const OrderID INVALID_ORDER = 0xFFFF;
+static const OrderID INVALID_ORDER = 0xFFFFFF;
 
 /**
  * Maximum number of orders in implicit-only lists before we start searching
