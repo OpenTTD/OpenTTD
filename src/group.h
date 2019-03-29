27a956ba6204e400e82fe10056bf730f286b23fe
peter1138 <peter1138@openttd.org>
2019-03-29 23:24:40 +0000

    Codechange: Replace Group::replace_protection with Group::flags

--- src/group.h
+++ src/group.h
@@ -62,13 +62,17 @@ struct GroupStatistics {
 	static void UpdateAutoreplace(CompanyID company);
 };
 
+enum GroupFlags : uint8 {
+	GF_REPLACE_PROTECTION, ///< If set to true, the global autoreplace has no effect on the group
+};
+
 /** Group data. */
 struct Group : GroupPool::PoolItem<&_group_pool> {
 	std::string name;           ///< Group Name
 	Owner owner;                ///< Group Owner
 	VehicleType vehicle_type;   ///< Vehicle type of the group
 
-	bool replace_protection;    ///< If set to true, the global autoreplace have no effect on the group
+	uint8 flags;                ///< Group flags
 	Livery livery;              ///< Custom colour scheme for vehicles in this group
 	GroupStatistics statistics; ///< NOSAVE: Statistics and caches on the vehicles in the group.
 
