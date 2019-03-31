d7e881eadb4fef6a9e3d2091639338376066dc64
Niels Martin Hansen <nielsm@indvikleren.dk>
2019-03-31 16:08:00 +0200

    Fix: Automatic line breaking of the warning

--- src/misc_gui.cpp
+++ src/misc_gui.cpp
@@ -941,6 +941,7 @@ struct QueryStringWindow : public Window
 {
 	QueryString editbox;    ///< Editbox.
 	QueryStringFlags flags; ///< Flags controlling behaviour of the window.
+	Dimension warning_size; ///< How much space to use for the warning text
 
 	QueryStringWindow(StringID str, StringID caption, uint max_bytes, uint max_chars, WindowDesc *desc, Window *parent, CharSetFilter afilter, QueryStringFlags flags) :
 			Window(desc), editbox(max_bytes, max_chars)
@@ -967,10 +968,27 @@ struct QueryStringWindow : public Window
 		this->flags = flags;
 
 		this->InitNested(WN_QUERY_STRING);
+		this->UpdateWarningStringSize();
 
 		this->parent = parent;
 
 		this->SetFocusedWidget(WID_QS_TEXT);
+	}
+
+	void UpdateWarningStringSize()
+	{
+		NWidgetBase *text_wid = this->GetWidget<NWidgetBase>(WID_QS_WARNING);
+
+		if (this->flags & QSF_PASSWORD) {
+			assert(this->nested_root->smallest_x > 0);
+			this->warning_size.width = this->nested_root->current_x - (WD_FRAMETEXT_LEFT + WD_FRAMETEXT_RIGHT + WD_FRAMERECT_LEFT + WD_FRAMERECT_RIGHT);
+			this->warning_size.height = GetStringHeight(STR_WARNING_PASSWORD_SECURITY, this->warning_size.width);
+			this->warning_size.height += WD_FRAMETEXT_TOP + WD_FRAMETEXT_BOTTOM + WD_FRAMERECT_TOP + WD_FRAMERECT_BOTTOM;
+		} else {
+			this->warning_size = Dimension{ 0, 0 };
+		}
+
+		this->ReInit();
 	}
 
 	void UpdateWidgetSize(int widget, Dimension *size, const Dimension &padding, Dimension *fill, Dimension *resize) override
@@ -982,14 +1000,8 @@ struct QueryStringWindow : public Window
 			size->width = 0;
 		}
 
-		if (widget == WID_QS_WARNINGBOX && (this->flags & QSF_PASSWORD) == 0) {
-			/* We don't want this widget to show! */
-			fill->width = 0;
-			resize->width = 0;
-			size->width = 0;
-		}
 		if (widget == WID_QS_WARNING) {
-			*size = GetStringBoundingBox(STR_WARNING_PASSWORD_SECURITY);
+			*size = this->warning_size;
 		}
 	}
 
@@ -997,7 +1009,11 @@ struct QueryStringWindow : public Window
 	{
 		if (widget != WID_QS_WARNING) return;
 
-		DrawStringMultiLine(r.left, r.right, r.top, r.bottom, STR_WARNING_PASSWORD_SECURITY, TC_FROMSTRING, SA_CENTER);
+		if (this->flags & QSF_PASSWORD) {
+			DrawStringMultiLine(r.left + WD_FRAMERECT_LEFT + WD_FRAMETEXT_LEFT, r.right - WD_FRAMETEXT_RIGHT - WD_FRAMERECT_RIGHT,
+				r.top + WD_FRAMERECT_TOP + WD_FRAMETEXT_TOP, r.bottom - WD_FRAMERECT_BOTTOM - WD_FRAMETEXT_BOTTOM,
+				STR_WARNING_PASSWORD_SECURITY, TC_FROMSTRING, SA_CENTER);
+		}
 	}
 
 	void SetStringParameters(int widget) const override
@@ -1054,9 +1070,7 @@ static const NWidgetPart _nested_query_string_widgets[] = {
 	NWidget(WWT_PANEL, COLOUR_GREY),
 		NWidget(WWT_EDITBOX, COLOUR_GREY, WID_QS_TEXT), SetMinimalSize(256, 12), SetFill(1, 1), SetPadding(2, 2, 2, 2),
 	EndContainer(),
-	NWidget(WWT_PANEL, COLOUR_GREY, WID_QS_WARNINGBOX),
-		NWidget(WWT_LABEL, COLOUR_GREY, WID_QS_WARNING), SetPadding(WD_FRAMETEXT_TOP, WD_FRAMETEXT_RIGHT, WD_FRAMETEXT_BOTTOM, WD_FRAMETEXT_LEFT), SetFill(0, 1),
-	EndContainer(),
+	NWidget(WWT_PANEL, COLOUR_GREY, WID_QS_WARNING), EndContainer(),
 	NWidget(NWID_HORIZONTAL, NC_EQUALSIZE),
 		NWidget(WWT_TEXTBTN, COLOUR_GREY, WID_QS_DEFAULT), SetMinimalSize(87, 12), SetFill(1, 1), SetDataTip(STR_BUTTON_DEFAULT, STR_NULL),
 		NWidget(WWT_TEXTBTN, COLOUR_GREY, WID_QS_CANCEL), SetMinimalSize(86, 12), SetFill(1, 1), SetDataTip(STR_BUTTON_CANCEL, STR_NULL),
