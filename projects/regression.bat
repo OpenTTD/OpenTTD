12915b3f091bc17a109e33fba59895cb904dac61
Patric Stout <truebrain@openttd.org>
2019-04-07 11:42:20 +0200

    Remove: all existing project files
    This prepares for the switch to CMake, which takes over all current
    exisiting forms of project files.

--- projects/regression.bat
+++
@@ -1,6 +0,0 @@
-cd ..\bin
-editbin /nologo /subsystem:console openttd.exe
-cscript /nologo ai\regression\run.vbs
-set RESULT=%ERRORLEVEL%
-editbin /nologo /subsystem:windows openttd.exe
-exit %RESULT%
