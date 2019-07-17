ed6fc9dbc77794b7552bc328432e644032110fdc
Niels Martin Hansen <nielsm@indvikleren.dk>
2019-07-17 22:08:41 +0200

    Fix: Narrowing cast in one storybook command
    CompanyID is 8 bit wide, so this incorrect cast would make it impossible to create story page elements for pages past 255.

--- src/story.cpp
+++ src/story.cpp
@@ -154,7 +154,7 @@ CommandCost CmdCreateStoryPageElement(TileIndex tile, DoCommandFlag flags, uint3
 {
 	if (!StoryPageElement::CanAllocateItem()) return CMD_ERROR;
 
-	StoryPageID page_id = (CompanyID)GB(p1, 0, 16);
+	StoryPageID page_id = (StoryPageID)GB(p1, 0, 16);
 	StoryPageElementType type = Extract<StoryPageElementType, 16, 8>(p1);
 
 	/* Allow at most 128 elements per page. */
