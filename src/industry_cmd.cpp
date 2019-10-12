ac2111873626263ae5492bd2c18ec30f69e84158
abmyii <52673001+abmyii@users.noreply.github.com>
2019-10-12 09:16:16 +0100

    Fix #7703: Prevent sounds being produced by inactive industries (#7752)

--- src/industry_cmd.cpp
+++ src/industry_cmd.cpp
@@ -1124,11 +1124,16 @@ static void ProduceIndustryGoods(Industry *i)
 	/* play a sound? */
 	if ((i->counter & 0x3F) == 0) {
 		uint32 r;
-		uint num;
-		if (Chance16R(1, 14, r) && (num = indsp->number_of_sounds) != 0 && _settings_client.sound.ambient) {
-			SndPlayTileFx(
-				(SoundFx)(indsp->random_sounds[((r >> 16) * num) >> 16]),
-				i->location.tile);
+		if (Chance16R(1, 14, r) && indsp->number_of_sounds != 0 && _settings_client.sound.ambient) {
+			for (size_t j = 0; j < lengthof(i->last_month_production); j++) {
+				if (i->last_month_production[j] > 0) {
+					/* Play sound since last month had production */
+					SndPlayTileFx(
+						(SoundFx)(indsp->random_sounds[((r >> 16) * indsp->number_of_sounds) >> 16]),
+						i->location.tile);
+					break;
+				}
+			}
 		}
 	}
 
