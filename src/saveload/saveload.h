e4f08f2880285a16fb8359f74581afd3f194c02b
Jack Baron <jackmbaron@gmail.com>
2019-10-18 03:26:05 +0100

    Fix: Bump savegame version

--- src/saveload/saveload.h
+++ src/saveload/saveload.h
@@ -302,6 +302,7 @@ enum SaveLoadVersion : uint16 {
 
 	SLV_SCRIPT_MEMLIMIT,                    ///< 215  PR#7516 Limit on AI/GS memory consumption.
 	SLV_MULTITILE_DOCKS,                    ///< 216  PR#7380 Multiple docks per station.
+	SLV_TRADING_AGE,                        ///< 217  PR#7780 Configurable company trading age.
 
 	SL_MAX_VERSION,                         ///< Highest possible saveload version
 };
