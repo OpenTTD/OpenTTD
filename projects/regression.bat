7b69d1332bce16e8187bee2abcbb838865a1ef7b
Charles Pigott <charlespigott@googlemail.com>
2020-01-03 18:30:03 +0000

    Add: [AzurePipelines] Savegame regression tests to the CI pipeline

--- projects/regression.bat
+++ projects/regression.bat
@@ -1,6 +1,12 @@
-cd ..\bin
-editbin /nologo /subsystem:console openttd.exe
+cd ..
+editbin /nologo /subsystem:console bin\openttd.exe
+cd bin
 cscript /nologo ai\regression\run.vbs
 set RESULT=%ERRORLEVEL%
-editbin /nologo /subsystem:windows openttd.exe
+cd ..
+if /i "%RESULT%" EQU "0" (
+	cscript /nologo tests\regression\run.vbs
+	set RESULT=%ERRORLEVEL%
+)
+editbin /nologo /subsystem:windows bin\openttd.exe
 exit %RESULT%
