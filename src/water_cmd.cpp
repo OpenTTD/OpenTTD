75ff59dd66861599614b6177c34fde40bba5cc00
Niels Martin Hansen <nielsm@indvikleren.dk>
2020-01-11 19:38:03 +0100

    Change: Use square of water depth for clear cost multiplier

--- src/water_cmd.cpp
+++ src/water_cmd.cpp
@@ -616,8 +616,9 @@ static CommandCost ClearTile_Water(TileIndex tile, DoCommandFlags flags)
 				if (ret.Failed()) return ret;
 			}
 
-			/* Deeper water is more expensive to clear */
-			const int cost_multiplier = GetWaterDepth(tile) + 1;
+			/* Deeper water is much more expensive to clear */
+			const int real_depth = std::max<int>(GetWaterDepth(tile), 1);
+			const int cost_multiplier = real_depth * real_depth;
 
 			if (flags.Test(DoCommandFlag::Execute)) {
 				if (IsCanal(tile) && Company::IsValidID(owner)) {
