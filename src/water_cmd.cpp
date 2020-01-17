30fe0015e2152e0cada8997e760271f31a001a1a
Samu <dj_samu@hotmail.com>
2020-01-17 19:58:07 +0000

    Fix #7944: Demolishing locks built on rivers didn't always restore the river

--- src/water_cmd.cpp
+++ src/water_cmd.cpp
@@ -301,7 +301,7 @@ static CommandCost DoBuildLock(TileIndex tile, DiagDirection dir, DoCommandFlag 
 	if (ret.Failed()) return ret;
 
 	/* middle tile */
-	WaterClass wc_middle = IsWaterTile(tile) ? GetWaterClass(tile) : WATER_CLASS_CANAL;
+	WaterClass wc_middle = HasTileWaterGround(tile) ? GetWaterClass(tile) : WATER_CLASS_CANAL;
 	ret = DoCommand(tile, 0, 0, flags, CMD_LANDSCAPE_CLEAR);
 	if (ret.Failed()) return ret;
 	cost.AddCost(ret);
