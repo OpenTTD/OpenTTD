5c9a2d3e370cd0daa584d093818ca4d7ce8309b9
Niels Martin Hansen <nielsm@indvikleren.dk>
2020-01-19 11:49:05 +0100

    Remove: Industry var 0x72 again, it's a bad way of doing water depth for industries

--- src/newgrf_industries.cpp
+++ src/newgrf_industries.cpp
@@ -340,13 +340,6 @@ static uint32_t GetCountAndDistanceOfClosestInstance(const ResolverObject &objec
 			}
 		}
 
-		/* Water depth (byte) at nearby tile, if it's water, otherwise 0 */
-		case 0x72: {
-			if (this->tile == INVALID_TILE) break;
-			TileIndex tile = GetNearbyTile(parameter, this->tile);
-			return IsWaterTile(this->tile) ? GetWaterDepth(this->tile) : 0;
-		}
-
 
 		case 0x6E:
 		case 0x6F: {
