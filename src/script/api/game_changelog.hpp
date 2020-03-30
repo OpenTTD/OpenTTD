6311c0fcb61c0a675d4091846770f19b368e907a
Charles Pigott <charlespigott@googlemail.com>
2020-03-30 20:08:29 +0100

    Doc: Prepare for 1.10.0 release

--- src/script/api/game_changelog.hpp
+++ src/script/api/game_changelog.hpp
@@ -15,8 +15,6 @@
  *
  * \b 1.10.0
  *
- * This version is not yet released. The following changes are not set in stone yet.
- *
  * API additions:
  * \li GSVehicle::BuildVehicleWithRefit
  * \li GSVehicle::GetBuildWithRefitCapacity
