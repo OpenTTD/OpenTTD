1c95b37d26195e42c2c06fd340413d15bd779ba0
SamuXarick <43006711+SamuXarick@users.noreply.github.com>
2020-03-31 15:47:29 +0100

    Doc: [Script] Make it clear random_deviation and CONFIG_RANDOM range upper bounds are inclusive

--- src/script/api/script_info_docs.hpp
+++ src/script/api/script_info_docs.hpp
@@ -203,7 +203,7 @@ public:
 	/** Miscellaneous flags for Script settings. */
 	enum ScriptConfigFlags {
 		CONFIG_NONE,      ///< Normal setting.
-		CONFIG_RANDOM,    ///< When randomizing the Script, pick any value between min_value and max_value.
+		CONFIG_RANDOM,    ///< When randomizing the Script, pick any value between min_value and max_value (inclusive).
 		CONFIG_BOOLEAN,   ///< This value is a boolean (either 0 (false) or 1 (true) ).
 		CONFIG_INGAME,    ///< This setting can be changed while the Script is running.
 		CONFIG_DEVELOPER, ///< This setting will only be visible when the Script development tools are active.
@@ -230,8 +230,8 @@ public:
 	 *  - custom_value The default value if the custom difficulty level
 	 *    is selected. Required.
 	 *  - random_deviation If this property has a nonzero value, then the
-	 *    actual value of the setting in game will be
-	 *    user_configured_value + random(-random_deviation, random_deviation).
+	 *    actual value of the setting in game will be randomized in the range
+	 *    [user_configured_value - random_deviation, user_configured_value + random_deviation] (inclusive).
 	 *    Not allowed if the CONFIG_RANDOM flag is set, otherwise optional.
 	 *  - step_size The increase/decrease of the value every time the user
 	 *    clicks one of the up/down arrow buttons. Optional, default is 1.
