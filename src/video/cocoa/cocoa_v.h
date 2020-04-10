8f7788795b3a67b038184d34dc9fcf2c78253d13
Michael Lutz <michi@icosahedron.de>
2020-04-10 14:21:54 +0200

    Remove: [OSX] QuickDraw video subdriver used for OSX versions up to 10.4.
    Since the C++11 move, getting OpenTTD to compile and run for anything
    below 10.7 basically requires building a custom compiler and libc++.
    Also, the QuickDraw subdriver crashes on more modern OSX version. While this
    is fixable, keeping the driver around is probably pointless.

--- src/video/cocoa/cocoa_v.h
+++ src/video/cocoa/cocoa_v.h
@@ -191,10 +191,6 @@ extern CocoaSubdriver *_cocoa_subdriver;
 
 CocoaSubdriver *QZ_CreateFullscreenSubdriver(int width, int height, int bpp);
 
-#ifdef ENABLE_COCOA_QUICKDRAW
-CocoaSubdriver *QZ_CreateWindowQuickdrawSubdriver(int width, int height, int bpp);
-#endif
-
 #ifdef ENABLE_COCOA_QUARTZ
 #if MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_4
 CocoaSubdriver *QZ_CreateWindowQuartzSubdriver(int width, int height, int bpp);
