5cb9780080bb6e2374e446faf5386951cf6ff07a
Jonathan G Rennison <j.g.rennison@gmail.com>
2020-05-17 22:13:08 +0100

    Fix: Incorrect save/load array size of Town::cargo_accepted
    In 11ab3c4e the number of cargo types was changed from 32 to 64.
    The save/load of Town::cargo_accepted was not updated, such that
    only half of the data structure is saved/loaded in savegame versions
    199 to 218.
    Discard and regenerate data from all savegame versions prior to 219.

--- src/saveload/saveload.h
+++ src/saveload/saveload.h
@@ -302,6 +302,7 @@ enum SaveLoadVersion : uint16 {
 	SLV_MULTITILE_DOCKS,                    ///< 216  PR#7380 Multiple docks per station.
 	SLV_TRADING_AGE,                        ///< 217  PR#7780 Configurable company trading age.
 	SLV_ENDING_YEAR,                        ///< 218  PR#7747 v1.10 Configurable ending year.
+	SLV_FIX_TOWN_ACCEPTANCE,                ///< 219  PR#8157 Fix Town::cargo_accepted savegame format.
 
 	SL_MAX_VERSION,                         ///< Highest possible saveload version
 };
