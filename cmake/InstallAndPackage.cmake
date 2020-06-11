60d90985cc7759d2041e059795433133f58c72fc
glx22 <glx@openttd.org>
2020-06-11 21:57:14 +0200

    Add: [CMake] Allow renaming of openttd binary

--- cmake/InstallAndPackage.cmake
+++ cmake/InstallAndPackage.cmake
@@ -3,7 +3,7 @@ include(GNUInstallDirs)
 # If requested, use FHS layout; otherwise fall back to a flat layout.
 if (OPTION_INSTALL_FHS)
     set(BINARY_DESTINATION_DIR "${CMAKE_INSTALL_BINDIR}")
-    set(DATA_DESTINATION_DIR "${CMAKE_INSTALL_DATADIR}/openttd")
+    set(DATA_DESTINATION_DIR "${CMAKE_INSTALL_DATADIR}/${BINARY_NAME}")
     set(DOCS_DESTINATION_DIR "${CMAKE_INSTALL_DOCDIR}")
     set(MAN_DESTINATION_DIR "${CMAKE_INSTALL_MANDIR}")
 else (OPTION_INSTALL_FHS)
@@ -41,7 +41,7 @@ install(FILES
 # file with little context to what it is.
 if (OPTION_INSTALL_FHS)
     set(MAN_SOURCE_FILE ${CMAKE_SOURCE_DIR}/docs/openttd.6)
-    set(MAN_BINARY_FILE ${CMAKE_BINARY_DIR}/docs/openttd.6)
+    set(MAN_BINARY_FILE ${CMAKE_BINARY_DIR}/docs/${BINARY_NAME}.6)
     install(CODE
             "
                 execute_process(COMMAND ${CMAKE_COMMAND} -E copy ${MAN_SOURCE_FILE} ${MAN_BINARY_FILE})
