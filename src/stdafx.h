e330fa6d2193986ebbb47402fc6138abceda9747
TechGeekNZ <git@tech.geek.nz>
2020-06-26 15:00:21 +1200

    Update: Make stdafx.h comply with coding style.
    Hash mark must go in first column, as per https://wiki.openttd.org/Coding_style#Other_important_rules

--- src/stdafx.h
+++ src/stdafx.h
@@ -431,11 +431,11 @@ assert_compile(SIZE_MAX >= UINT32_MAX);
 #endif /* __APPLE__ */
 
 #ifdef __GNUC__
-#define likely(x)       __builtin_expect(!!(x), 1)
-#define unlikely(x)     __builtin_expect(!!(x), 0)
+#	define likely(x)       __builtin_expect(!!(x), 1)
+#	define unlikely(x)     __builtin_expect(!!(x), 0)
 #else
-#define likely(x)       (x)
-#define unlikely(x)     (x)
+#	define likely(x)       (x)
+#	define unlikely(x)     (x)
 #endif
 
 void NORETURN CDECL usererror(const char *str, ...) WARN_FORMAT(1, 2);
