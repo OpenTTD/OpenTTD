f51f1b87a5572cfb4e82a2982368d7fe7c358849
Foxar <mostek3pl@gmail.com>
2020-12-03 19:39:50 +0100

    Update: Updated printfs for review of PR 8346

--- src/console_cmds.cpp
+++ src/console_cmds.cpp
@@ -919,10 +919,9 @@ DEF_CONSOLE_CMD(ConNetworkConnect)
 	IConsolePrintF(CC_DEFAULT, "Connecting to %s...", ip);
 	if (company != nullptr) {
 		join_as = (CompanyID)atoi(company);
-		IConsolePrintF(CC_DEFAULT, "Connecting as company id %s ...", company);
+		IConsolePrintF(CC_DEFAULT, "Connecting as company id %d ...", join_as);
 		/* Check if we have a valid company id! */
 		if(!Company::IsValidID(join_as) && join_as != COMPANY_SPECTATOR) {
-			IConsolePrintF(CC_ERROR, "Company does not exist. Company-id must be between 1 and %d.", MAX_COMPANIES);
 			return true;
 		}
 		IConsolePrintF(CC_DEFAULT, "    company-no: %d", join_as);
