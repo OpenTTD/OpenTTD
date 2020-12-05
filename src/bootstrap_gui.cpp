6ad3cca4caff8f1a45f96c69e1193a97dc072cc3
Patric Stout <truebrain@openttd.org>
2020-12-05 15:03:16 +0100

    Fix a49fdb7ebb: bootstrap crash when trying to load new baseset (#8353)
    Using nullptr as "name" crashes on "name.empty()". Use an empty
    string instead.

--- src/bootstrap_gui.cpp
+++ src/bootstrap_gui.cpp
@@ -253,7 +253,7 @@ bool HandleBootstrap()
 	if (_exit_game) return false;
 
 	/* Try to probe the graphics. Should work this time. */
-	if (!BaseGraphics::SetSet(nullptr)) goto failure;
+	if (!BaseGraphics::SetSet({})) goto failure;
 
 	/* Finally we can continue heading for the menu. */
 	_game_mode = GM_MENU;
