982cd6199945cd9f122a41e339f08a73a599c2b4
Patric Stout <truebrain@openttd.org>
2020-12-06 20:25:07 +0100

    Fix: useless warning with -snull and no BaseSounds available
    If I explicitly tell the system I do not want sound, I still get
    presented a nice message I do not have any BaseSounds available
    on my system, and that I should download one to enjoy sound. Well,
    let me tell you, with "-snull" that is really really not going to
    help. So please, be quiet, and let me enjoy the game without
    "boooooo" and "DING DING DING".
    
    Thank you.

--- src/sound/sound_driver.hpp
+++ src/sound/sound_driver.hpp
@@ -19,6 +19,17 @@ public:
 	virtual void MainLoop() {}
 
 	/**
+	 * Whether the driver has an output from which the user can hear sound.
+	 * Or in other words, whether we should warn the user if no soundset is
+	 * loaded and that loading one would fix the sound problems.
+	 * @return True for all drivers except null.
+	 */
+	virtual bool HasOutput() const
+	{
+		return true;
+	}
+
+	/**
 	 * Get the currently active instance of the sound driver.
 	 */
 	static SoundDriver *GetInstance() {
