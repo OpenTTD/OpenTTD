373ab86ca6965e6a3a25324686040364f655d34e
Patric Stout <truebrain@openttd.org>
2020-12-25 14:44:28 +0100

    Codechange: move block a bit lower to increase readability
    It was rather confusing that "library_name" was calculated, and
    then not used to do the FindLibrary() call. Flipping those two
    blocks around makes it a bit more sane to read.

--- src/script/api/script_controller.cpp
+++ src/script/api/script_controller.cpp
@@ -111,17 +111,17 @@ ScriptController::~ScriptController()
 	Squirrel *engine = ScriptObject::GetActiveInstance()->engine;
 	HSQUIRRELVM vm = engine->GetVM();
 
-	/* Internally we store libraries as 'library.version' */
-	char library_name[1024];
-	seprintf(library_name, lastof(library_name), "%s.%d", library, version);
-	strtolower(library_name);
-
 	ScriptInfo *lib = ScriptObject::GetActiveInstance()->FindLibrary(library, version);
 	if (lib == nullptr) {
 		char error[1024];
 		seprintf(error, lastof(error), "couldn't find library '%s' with version %d", library, version);
 		throw sq_throwerror(vm, error);
 	}
+
+	/* Internally we store libraries as 'library.version' */
+	char library_name[1024];
+	seprintf(library_name, lastof(library_name), "%s.%d", library, version);
+	strtolower(library_name);
 
 	/* Get the current table/class we belong to */
 	HSQOBJECT parent;
