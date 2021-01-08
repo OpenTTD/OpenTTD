9fbee3ab8e0b6b4fa289e36354b765e6ff039664
milek7 <me@milek7.pl>
2021-01-08 01:57:58 +0100

    Codechange: Check if access __attribute__ is supported before trying to use it.

--- src/stdafx.h
+++ src/stdafx.h
@@ -136,8 +136,12 @@
 #	endif
 #endif /* __GNUC__ || __clang__ */
 
-#if defined(__GNUC__)
-#	define NOACCESS(args) __attribute__ ((access (none, args)))
+#if defined __has_attribute
+#	if __has_attribute (access)
+#		define NOACCESS(args) __attribute__ ((access (none, args)))
+#	else
+#		define NOACCESS(args)
+#	endif
 #else
 #	define NOACCESS(args)
 #endif
