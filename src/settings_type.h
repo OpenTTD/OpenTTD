7eeb455cf96dbac568ce2fc0b6e700844f0aab31
Patric Stout <truebrain@openttd.org>
2021-02-17 15:31:09 +0100

    Feature: configurable refresh-rate and change default to 60fps
    Most modern games run on 60 fps, and for good reason. This gives
    a much smoother experiences.
    
    As some people have monitors that can do 144Hz or even 240Hz, allow
    people to configure the refresh rate. Of course, the higher you
    set the value, the more time the game spends on drawing pixels
    instead of simulating the game, which has an effect on simulation
    speed.
    
    The simulation will still always run at 33.33 fps, and is not
    influences by this setting.

--- src/settings_type.h
+++ src/settings_type.h
@@ -147,6 +147,7 @@ struct GUISettings {
 	byte   starting_colour;                  ///< default color scheme for the company to start a new game with
 	bool   show_newgrf_name;                 ///< Show the name of the NewGRF in the build vehicle window
 	bool   auto_remove_signals;              ///< automatically remove signals when in the way during rail construction
+	uint16 refresh_rate;                     ///< How often we refresh the screen (time between draw-ticks).
 
 	uint16 console_backlog_timeout;          ///< the minimum amount of time items should be in the console backlog before they will be removed in ~3 seconds granularity.
 	uint16 console_backlog_length;           ///< the minimum amount of items in the console backlog before items will be removed.
--- src/video/sdl2_v.cpp
+++ src/video/sdl2_v.cpp
@@ -778,11 +778,11 @@ void VideoDriver_SDL::LoopOnce()
 
 	if (cur_ticks >= next_game_tick || (_fast_forward && !_pause_mode)) {
 		if (_fast_forward && !_pause_mode) {
-			next_game_tick = cur_ticks + std::chrono::milliseconds(MILLISECONDS_PER_TICK);
+			next_game_tick = cur_ticks + this->GetGameInterval();
 		} else {
-			next_game_tick += std::chrono::milliseconds(MILLISECONDS_PER_TICK);
+			next_game_tick += this->GetGameInterval();
 			/* Avoid next_game_tick getting behind more and more if it cannot keep up. */
-			if (next_game_tick < cur_ticks - std::chrono::milliseconds(ALLOWED_DRIFT * MILLISECONDS_PER_TICK)) next_game_tick = cur_ticks;
+			if (next_game_tick < cur_ticks - ALLOWED_DRIFT * this->GetGameInterval()) next_game_tick = cur_ticks;
 		}
 
 		/* The gameloop is the part that can run asynchronously. The rest
@@ -793,9 +793,9 @@ void VideoDriver_SDL::LoopOnce()
 	}
 
 	if (cur_ticks >= next_draw_tick) {
-		next_draw_tick += std::chrono::milliseconds(MILLISECONDS_PER_TICK);
+		next_draw_tick += this->GetDrawInterval();
 		/* Avoid next_draw_tick getting behind more and more if it cannot keep up. */
-		if (next_draw_tick < cur_ticks - std::chrono::microseconds(ALLOWED_DRIFT * MILLISECONDS_PER_TICK)) next_draw_tick = cur_ticks;
+		if (next_draw_tick < cur_ticks - ALLOWED_DRIFT * this->GetDrawInterval()) next_draw_tick = cur_ticks;
 
 		bool old_ctrl_pressed = _ctrl_pressed;
 
