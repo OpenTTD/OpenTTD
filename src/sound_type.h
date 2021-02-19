114e453ad46dee206a7fd78772828c9ae7672d63
frosch <frosch@openttd.org>
2021-02-19 22:30:01 +0100

    Codechange: rename sound ids to make more sense.

--- src/sound_type.h
+++ src/sound_type.h
@@ -36,79 +36,79 @@ struct SoundEntry {
  */
 enum SoundFx {
 	SND_BEGIN = 0,
-	SND_02_SPLAT_WATER = 0,                ///< Water construction.
-	SND_03_FACTORY_WHISTLE,
-	SND_04_TRAIN,
-	SND_05_TRAIN_THROUGH_TUNNEL,
-	SND_06_SHIP_HORN,
-	SND_07_FERRY_HORN,
-	SND_08_PLANE_TAKE_OFF,
-	SND_09_JET,
-	SND_0A_TRAIN_HORN,
-	SND_0B_MINING_MACHINERY,
-	SND_0C_ELECTRIC_SPARK,
-	SND_0D_STEAM,
-	SND_0E_LEVEL_CROSSING,
-	SND_0F_VEHICLE_BREAKDOWN,
-	SND_10_TRAIN_BREAKDOWN,
-	SND_11_CRASH,
-	SND_12_EXPLOSION,                      // 16 == 0x10
-	SND_13_BIG_CRASH,
-	SND_14_CASHTILL,
-	SND_15_BEEP,                           // 19 == 0x13
-	SND_16_MORSE,                          // 20 == 0x14
-	SND_17_SKID_PLANE,
-	SND_18_HELICOPTER,
-	SND_19_BUS_START_PULL_AWAY,
-	SND_1A_BUS_START_PULL_AWAY_WITH_HORN,
-	SND_1B_TRUCK_START,
-	SND_1C_TRUCK_START_2,
-	SND_1D_APPLAUSE,
-	SND_1E_OOOOH,
-	SND_1F_SPLAT_OTHER,                    ///< Non-water non-rail construction.
-	SND_20_SPLAT_RAIL,                     ///< Rail construction.
-	SND_21_JACKHAMMER,
-	SND_22_CAR_HORN,
-	SND_23_CAR_HORN_2,
-	SND_24_SHEEP,
-	SND_25_COW,
-	SND_26_HORSE,
-	SND_27_BLACKSMITH_ANVIL,
-	SND_28_SAWMILL,                        // 38 == 0x26 !
-	SND_00_GOOD_YEAR,                      // 39 == 0x27 !
-	SND_01_BAD_YEAR,                       // 40 == 0x28 !
-	SND_29_RIP,                            // 41 == 0x29 !
-	SND_2A_EXTRACT_AND_POP,
-	SND_2B_COMEDY_HIT,
-	SND_2C_MACHINERY,
-	SND_2D_RIP_2,
-	SND_2E_EXTRACT_AND_POP,
-	SND_2F_POP,
-	SND_30_CARTOON_SOUND,
-	SND_31_EXTRACT,
-	SND_32_POP_2,
-	SND_33_PLASTIC_MINE,
-	SND_34_WIND,
-	SND_35_COMEDY_BREAKDOWN,
-	SND_36_CARTOON_CRASH,
-	SND_37_BALLOON_SQUEAK,
-	SND_38_CHAINSAW,
-	SND_39_HEAVY_WIND,
-	SND_3A_COMEDY_BREAKDOWN_2,
-	SND_3B_JET_OVERHEAD,
-	SND_3C_COMEDY_CAR,
-	SND_3D_ANOTHER_JET_OVERHEAD,
-	SND_3E_COMEDY_CAR_2,
-	SND_3F_COMEDY_CAR_3,
-	SND_40_COMEDY_CAR_START_AND_PULL_AWAY,
-	SND_41_MAGLEV,
-	SND_42_LOON_BIRD,
-	SND_43_LION,
-	SND_44_MONKEYS,
-	SND_45_PLANE_CRASHING,
-	SND_46_PLANE_ENGINE_SPUTTERING,
-	SND_47_MAGLEV_2,
-	SND_48_DISTANT_BIRD,                    // 72 == 0x48
+	SND_02_CONSTRUCTION_WATER = 0,         ///<  0 == 0x00  Construction: water infrastructure
+	SND_03_FACTORY,                        ///<  1 == 0x01  Industry producing: factory: whistle
+	SND_04_DEPARTURE_STEAM,                ///<  2 == 0x02  Station departure: steam engine
+	SND_05_TRAIN_THROUGH_TUNNEL,           ///<  3 == 0x03  Train enters tunnel
+	SND_06_DEPARTURE_CARGO_SHIP,           ///<  4 == 0x04  Station departure: cargo ships
+	SND_07_DEPARTURE_FERRY,                ///<  5 == 0x05  Station departure: passenger ships
+	SND_08_TAKEOFF_PROPELLER,              ///<  6 == 0x06  Takeoff: propeller plane (non-toyland)
+	SND_09_TAKEOFF_JET,                    ///<  7 == 0x07  Takeoff: regular jet plane
+	SND_0A_DEPARTURE_TRAIN,                ///<  8 == 0x08  Station departure: diesel and electric engine
+	SND_0B_MINE,                           ///<  9 == 0x09  Industry animation: coal/copper/gold mine: headgear
+	SND_0C_POWER_STATION,                  ///< 10 == 0x0A  Industry animation: power station: spark
+	SND_0D_UNUSED,                         ///< 11 == 0x0B  unused (1)
+	SND_0E_LEVEL_CROSSING,                 ///< 12 == 0x0C  Train passes through level crossing
+	SND_0F_BREAKDOWN_ROADVEHICLE,          ///< 13 == 0x0D  Breakdown: road vehicle (non-toyland)
+	SND_10_BREAKDOWN_TRAIN_SHIP,           ///< 14 == 0x0E  Breakdown: train or ship (non-toyland)
+	SND_11_UNUSED,                         ///< 15 == 0x0F  unused (2)
+	SND_12_EXPLOSION,                      ///< 16 == 0x10  Destruction, crashes, disasters, ...
+	SND_13_TRAIN_COLLISION,                ///< 15 == 0x11  Train+train crash
+	SND_14_CASHTILL,                       ///< 18 == 0x12  Income from cargo delivery
+	SND_15_BEEP,                           ///< 19 == 0x13  GUI button click
+	SND_16_NEWS_TICKER,                    ///< 20 == 0x14  News ticker
+	SND_17_SKID_PLANE,                     ///< 21 == 0x15  Plane landing / touching ground
+	SND_18_TAKEOFF_HELICOPTER,             ///< 22 == 0x16  Takeoff: helicopter
+	SND_19_DEPARTURE_OLD_RV_1,             ///< 23 == 0x17  Station departure: truck and old bus (1) (non-toyland)
+	SND_1A_DEPARTURE_OLD_RV_2,             ///< 24 == 0x18  Station departure: truck and old bus (2) (random variation of SND_19_DEPARTURE_OLD_RV_1) (non-toyland)
+	SND_1B_DEPARTURE_MODERN_BUS,           ///< 25 == 0x19  Station departure: modern bus (non-toyland)
+	SND_1C_DEPARTURE_OLD_BUS,              ///< 26 == 0x1A  Station departure: old bus (non-toyland)
+	SND_1D_APPLAUSE,                       ///< 27 == 0x1B  News: first vehicle at station
+	SND_1E_NEW_ENGINE,                     ///< 28 == 0x1C  News: new engine available
+	SND_1F_CONSTRUCTION_OTHER,             ///< 29 == 0x1D  Construction: other (non-water, non-rail, non-bridge)
+	SND_20_CONSTRUCTION_RAIL,              ///< 30 == 0x1E  Construction: rail infrastructure
+	SND_21_ROAD_WORKS,                     ///< 31 == 0x1F  Road reconstruction animation
+	SND_22_UNUSED,                         ///< 32 == 0x20  unused (3)
+	SND_23_UNUSED,                         ///< 33 == 0x21  unused (4)
+	SND_24_FARM_1,                         ///< 34 == 0x22  Industry producing: farm (1): sheep
+	SND_25_FARM_2,                         ///< 35 == 0x23  Industry producing: farm (2): cow
+	SND_26_FARM_3,                         ///< 36 == 0x24  Industry producing: farm (3): horse
+	SND_27_CONSTRUCTION_BRIDGE,            ///< 37 == 0x25  Construction: bridge
+	SND_28_SAWMILL,                        ///< 38 == 0x26  Industry producing: sawmill
+	SND_00_GOOD_YEAR,                      ///< 39 == 0x27  New year: performance improved
+	SND_01_BAD_YEAR,                       ///< 40 == 0x28  New year: performance declined
+	SND_29_SUGAR_MINE_2,                   ///< 41 == 0x29  Industry animation: sugar mine (2): shaking sieve
+	SND_2A_TOY_FACTORY_3,                  ///< 42 == 0x2A  Industry animation: toy factory (3): eject product
+	SND_2B_TOY_FACTORY_2,                  ///< 43 == 0x2B  Industry animation: toy factory (2): stamp product
+	SND_2C_TOY_FACTORY_1,                  ///< 44 == 0x2C  Industry animation: toy factory (1): conveyor belt
+	SND_2D_SUGAR_MINE_1,                   ///< 45 == 0x2D  Industry animation: sugar mine (1): shaking sieve
+	SND_2E_BUBBLE_GENERATOR,               ///< 46 == 0x2E  Industry animation: bubble generator (1): generate bubble
+	SND_2F_BUBBLE_GENERATOR_FAIL,          ///< 47 == 0x2F  Industry animation: bubble generator (2a): bubble pop
+	SND_30_TOFFEE_QUARRY,                  ///< 48 == 0x30  Industry animation: toffee quarry: drill
+	SND_31_BUBBLE_GENERATOR_SUCCESS,       ///< 49 == 0x31  Industry animation: bubble generator (2b): bubble slurped
+	SND_32_UNUSED,                         ///< 50 == 0x32  unused (5)
+	SND_33_PLASTIC_MINE,                   ///< 51 == 0x33  Industry producing: plastic fountain
+	SND_34_ARCTIC_SNOW_1,                  ///< 52 == 0x34  Tree ambient: arctic snow (1): wind
+	SND_35_BREAKDOWN_ROADVEHICLE_TOYLAND,  ///< 53 == 0x35  Breakdown: road vehicle (toyland)
+	SND_36_LUMBER_MILL_3,                  ///< 54 == 0x36  Industry animation: lumber mill (3): crashing tree
+	SND_37_LUMBER_MILL_2,                  ///< 55 == 0x37  Industry animation: lumber mill (2): falling tree
+	SND_38_LUMBER_MILL_1,                  ///< 56 == 0x38  Industry animation: lumber mill (1): chainsaw
+	SND_39_ARCTIC_SNOW_2,                  ///< 57 == 0x39  Tree ambient: arctic snow (2): heavy wind
+	SND_3A_BREAKDOWN_TRAIN_SHIP_TOYLAND,   ///< 58 == 0x3A  Breakdown: train or ship (toyland)
+	SND_3B_TAKEOFF_JET_FAST,               ///< 59 == 0x3B  Takeoff: supersonic plane (fast)
+	SND_3C_DEPARTURE_BUS_TOYLAND_1,        ///< 60 == 0x3C  Station departure: bus (1) (toyland)
+	SND_3D_TAKEOFF_JET_BIG,                ///< 61 == 0x3D  Takeoff: huge jet plane (high capacity)
+	SND_3E_DEPARTURE_BUS_TOYLAND_2,        ///< 62 == 0x3E  Station departure: bus (2) (toyland)
+	SND_3F_DEPARTURE_TRUCK_TOYLAND_1,      ///< 63 == 0x3F  Station departure: truck (1) (toyland)
+	SND_40_DEPARTURE_TRUCK_TOYLAND_2,      ///< 64 == 0x40  Station departure: truck (2) (toyland)
+	SND_41_DEPARTURE_MAGLEV,               ///< 65 == 0x41  Station departure: maglev engine
+	SND_42_RAINFOREST_1,                   ///< 66 == 0x42  Tree ambient: rainforest ambient (1): bird (1)
+	SND_43_RAINFOREST_2,                   ///< 67 == 0x43  Tree ambient: rainforest ambient (2): lion
+	SND_44_RAINFOREST_3,                   ///< 68 == 0x44  Tree ambient: rainforest ambient (3): monkeys
+	SND_45_TAKEOFF_PROPELLER_TOYLAND_1,    ///< 69 == 0x45  Takeoff: propeller plane (1) (toyland)
+	SND_46_TAKEOFF_PROPELLER_TOYLAND_2,    ///< 70 == 0x46  Takeoff: propeller plane (2) (toyland)
+	SND_47_DEPARTURE_MONORAIL,             ///< 71 == 0x47  Station departure: monorail engine
+	SND_48_RAINFOREST_4,                   ///< 72 == 0x48  Tree ambient: rainforest ambient (4): bird (2)
 	SND_END
 };
 
