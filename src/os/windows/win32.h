3ca023998574548fee50ad4c42e0b0996b9f0919
glx22 <glx@openttd.org>
2021-02-19 23:49:14 +0100

    Fix: [MinGW32] SHGFP_TYPE_CURRENT is now present in win32api

--- src/os/windows/win32.h
+++ src/os/windows/win32.h
@@ -19,10 +19,6 @@ bool LoadLibraryList(Function proc[], const char *dll);
 char *convert_from_fs(const wchar_t *name, char *utf8_buf, size_t buflen);
 wchar_t *convert_to_fs(const char *name, wchar_t *utf16_buf, size_t buflen);
 
-#if defined(__MINGW32__) && !defined(__MINGW64__)
-#define SHGFP_TYPE_CURRENT 0
-#endif /* __MINGW32__ */
-
 void Win32SetCurrentLocaleName(const char *iso_code);
 int OTTDStringCompare(const char *s1, const char *s2);
 
