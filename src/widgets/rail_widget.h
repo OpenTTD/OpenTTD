e708fb38da7a3635c7cc15dd1cc516ea1d2d5871
Didac Perez Parera <perez.didac@gmail.com>
2021-03-13 01:00:36 -0800

    Feature: allow filtering on name in rail station window (#8706)

--- src/widgets/rail_widget.h
+++ src/widgets/rail_widget.h
@@ -62,6 +62,8 @@ enum BuildRailStationWidgets {
 	WID_BRAS_IMAGE,                ///< Panel used at each cell of the matrix.
 	WID_BRAS_MATRIX_SCROLL,        ///< Scrollbar of the matrix widget.
 
+	WID_BRAS_FILTER_CONTAINER,     ///< Container for the filter text box for the station class list.
+	WID_BRAS_FILTER_EDITBOX,       ///< Filter text box for the station class list.
 	WID_BRAS_SHOW_NEWST_DEFSIZE,   ///< Selection for default-size button for newstation.
 	WID_BRAS_SHOW_NEWST_ADDITIONS, ///< Selection for newstation class selection list.
 	WID_BRAS_SHOW_NEWST_MATRIX,    ///< Selection for newstation image matrix.
