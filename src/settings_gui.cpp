877788174c35fc0b5577d9c0a34fd3ec51ef3809
Patric Stout <truebrain@openttd.org>
2021-03-24 16:38:36 +0100

    Feature: setting to indicate desert coverage for tropic climate
    This is an indication value; the game tries to get as close as it
    can, but due to the complex tropic rules, that is unlikely to be
    exact.
    
    In the end, it picks a height-level to base the desert/tropic
    line on. This is strictly seen not needed, as we can convert any
    tile to either. But it is the simplest way to get started with
    this without redoing all related functions.

--- src/settings_gui.cpp
+++ src/settings_gui.cpp
@@ -1691,6 +1691,7 @@ static SettingsContainer &GetSettingsTree()
 			genworld->Add(new SettingEntry("game_creation.variety"));
 			genworld->Add(new SettingEntry("game_creation.snow_coverage"));
 			genworld->Add(new SettingEntry("game_creation.snow_line_height"));
+			genworld->Add(new SettingEntry("game_creation.desert_coverage"));
 			genworld->Add(new SettingEntry("game_creation.amount_of_rivers"));
 			genworld->Add(new SettingEntry("game_creation.tree_placer"));
 			genworld->Add(new SettingEntry("vehicle.road_side"));
