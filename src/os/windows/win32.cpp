9d314515e7ae69baff639e01ee068c61fe89cbc6
Wim Leflere <wim.leflere@gmail.com>
2021-04-09 08:32:18 +0200

    Change: [win32] Use user UI language instead of date/time locale for initial language selection (#8973)
    Convert language ID to locale ID to have the correct type for GetLocaleInfo

--- src/os/windows/win32.cpp
+++ src/os/windows/win32.cpp
@@ -630,10 +630,11 @@ wchar_t *convert_to_fs(const char *name, wchar_t *system_buf, size_t buflen)
 const char *GetCurrentLocale(const char *)
 {
 	const LANGID userUiLang = GetUserDefaultUILanguage();
+	const LCID userUiLocale = MAKELCID(userUiLang, SORT_DEFAULT);
 
 	char lang[9], country[9];
-	if (GetLocaleInfoA(userUiLang, LOCALE_SISO639LANGNAME, lang, lengthof(lang)) == 0 ||
-	    GetLocaleInfoA(userUiLang, LOCALE_SISO3166CTRYNAME, country, lengthof(country)) == 0) {
+	if (GetLocaleInfoA(userUiLocale, LOCALE_SISO639LANGNAME, lang, lengthof(lang)) == 0 ||
+	    GetLocaleInfoA(userUiLocale, LOCALE_SISO3166CTRYNAME, country, lengthof(country)) == 0) {
 		/* Unable to retrieve the locale. */
 		return nullptr;
 	}
