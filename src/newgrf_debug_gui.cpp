b43933a011207a2f57cb89535cfbc3895e55c513
Peter Nelson <peter1138@openttd.org>
2021-04-22 19:07:20 +0100

    Fix: Tidy up sizing of sprite aligner window sprite list.

--- src/newgrf_debug_gui.cpp
+++ src/newgrf_debug_gui.cpp
@@ -863,8 +863,9 @@ struct SpriteAlignerWindow : Window {
 				size->height = ScaleGUITrad(200);
 				break;
 			case WID_SA_LIST:
-				resize->height = std::max(11, FONT_HEIGHT_NORMAL + 1);
+				resize->height = FONT_HEIGHT_NORMAL + WD_FRAMERECT_TOP + WD_FRAMERECT_BOTTOM;
 				resize->width  = 1;
+				fill->height = resize->height;
 				break;
 			default:
 				break;
