2efa390a7d2091b2c2cef4aa7f986fdcb24eac2a
Peter Nelson <peter1138@openttd.org>
2021-04-22 01:17:30 +0100

    Codechange: Simplify calling of DrawCharCentered()

--- src/osk_gui.cpp
+++ src/osk_gui.cpp
@@ -103,10 +103,7 @@ struct OskWindow : public Window {
 		if (widget < WID_OSK_LETTERS) return;
 
 		widget -= WID_OSK_LETTERS;
-		DrawCharCentered(_keyboard[this->shift][widget],
-			r.left + (r.right - r.left) / 2,
-			r.top + (r.bottom - r.top - FONT_HEIGHT_NORMAL) / 2,
-			TC_BLACK);
+		DrawCharCentered(_keyboard[this->shift][widget], r, TC_BLACK);
 	}
 
 	void OnClick(Point pt, int widget, int click_count) override
