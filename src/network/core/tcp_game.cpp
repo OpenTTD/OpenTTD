b52d61198a9801b4d5278f3d7e5d14d0312c427a
Berbe <4251220+Berbe@users.noreply.github.com>
2021-04-25 13:43:16 +0200

    Change: Use member initialisers

--- src/network/core/tcp_game.cpp
+++ src/network/core/tcp_game.cpp
@@ -24,7 +24,7 @@
  * Create a new socket for the game connection.
  * @param s The socket to connect with.
  */
-NetworkGameSocketHandler::NetworkGameSocketHandler(SOCKET s) : info(nullptr), client_id(INVALID_CLIENT_ID),
+NetworkGameSocketHandler::NetworkGameSocketHandler(SOCKET s, ClientID client_id) : info(nullptr), client_id(client_id),
 		last_frame(_frame_counter), last_frame_server(_frame_counter)
 {
 	this->sock = s;
