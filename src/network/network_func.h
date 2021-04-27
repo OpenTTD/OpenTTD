61a52b1b1970395d69cfe91171618ac7eab1f0b8
rubidium42 <rubidium@openttd.org>
2021-04-27 10:56:26 +0200

    Codechange: [Network] Do not leak os_abstraction.h via network_func

--- src/network/network_func.h
+++ src/network/network_func.h
@@ -17,7 +17,6 @@
 // #define DEBUG_DUMP_COMMANDS
 // #define DEBUG_FAILED_DUMP_COMMANDS
 
-#include "core/address.h"
 #include "network_type.h"
 #include "../console_type.h"
 #include "../gfx_type.h"
@@ -48,12 +47,12 @@ void NetworkGameLoop();
 void NetworkBackgroundLoop();
 void ParseConnectionString(const char **port, char *connection_string);
 void ParseGameConnectionString(const char **company, const char **port, char *connection_string);
-void NetworkStartDebugLog(NetworkAddress address);
+void NetworkStartDebugLog(const char *hostname, uint16 port);
 void NetworkPopulateCompanyStats(NetworkCompanyStats *stats);
 
 void NetworkUpdateClientInfo(ClientID client_id);
 void NetworkClientsToSpectators(CompanyID cid);
-void NetworkClientConnectGame(NetworkAddress address, CompanyID join_as, const char *join_server_password = nullptr, const char *join_company_password = nullptr);
+void NetworkClientConnectGame(const char *hostname, uint16 port, CompanyID join_as, const char *join_server_password = nullptr, const char *join_company_password = nullptr);
 void NetworkClientRequestMove(CompanyID company, const char *pass = "");
 void NetworkClientSendRcon(const char *password, const char *command);
 void NetworkClientSendChat(NetworkAction action, DestType type, int dest, const char *msg, int64 data = 0);
