d38004a821fe8f28ae82ab1f9091a4b4d8b0b930
Peter Nelson <peter1138@openttd.org>
2021-04-28 21:50:46 +0100

    Cleanup: Replace FOR_ALL_SORTED_CARGOSPECS macro with range iterator.

--- src/strings.cpp
+++ src/strings.cpp
@@ -1171,8 +1171,7 @@ static char *FormatString(char *buff, const char *str_arg, StringParameters *arg
 				CargoTypes cmask = args->GetInt64(SCC_CARGO_LIST);
 				bool first = true;
 
-				const CargoSpec *cs;
-				FOR_ALL_SORTED_CARGOSPECS(cs) {
+				for (const auto &cs : _sorted_cargo_specs) {
 					if (!HasBit(cmask, cs->Index())) continue;
 
 					if (buff >= last - 2) break; // ',' and ' '
