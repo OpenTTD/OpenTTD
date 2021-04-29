18ab2d5e05bcc851760013c8421dba3ca674b1d0
Peter Nelson <peter1138@openttd.org>
2021-04-29 20:04:50 +0100

    Cleanup: nested_array should not be accessed directly.
    Replace accesses with GetWidget() as documented.

--- src/object_gui.cpp
+++ src/object_gui.cpp
@@ -515,7 +515,7 @@ public:
 	{
 		switch (GB(widget, 0, 16)) {
 			case WID_BO_CLASS_LIST: {
-				int num_clicked = this->vscroll->GetPosition() + (pt.y - this->nested_array[widget]->pos_y) / this->line_height;
+				int num_clicked = this->vscroll->GetPosition() + (pt.y - this->GetWidget<NWidgetBase>(widget)->pos_y) / this->line_height;
 				if (num_clicked >= (int)this->object_classes.size()) break;
 
 				this->SelectOtherClass(this->object_classes[num_clicked]);
