d451ed0c673d9da1bb5ea47ea513e874ad0c298b
William Davis <daviswill048@icloud.com>
2021-05-07 14:22:21 -0400

    Update ai_sl.cpp

--- src/saveload/ai_sl.cpp
+++ src/saveload/ai_sl.cpp
@@ -98,7 +98,7 @@ static void Load_AIPL()
 					DEBUG(script, 0, "The savegame has an AI by the name '%s', version %d which is no longer available.", _ai_saveload_name, _ai_saveload_version);
 					DEBUG(script, 0, "The latest version of that AI has been loaded instead, but it'll not get the savegame data as it's incompatible.");
 				}
-				/* Make sure the AI doesn't get the saveload data, as he was not the
+				/* Make sure the AI doesn't get the saveload data, as it was not the
 				 *  writer of the saveload data in the first place */
 				_ai_saveload_version = -1;
 			}
