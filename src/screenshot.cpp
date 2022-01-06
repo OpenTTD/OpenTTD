ad63299694bf1e672d58b46f88a99cfd22da2fa2
Larry Fenn <larry.fenn@gmail.com>
2022-01-05 23:09:09 -0500

    Change: Automatic screenshot numbering
    Invoking the `screenshot` console command with a filename ending in %d
    will cause it to be numbered automatically.

--- src/screenshot.cpp
+++ src/screenshot.cpp
@@ -680,8 +680,18 @@ static const char *MakeScreenshotName(const char *default_fn, const char *ext, b
 		}
 	}
 
-	/* Add extension to screenshot file */
 	size_t len = strlen(_screenshot_name);
+
+	/* Handle user-specified filenames ending in %d with automatic numbering */
+	if (len >= 2) {
+		if (_screenshot_name[len - 2] == '%' && _screenshot_name[len - 1] == 'd') {
+			generate = true;
+			len -= 2;
+			_screenshot_name[len] = '\0';
+		}
+	}
+
+	/* Add extension to screenshot file */
 	seprintf(&_screenshot_name[len], lastof(_screenshot_name), ".%s", ext);
 
 	const char *screenshot_dir = crashlog ? _personal_dir.c_str() : FiosGetScreenshotDir();
