b8a8891a8656c0482a045505e531c812c60026df
Andy <andy@teamrubber.com>
2022-01-22 12:39:55 +0000

    Add: [Script] IndustryType::ResolveNewGRFID to resolve industry id from grfid and grf_local_id

--- src/script/api/ai_changelog.hpp
+++ src/script/api/ai_changelog.hpp
@@ -17,6 +17,7 @@
  *
  * This version is not yet released. The following changes are not set in stone yet.
  *
+ * \li AIIndustryType::ResolveNewGRFID
  * \li AIObjectType::ResolveNewGRFID
  *
  * \b 12.0
