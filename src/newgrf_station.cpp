f7022925d385ee98fa110b05bf5c90727295e465
frosch <frosch@openttd.org>
2022-09-10 22:53:02 +0200

    Cleanup: [NewGRF] Callback 14 used a weird value for var10 in the GUI. The value 0x2110000 probably originated from a mixup between callback 14 (sprite layout) and 24 (tile layout). The latter does indeed use a var10 value like that.

--- src/newgrf_station.cpp
+++ src/newgrf_station.cpp
@@ -788,7 +788,7 @@ bool DrawStationTile(int x, int y, RailType railtype, Axis axis, StationClassID 
 	if (statspec == nullptr) return false;
 
 	if (HasBit(statspec->callback_mask, CBM_STATION_SPRITE_LAYOUT)) {
-		uint16 callback = GetStationCallback(CBID_STATION_SPRITE_LAYOUT, 0x2110000, 0, statspec, nullptr, INVALID_TILE);
+		uint16 callback = GetStationCallback(CBID_STATION_SPRITE_LAYOUT, 0, 0, statspec, nullptr, INVALID_TILE);
 		if (callback != CALLBACK_FAILED) tile = callback;
 	}
 
