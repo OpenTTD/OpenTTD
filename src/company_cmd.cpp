a0a1efd0d361409e8791a082b7917c95fb2580c0
Tyler Trahan <tyler@tylertrahan.com>
2022-09-12 09:49:23 -0600

    Fix ca19a0d: Network hosts also use favorite face

--- src/company_cmd.cpp
+++ src/company_cmd.cpp
@@ -564,8 +564,8 @@ Company *DoStartupNewCompany(bool is_ai, CompanyID company = INVALID_COMPANY)
 	c->inaugurated_year = _cur_year;
 
 	/* If starting a player company in singleplayer and a favorite company manager face is selected, choose it. Otherwise, use a random face.
-	 * In a network game, we'll choose the favorite face later in CmdCompanyCtrl to sync it to all clients. */
-	if (_company_manager_face != 0 && !is_ai && !_networking) {
+	 * In a network game, we'll choose the favorite face later in CmdCompanyCtrl to sync it to all clients, but we choose it here for the first (host) company. */
+	if (_company_manager_face != 0 && !is_ai) {
 		c->face = _company_manager_face;
 	} else {
 		RandomCompanyManagerFaceBits(c->face, (GenderEthnicity)Random(), false, false);
