dd90d79e7bfb76ac358755dc4563dbe19221724b
Peter Nelson <peter1138@openttd.org>
2022-09-23 09:34:46 +0100

    Add: Setting to toggle thin vs chunky (scaled) bevels.

--- src/widgets/settings_widget.h
+++ src/widgets/settings_widget.h
@@ -20,6 +20,7 @@ enum GameOptionsWidgets {
 	WID_GO_RESOLUTION_DROPDOWN,    ///< Dropdown for the resolution.
 	WID_GO_FULLSCREEN_BUTTON,      ///< Toggle fullscreen.
 	WID_GO_GUI_ZOOM_DROPDOWN,      ///< Dropdown for the GUI zoom level.
+	WID_GO_GUI_SCALE_BEVEL_BUTTON, ///< Toggle for chunky bevels.
 	WID_GO_BASE_GRF_DROPDOWN,      ///< Use to select a base GRF.
 	WID_GO_BASE_GRF_STATUS,        ///< Info about missing files etc.
 	WID_GO_BASE_GRF_TEXTFILE,      ///< Open base GRF readme, changelog (+1) or license (+2).
