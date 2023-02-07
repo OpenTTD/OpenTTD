f12498b633d14ee2c5b6766bf6af9c37ad84c958
Tyler Trahan <tyler@tylertrahan.com>
2023-02-07 15:13:32 -0500

    Add: Show NewGRF name in NewGRF-created errors (#10457)

--- src/newgrf_gui.cpp
+++ src/newgrf_gui.cpp
@@ -50,12 +50,13 @@ void ShowNewGRFError()
 		/* Only show Fatal and Error level messages */
 		if (c->error == nullptr || (c->error->severity != STR_NEWGRF_ERROR_MSG_FATAL && c->error->severity != STR_NEWGRF_ERROR_MSG_ERROR)) continue;
 
-		SetDParam   (0, c->error->message != STR_NULL ? c->error->message : STR_JUST_RAW_STRING);
-		SetDParamStr(1, c->error->custom_message);
-		SetDParamStr(2, c->filename);
-		SetDParamStr(3, c->error->data);
+		SetDParamStr(0, c->GetName());
+		SetDParam   (1, c->error->message != STR_NULL ? c->error->message : STR_JUST_RAW_STRING);
+		SetDParamStr(2, c->error->custom_message);
+		SetDParamStr(3, c->filename);
+		SetDParamStr(4, c->error->data);
 		for (uint i = 0; i < lengthof(c->error->param_value); i++) {
-			SetDParam(4 + i, c->error->param_value[i]);
+			SetDParam(5 + i, c->error->param_value[i]);
 		}
 		if (c->error->severity == STR_NEWGRF_ERROR_MSG_FATAL) {
 			ShowErrorMessage(STR_NEWGRF_ERROR_FATAL_POPUP, INVALID_STRING_ID, WL_CRITICAL);
