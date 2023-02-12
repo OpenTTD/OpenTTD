6eabbaa751c24c447668576c42331318d7a0c880
SamuXarick <43006711+SamuXarick@users.noreply.github.com>
2023-02-12 14:29:18 +0000

    Fix #10469, 5e14a20: [Script] League Table rating element is a int64 everywhere else

--- src/saveload/league_sl.cpp
+++ src/saveload/league_sl.cpp
@@ -16,13 +16,14 @@
 #include "../safeguards.h"
 
 static const SaveLoad _league_table_elements_desc[] = {
-	 SLE_VAR(LeagueTableElement, table,       SLE_UINT8),
-	 SLE_VAR(LeagueTableElement, rating,      SLE_UINT64),
-	 SLE_VAR(LeagueTableElement, company,     SLE_UINT8),
-	SLE_SSTR(LeagueTableElement, text,        SLE_STR | SLF_ALLOW_CONTROL),
-	SLE_SSTR(LeagueTableElement, score,       SLE_STR | SLF_ALLOW_CONTROL),
-	 SLE_VAR(LeagueTableElement, link.type,   SLE_UINT8),
-	 SLE_VAR(LeagueTableElement, link.target, SLE_UINT32),
+	    SLE_VAR(LeagueTableElement, table,       SLE_UINT8),
+	SLE_CONDVAR(LeagueTableElement, rating,      SLE_FILE_U64 | SLE_VAR_I64, SL_MIN_VERSION, SLV_LINKGRAPH_EDGES),
+	SLE_CONDVAR(LeagueTableElement, rating,      SLE_INT64,                  SLV_LINKGRAPH_EDGES, SL_MAX_VERSION),
+	    SLE_VAR(LeagueTableElement, company,     SLE_UINT8),
+	   SLE_SSTR(LeagueTableElement, text,        SLE_STR | SLF_ALLOW_CONTROL),
+	   SLE_SSTR(LeagueTableElement, score,       SLE_STR | SLF_ALLOW_CONTROL),
+	    SLE_VAR(LeagueTableElement, link.type,   SLE_UINT8),
+	    SLE_VAR(LeagueTableElement, link.target, SLE_UINT32),
 };
 
 struct LEAEChunkHandler : ChunkHandler {
