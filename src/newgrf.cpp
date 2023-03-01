5d0ad5625bb3df5839154f292af184c591abcf97
Jonathan G Rennison <j.g.rennison@gmail.com>
2023-03-01 00:19:39 +0000

    Fix : [NewGRF] Object and road stop ignore property handlers (#10525)
    * Fix: IgnoreObjectProperty did not handle object property 0x18
    
    * Fix: IgnoreRoadStopProperty did not handle properties 0x0E - 0x12, 0x15

--- src/newgrf.cpp
+++ src/newgrf.cpp
@@ -4059,6 +4059,7 @@ static ChangeInfoResult IgnoreObjectProperty(uint prop, ByteReader *buf)
 		case 0x14:
 		case 0x16:
 		case 0x17:
+		case 0x18:
 			buf->ReadByte();
 			break;
 
@@ -4761,16 +4762,22 @@ static ChangeInfoResult IgnoreRoadStopProperty(uint prop, ByteReader *buf)
 	switch (prop) {
 		case 0x09:
 		case 0x0C:
+		case 0x0F:
+		case 0x11:
 			buf->ReadByte();
 			break;
 
 		case 0x0A:
 		case 0x0B:
+		case 0x0E:
+		case 0x10:
+		case 0x15:
 			buf->ReadWord();
 			break;
 
 		case 0x08:
 		case 0x0D:
+		case 0x12:
 			buf->ReadDWord();
 			break;
 
