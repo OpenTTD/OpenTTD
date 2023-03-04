970aa2597eee89c6ecbcd6b9c29a7aba9bdee158
glx22 <glx@openttd.org>
2023-03-04 17:45:03 +0100

    Codechange: Use SQInteger for generic numbers in script_news

--- src/script/api/script_news.hpp
+++ src/script/api/script_news.hpp
@@ -63,7 +63,7 @@ public:
 	 * @pre company == COMPANY_INVALID || ResolveCompanyID(company) != COMPANY_INVALID.
 	 * @pre The \a reference condition must be fulfilled.
 	 */
-	static bool Create(NewsType type, Text *text, ScriptCompany::CompanyID company, NewsReferenceType ref_type, uint32 reference);
+	static bool Create(NewsType type, Text *text, ScriptCompany::CompanyID company, NewsReferenceType ref_type, SQInteger reference);
 };
 
 #endif /* SCRIPT_NEWS_HPP */
