9eda419f9b8f48637645b12a373da195c2d18307
Patric Stout <truebrain@openttd.org>
2023-06-01 14:21:33 +0200

    Fix 646a7e62: recalc_time was not scaled properly (#10901)
    This caused "runtime" to underflow or, if you are lucky, hit an
    assert in ScaleToMonthly when it hits zero. But mostly underflow.

--- src/linkgraph/flowmapper.cpp
+++ src/linkgraph/flowmapper.cpp
@@ -50,7 +50,7 @@ void FlowMapper::Run(LinkGraphJob &job) const
 			/* Scale by time the graph has been running without being compressed. Add 1 to avoid
 			 * division by 0 if spawn date == last compression date. This matches
 			 * LinkGraph::Monthly(). */
-			uint runtime = job.JoinDate() - job.Settings().recalc_time - job.LastCompression() + 1;
+			uint runtime = job.JoinDate() - job.Settings().recalc_time / SECONDS_PER_DAY - job.LastCompression() + 1;
 			for (auto &it : flows) {
 				it.second.ScaleToMonthly(runtime);
 			}
