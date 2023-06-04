f36dbdb30a08bf8c265e1647d1ed73a2892085bc
Peter Nelson <peter1138@openttd.org>
2023-06-04 12:13:51 +0100

    Fix: Vehicle isn't a Train

--- src/vehicle_cmd.cpp
+++ src/vehicle_cmd.cpp
@@ -173,7 +173,7 @@ std::tuple<CommandCost, VehicleID, uint, uint16, CargoArray> CmdBuildVehicle(DoC
 
 		if (flags & DC_EXEC) {
 			if (type == VEH_TRAIN && use_free_vehicles && !(flags & DC_AUTOREPLACE)) { // check if the cars should be added to the new vehicle
-				NormalizeTrainVehInDepot(v);
+				NormalizeTrainVehInDepot(Train::From(v));
 			}
 
 			InvalidateWindowData(WC_VEHICLE_DEPOT, v->tile);
