390cea13422facccbde5b5d0f382c28360ff606b
Rubidium <rubidium@openttd.org>
2023-06-05 17:15:20 +0200

    Cleanup: remove DRAW_STRING_BUFFER constant

--- src/network/network_chat_gui.cpp
+++ src/network/network_chat_gui.cpp
@@ -30,10 +30,6 @@
 
 #include "../safeguards.h"
 
-/** The draw buffer must be able to contain the chat message, client name and the "[All]" message,
- * some spaces and possible translations of [All] to other languages. */
-static_assert((int)DRAW_STRING_BUFFER >= (int)NETWORK_CHAT_LENGTH + NETWORK_NAME_LENGTH + 40);
-
 /** Spacing between chat lines. */
 static const uint NETWORK_CHAT_LINE_SPACING = 3;
 
