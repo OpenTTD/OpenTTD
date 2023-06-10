4e316c8e0614fd9eecd83213c2a451f47e05356b
Andrii Olkhovych <the.def4alt@gmail.com>
2023-06-10 22:13:33 +0200

    Fix #10982: No help text for gamelog command

--- src/gamelog.h
+++ src/gamelog.h
@@ -5,7 +5,7 @@
  * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OpenTTD. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/** @file gamelog.h Functions to be called to log possibly unsafe game events */
+/** @file gamelog.h Functions to be called to log fundamental changes to the game */
 
 #ifndef GAMELOG_H
 #define GAMELOG_H
