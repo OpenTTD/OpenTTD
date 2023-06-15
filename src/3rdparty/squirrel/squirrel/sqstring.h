e097b3ee78a2e8a3ff65bcf89e486f106fd5971e
Rubidium <rubidium@openttd.org>
2023-06-15 18:07:58 +0200

    Codechange: make SQString::Create that supports std::string and use that

--- src/3rdparty/squirrel/squirrel/sqstring.h
+++ src/3rdparty/squirrel/squirrel/sqstring.h
@@ -17,6 +17,7 @@ struct SQString : public SQRefCounted
 	~SQString(){}
 public:
 	static SQString *Create(SQSharedState *ss, const SQChar *, SQInteger len = -1 );
+	static SQString *Create(SQSharedState *ss, const std::string &str) { return Create(ss, str.data(), str.size()); }
 	SQInteger Next(const SQObjectPtr &refpos, SQObjectPtr &outkey, SQObjectPtr &outval);
 	void Release();
 	SQSharedState *_sharedstate;
