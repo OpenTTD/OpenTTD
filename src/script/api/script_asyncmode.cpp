f1d8264d02577ea89d35694895c9966a3a05aa93
glx22 <glx@openttd.org>
2023-12-14 21:58:02 +0100

    Fix: [Script] Properly store the previous AsyncMode state

--- src/script/api/script_asyncmode.cpp
+++ src/script/api/script_asyncmode.cpp
@@ -39,8 +39,8 @@ ScriptAsyncMode::ScriptAsyncMode(HSQUIRRELVM vm)
 		throw sq_throwerror(vm, "Argument must be a boolean");
 	}
 
-	this->last_mode     = this->GetDoCommandMode();
-	this->last_instance = this->GetDoCommandModeInstance();
+	this->last_mode     = this->GetDoCommandAsyncMode();
+	this->last_instance = this->GetDoCommandAsyncModeInstance();
 
 	this->SetDoCommandAsyncMode(sqasync ? &ScriptAsyncMode::AsyncModeProc : &ScriptAsyncMode::NonAsyncModeProc, this);
 }
