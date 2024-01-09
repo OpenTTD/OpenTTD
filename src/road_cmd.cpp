4dedc02cd51e044a942ad1564613241cabe0c1a7
Jonathan G Rennison <j.g.rennison@gmail.com>
2024-01-09 08:59:44 +0000

    Change: Clear one way state when removing road from road+tram tile

--- src/road_cmd.cpp
+++ src/road_cmd.cpp
@@ -467,6 +467,7 @@ static CommandCost RemoveRoad(TileIndex tile, DoCommandFlag flags, RoadBits piec
 							const Town *town = CalcClosestTownFromTile(tile);
 							SetTownIndex(tile, town == nullptr ? INVALID_TOWN : town->index);
 						}
+						if (rtt == RTT_ROAD) SetDisallowedRoadDirections(tile, DRD_NONE);
 						SetRoadBits(tile, ROAD_NONE, rtt);
 						SetRoadType(tile, rtt, INVALID_ROADTYPE);
 						MarkTileDirtyByTile(tile);
