87dff3e9afc9d5bb6ca2b0dce2d80e15eabccffa
Rubidium <rubidium@openttd.org>
2024-01-20 15:59:53 +0100

    Remove: LeastCommonMultiple / GreatestCommonDivisor
    Use std::lcm / std::gcd instead.

--- src/tests/math_func.cpp
+++ src/tests/math_func.cpp
@@ -13,41 +13,6 @@
 
 #include "../core/math_func.hpp"
 
-TEST_CASE("LeastCommonMultipleTest - Zero")
-{
-	CHECK(0 == LeastCommonMultiple(0, 0));
-	CHECK(0 == LeastCommonMultiple(0, 600));
-	CHECK(0 == LeastCommonMultiple(600, 0));
-}
-
-TEST_CASE("LeastCommonMultipleTest - FindLCM")
-{
-	CHECK(25 == LeastCommonMultiple(5, 25));
-	CHECK(25 == LeastCommonMultiple(25, 5));
-	CHECK(130 == LeastCommonMultiple(5, 26));
-	CHECK(130 == LeastCommonMultiple(26, 5));
-}
-
-TEST_CASE("GreatestCommonDivisorTest - Negative")
-{
-	CHECK(4 == GreatestCommonDivisor(4, -52));
-	// CHECK(3 == GreatestCommonDivisor(-27, 6)); // error - returns -3
-}
-
-TEST_CASE("GreatestCommonDivisorTest - Zero")
-{
-	CHECK(27 == GreatestCommonDivisor(0, 27));
-	CHECK(27 == GreatestCommonDivisor(27, 0));
-}
-
-TEST_CASE("GreatestCommonDivisorTest - FindGCD")
-{
-	CHECK(5 == GreatestCommonDivisor(5, 25));
-	CHECK(5 == GreatestCommonDivisor(25, 5));
-	CHECK(1 == GreatestCommonDivisor(7, 27));
-	CHECK(1 == GreatestCommonDivisor(27, 7));
-}
-
 TEST_CASE("DivideApproxTest - Negative")
 {
 	CHECK(-2 == DivideApprox(-5, 2));
