64e29418137bb182d7d10fc0271cb11d0e11bce6
dP <dp@dpointer.org>
2024-01-29 15:58:03 +0530

    Fix: Don't stop growth attempt if house is not allowed

--- src/town_cmd.cpp
+++ src/town_cmd.cpp
@@ -1593,8 +1593,6 @@ static void GrowTownInTile(TileIndex *tile_ptr, RoadBits cur_rb, DiagDirection t
 		}
 
 		if (!TownCanGrowRoad(tile)) return;
-
-		_grow_town_result = GROWTH_SEARCH_STOPPED;
 	}
 
 	/* Return if a water tile */
