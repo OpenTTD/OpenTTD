2efd07458e04b7292de18fa142f891732f388362
Patric Stout <truebrain@openttd.org>
2024-01-29 22:21:41 +0100

    Fix: use economy timer for dmp_cmds files

--- src/openttd.cpp
+++ src/openttd.cpp
@@ -1465,7 +1465,7 @@ void StateGameLoop()
 	} else {
 		if (_debug_desync_level > 2 && TimerGameEconomy::date_fract == 0 && (TimerGameEconomy::date.base() & 0x1F) == 0) {
 			/* Save the desync savegame if needed. */
-			std::string name = fmt::format("dmp_cmds_{:08x}_{:08x}.sav", _settings_game.game_creation.generation_seed, TimerGameCalendar::date);
+			std::string name = fmt::format("dmp_cmds_{:08x}_{:08x}.sav", _settings_game.game_creation.generation_seed, TimerGameEconomy::date);
 			SaveOrLoad(name, SLO_SAVE, DFT_GAME_FILE, AUTOSAVE_DIR, false);
 		}
 
