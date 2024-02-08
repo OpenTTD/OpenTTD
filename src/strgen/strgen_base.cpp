7e2eefb91ff5999667752eea6e13bd8a0d293f48
Rubidium <rubidium@openttd.org>
2024-02-08 21:51:38 +0100

    Cleanup: Remove digit group separators from strgen and languages

--- src/strgen/strgen_base.cpp
+++ src/strgen/strgen_base.cpp
@@ -758,8 +758,6 @@ void StringReader::ParseFile()
 	MemSetT(&_lang, 0);
 	strecpy(_lang.number_format, "00,000,000,000,000,000,000", lastof(_lang.number_format));
 	strecpy(_lang.number_abbreviations, "3=00,000,000,000,000,000{NBSP}k|6=00,000,000,000,000{NBSP}m|9=00,000,000,000{NBSP}bn|12=00,000,000{NBSP}tn|15=00,000{NBSP}Qa|18=00{NBSP}Qi", lastof(_lang.number_abbreviations));
-	strecpy(_lang.digit_group_separator, ",", lastof(_lang.digit_group_separator));
-	strecpy(_lang.digit_group_separator_currency, ",", lastof(_lang.digit_group_separator_currency));
 	strecpy(_lang.digit_decimal_separator, ".", lastof(_lang.digit_decimal_separator));
 
 	_cur_line = 1;
