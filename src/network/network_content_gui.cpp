5b3bfe4c4ca052d636a87c6ad54f1e2f22c0c7b6
Patric Stout <truebrain@openttd.org>
2024-02-11 20:24:28 +0100

    Fix fdfcb09: for content service, fallback to TCP downloads when HTTP stalls (#12056)

--- src/network/network_content_gui.cpp
+++ src/network/network_content_gui.cpp
@@ -291,7 +291,7 @@ public:
 	{
 		if (widget == WID_NCDS_CANCELOK) {
 			if (this->downloaded_bytes != this->total_bytes) {
-				_network_content_client.CloseConnection();
+				_network_content_client.Cancel();
 				this->Close();
 			} else {
 				/* If downloading succeeded, close the online content window. This will close
