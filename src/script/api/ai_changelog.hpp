6ddbdca0fde96407101d516219b556c1ce167768
SamuXarick <43006711+SamuXarick@users.noreply.github.com>
2024-02-13 23:39:08 +0000

    Add: [Script] GetAirportNumTerminals
    Add a method to get the number of terminals of a type of airport.

--- src/script/api/ai_changelog.hpp
+++ src/script/api/ai_changelog.hpp
@@ -27,6 +27,7 @@
  * \li AICargo::CC_NON_POURABLE
  * \li AICargo::CC_POTABLE
  * \li AICargo::CC_NON_POTABLE
+ * \li AIAirport::GetAirportNumTerminals
  *
  * Other changes:
  * \li AIBridge::GetBridgeID renamed to AIBridge::GetBridgeType
