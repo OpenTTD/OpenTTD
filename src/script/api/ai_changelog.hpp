14b87b9fc2748840a9de07901ecd7947a62f5ea3
SamuXarick <43006711+SamuXarick@users.noreply.github.com>
2024-02-14 15:03:16 +0000

    Add: [Script] GetAirportPlaneCompatibility
    Add a function for Scripts to check whether a type of plane can use a type of airport.

--- src/script/api/ai_changelog.hpp
+++ src/script/api/ai_changelog.hpp
@@ -28,6 +28,11 @@
  * \li AICargo::CC_POTABLE
  * \li AICargo::CC_NON_POTABLE
  * \li AIVehicleList_Waypoint
+ * \li AIAirport::GetAirportPlaneCompatibility
+ * \li AIAirport::APC_INCOMPATIBLE
+ * \li AIAirport::APC_COMPATIBLE
+ * \li AIAirport::APC_SHORT_RUNWAY
+ * \li AIAirport::APC_INVALID
  *
  * Other changes:
  * \li AIBridge::GetBridgeID renamed to AIBridge::GetBridgeType
