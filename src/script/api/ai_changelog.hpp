5d8f3ae6db40c8b1889dd84650ebe13bfee152df
SamuXarick <43006711+SamuXarick@users.noreply.github.com>
2024-02-16 16:27:32 +0000

    Change: [Script] Improve some ScriptAirport return values for AT_OILRIG airport type
    Affected functions are:
    IsAirportInformationAvailable, GetAirportWidth, GetAirportHeight, GetAirportCoverageRadius, GetNumHangars, GetHangarOfAirport, GetNoiseLevelIncrease, GetNearestTown, GetMaintenanceCostFactor, GetMonthlyMaintenanceCost and GetAirportNumHelipads.
    
    Add AT_OILRIG to the AirportType enum even though it can't be built.
    
    Add an AirportTileTable for oilrig in AirportSpec definitions.

--- src/script/api/ai_changelog.hpp
+++ src/script/api/ai_changelog.hpp
@@ -28,6 +28,7 @@
  * \li AICargo::CC_POTABLE
  * \li AICargo::CC_NON_POTABLE
  * \li AIVehicleList_Waypoint
+ * \li AIAirport::AT_OILRIG
  *
  * Other changes:
  * \li AIBridge::GetBridgeID renamed to AIBridge::GetBridgeType
