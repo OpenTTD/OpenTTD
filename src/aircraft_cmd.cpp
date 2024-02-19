38d64619f852413df7b562d4a394642bba1781ec
SamuXarick <43006711+SamuXarick@users.noreply.github.com>
2024-02-19 22:21:06 +0000

    Fix ccb4c37: Aircraft could route to hangars outside their range when stopping at them

--- src/aircraft_cmd.cpp
+++ src/aircraft_cmd.cpp
@@ -134,7 +134,7 @@ static StationID FindNearestHangar(const Aircraft *v)
 	const Station *next_dest = nullptr;
 	if (max_range != 0) {
 		if (v->current_order.IsType(OT_GOTO_STATION) ||
-				(v->current_order.IsType(OT_GOTO_DEPOT) && v->current_order.GetDepotActionType() != ODATFB_NEAREST_DEPOT)) {
+				(v->current_order.IsType(OT_GOTO_DEPOT) && (v->current_order.GetDepotActionType() & ODATFB_NEAREST_DEPOT) == 0)) {
 			last_dest = Station::GetIfValid(v->last_station_visited);
 			next_dest = Station::GetIfValid(v->current_order.GetDestination());
 		} else {
