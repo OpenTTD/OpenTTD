b2ca6e1ac8216d0cacb1e44c8e08856cc8755f59
Peter Nelson <peter1138@openttd.org>
2024-03-03 09:31:04 +0000

    Codefix: Remove no-longer used ship special-case. (#12192)
    This special-case has not been triggered since multi-tile docks were introduced.

--- src/pathfinder/yapf/yapf.h
+++ src/pathfinder/yapf/yapf.h
@@ -22,11 +22,10 @@
  * @param v        the ship that needs to find a path
  * @param tile     the tile to find the path from (should be next tile the ship is about to enter)
  * @param enterdir diagonal direction which the ship will enter this new tile from
- * @param tracks   available tracks on the new tile (to choose from)
  * @param path_found [out] Whether a path has been found (true) or has been guessed (false)
  * @return         the best trackdir for next turn or INVALID_TRACK if the path could not be found
  */
-Track YapfShipChooseTrack(const Ship *v, TileIndex tile, DiagDirection enterdir, TrackBits tracks, bool &path_found, ShipPathCache &path_cache);
+Track YapfShipChooseTrack(const Ship *v, TileIndex tile, DiagDirection enterdir, bool &path_found, ShipPathCache &path_cache);
 
 /**
  * Returns true if it is better to reverse the ship before leaving depot using YAPF.
