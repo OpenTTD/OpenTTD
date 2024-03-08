3cc00d05f71b8eb27bee5187bd5e768f1ee0d693
Kuhnovic <68320206+Kuhnovic@users.noreply.github.com>
2024-03-08 18:31:58 +0100

    Update: Developer credits (#12235) (#12235)

--- src/misc_gui.cpp
+++ src/misc_gui.cpp
@@ -411,6 +411,7 @@ static const char * const _credits[] = {
 	"  Matthijs Kooijman (blathijs) - Pathfinder-guru, Debian port (since 0.3)",
 	"  Christoph Elsenhans (frosch) - General coding (since 0.6)",
 	"  Lo\u00efc Guilloux (glx) - General / Windows Expert (since 0.4.5)",
+	"  Koen Bussemaker (Kuhnovic) - General / Ship pathfinder (since 14)",
 	"  Charles Pigott (LordAro) - General / Correctness police (since 1.9)",
 	"  Michael Lutz (michi_cc) - Path based signals (since 0.7)",
 	"  Niels Martin Hansen (nielsm) - Music system, general coding (since 1.9)",
