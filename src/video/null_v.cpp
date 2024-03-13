27a841117834158169735b241648eef266245196
glx22 <glx@openttd.org>
2024-03-13 15:07:15 +0100

    Codechange: [Win32] Disable assert message box when no GUI

--- src/video/null_v.cpp
+++ src/video/null_v.cpp
@@ -24,6 +24,8 @@ const char *VideoDriver_Null::Start(const StringList &parm)
 #ifdef _MSC_VER
 	/* Disable the MSVC assertion message box. */
 	_set_error_mode(_OUT_TO_STDERR);
+	_CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
+	_CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
 #endif
 
 	this->UpdateAutoResolution();
