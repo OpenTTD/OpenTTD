29693734d5b1bae5a9a75996b9138e87911a9475
Peter Nelson <peter1138@openttd.org>
2024-03-14 17:09:35 +0000

    Change: Use per-company group numbers.
    This is used by the default group name, replacing the use of group index.

--- src/saveload/group_sl.cpp
+++ src/saveload/group_sl.cpp
@@ -26,6 +26,7 @@ static const SaveLoad _group_desc[] = {
 	 SLE_CONDVAR(Group, livery.colour1,     SLE_UINT8,                     SLV_GROUP_LIVERIES, SL_MAX_VERSION),
 	 SLE_CONDVAR(Group, livery.colour2,     SLE_UINT8,                     SLV_GROUP_LIVERIES, SL_MAX_VERSION),
 	 SLE_CONDVAR(Group, parent,             SLE_UINT16,                    SLV_189, SL_MAX_VERSION),
+	 SLE_CONDVAR(Group, number, SLE_UINT16, SLV_GROUP_NUMBERS, SL_MAX_VERSION),
 };
 
 struct GRPSChunkHandler : ChunkHandler {
@@ -58,6 +59,14 @@ struct GRPSChunkHandler : ChunkHandler {
 				const Company *c = Company::Get(g->owner);
 				g->livery.colour1 = c->livery[LS_DEFAULT].colour1;
 				g->livery.colour2 = c->livery[LS_DEFAULT].colour2;
+			}
+
+			Company *c = Company::Get(g->owner);
+			if (IsSavegameVersionBefore(SLV_GROUP_NUMBERS)) {
+				/* Use the index as group number when converting old savegames. */
+				g->number = c->freegroups.UseID(g->index);
+			} else {
+				c->freegroups.UseID(g->number);
 			}
 		}
 	}
