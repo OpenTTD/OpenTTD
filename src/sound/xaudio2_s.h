a58a55f59165cc0b3adc1e5ee7e199c9c48dc965
Peter Nelson <peter1138@openttd.org>
2024-04-12 17:24:45 +0100

    Fix 25aeb1c: Driver parameter documentation was not updated.

--- src/sound/xaudio2_s.h
+++ src/sound/xaudio2_s.h
@@ -24,7 +24,7 @@ public:
 /** Factory for the XAudio2 sound driver. */
 class FSoundDriver_XAudio2 : public DriverFactoryBase {
 public:
-	FSoundDriver_XAudio2() : DriverFactoryBase(Driver::DT_SOUND, 10, "xaudio2", "XAudio2 Sound Driver (param bufsize,hz)") {}
+	FSoundDriver_XAudio2() : DriverFactoryBase(Driver::DT_SOUND, 10, "xaudio2", "XAudio2 Sound Driver (param hz,samples)") {}
 	Driver *CreateInstance() const override { return new SoundDriver_XAudio2(); }
 };
 
