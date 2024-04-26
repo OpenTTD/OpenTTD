a6d401debfb13fc9998c6555c0e28dc30bd51038
Peter Nelson <peter1138@openttd.org>
2024-04-26 17:47:53 +0100

    Fix: Properly test for presence of waypoint in NewGRF resolver. (#12579)
    Test whether the BaseStation itself a Station or Waypoint, instead of by the station class ID assigned to it.

--- src/newgrf_station.cpp
+++ src/newgrf_station.cpp
@@ -504,7 +504,7 @@ uint32_t Waypoint::GetNewGRFVariable(const ResolverObject &, uint8_t variable, [
 
 /* virtual */ const SpriteGroup *StationResolverObject::ResolveReal(const RealSpriteGroup *group) const
 {
-	if (this->station_scope.st == nullptr || this->station_scope.statspec->cls_id == STAT_CLASS_WAYP) {
+	if (this->station_scope.st == nullptr || !Station::IsExpected(this->station_scope.st)) {
 		return group->loading[0];
 	}
 
