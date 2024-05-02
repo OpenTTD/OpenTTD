fd52747faf139c3003558366d84522549ae41124
James Addison <55152140+jayaddison@users.noreply.github.com>
2024-05-02 14:13:00 +0100

    Doc: Add Apache 2.0 licensing info for CheckAtomic.cmake (#12603)

--- cmake/3rdparty/llvm/CheckAtomic.cmake
+++ cmake/3rdparty/llvm/CheckAtomic.cmake
@@ -1,3 +1,11 @@
+#
+# This was originally part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See (https://llvm.org/)LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+# Modifications have been made to suit building OpenTTD.
+#
+
 # atomic builtins are required for threading support.
 
 INCLUDE(CheckCXXSourceCompiles)
