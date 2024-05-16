58fe6a4c8b17687967669565a16aeae0b2fd7316
Peter Nelson <peter1138@openttd.org>
2024-05-16 08:44:24 +0100

    Fix: Compile failure with newer C++ compiler on MinGW.
    > template-id not allowed for constructor in C++20

--- src/vehicle_base.h
+++ src/vehicle_base.h
@@ -1100,7 +1100,7 @@ struct SpecializedVehicle : public Vehicle {
 	/**
 	 * Set vehicle type correctly
 	 */
-	inline SpecializedVehicle<T, Type>() : Vehicle(Type)
+	inline SpecializedVehicle() : Vehicle(Type)
 	{
 		this->sprite_cache.sprite_seq.count = 1;
 	}
