5d1da5557a8e10c9a7c0da71f0d5e0dd3822b90d
Tyler Trahan <tyler@tylertrahan.com>
2024-05-28 17:43:37 -0400

    Change: Don't use house construction states in Scenario Editor

--- src/town_cmd.cpp
+++ src/town_cmd.cpp
@@ -2681,7 +2681,7 @@ static void BuildTownHouse(Town *t, TileIndex tile, const HouseSpec *hs, HouseID
 	uint8_t construction_counter = 0;
 	uint8_t construction_stage = 0;
 
-	if (_generating_world || _game_mode == GM_EDITOR) {
+	if (_generating_world) {
 		uint32_t construction_random = Random();
 
 		construction_stage = TOWN_HOUSE_COMPLETED;
