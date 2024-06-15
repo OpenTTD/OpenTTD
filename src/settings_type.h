890fb4b0bdb0feb0b66f109ee11076cbb7e86767
merni-ns <66267867+merni-ns@users.noreply.github.com>
2024-06-16 02:44:29 +0530

    Add: Setting to disable warning for old vehicles (#12714)

--- src/settings_type.h
+++ src/settings_type.h
@@ -132,6 +132,7 @@ struct GUISettings {
 	bool   lost_vehicle_warn;                ///< if a vehicle can't find its destination, show a warning
 	uint8_t  order_review_system;              ///< perform order reviews on vehicles
 	bool   vehicle_income_warn;              ///< if a vehicle isn't generating income, show a warning
+	bool   old_vehicle_warn;                 ///< if a vehicle is getting old, show a warning
 	bool   show_finances;                    ///< show finances at end of year
 	bool   sg_new_nonstop;                   ///< ttdpatch compatible nonstop handling read from pre v93 savegames
 	bool   new_nonstop;                      ///< ttdpatch compatible nonstop handling
