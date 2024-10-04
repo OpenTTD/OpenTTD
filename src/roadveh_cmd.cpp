db90a05b393acf13179e06f44008d2b6ea18d720
SamuXarick <43006711+SamuXarick@users.noreply.github.com>
2024-10-04 14:57:57 +0100

    Change: Also add OnNewEconomyDay vehicle performance measurements to the frame rate statistics

--- src/roadveh_cmd.cpp
+++ src/roadveh_cmd.cpp
@@ -1713,6 +1713,9 @@ void RoadVehicle::OnNewCalendarDay()
 void RoadVehicle::OnNewEconomyDay()
 {
 	if (!this->IsFrontEngine()) return;
+
+	PerformanceAccumulator framerate(PFE_GL_ROADVEHS);
+
 	EconomyAgeVehicle(this);
 
 	if ((++this->day_counter & 7) == 0) DecreaseVehicleValue(this);
