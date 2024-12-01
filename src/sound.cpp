e28617fda63f2697dcb547965dbda26e77fa09f8
Peter Nelson <peter1138@openttd.org>
2024-12-01 14:22:41 +0000

    Codefix: Avoiding passing new raw pointer into a smart pointer. (#13138)
    Use `std::make_shared` or `std::make_unique` instead of `reset(new ...)`.

--- src/sound.cpp
+++ src/sound.cpp
@@ -37,7 +37,7 @@ static void OpenBankFile(const std::string &filename)
 	/* If there is no sound file (nosound set), don't load anything */
 	if (filename.empty()) return;
 
-	original_sound_file.reset(new RandomAccessFile(filename, BASESET_DIR));
+	original_sound_file = std::make_unique<RandomAccessFile>(filename, BASESET_DIR);
 	size_t pos = original_sound_file->GetPos();
 	uint count = original_sound_file->ReadDword();
 
