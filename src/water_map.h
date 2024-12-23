b90d670878c2b39f520232bef3e1d400b4b32cbc
SamuXarick <43006711+SamuXarick@users.noreply.github.com>
2024-12-23 15:43:50 +0000

    Codechange 4f9c10d: Water tile type reduced to 2 bits in the map array
    Also reword water tile type tooltips in the landscape_grid document.

--- src/water_map.h
+++ src/water_map.h
@@ -17,7 +17,7 @@
  * Bit field layout of m5 for water tiles.
  */
 static constexpr uint8_t WBL_TYPE_BEGIN = 4; ///< Start of the 'type' bitfield.
-static constexpr uint8_t WBL_TYPE_COUNT = 4; ///< Length of the 'type' bitfield.
+static constexpr uint8_t WBL_TYPE_COUNT = 2; ///< Length of the 'type' bitfield.
 
 static constexpr uint8_t WBL_LOCK_ORIENT_BEGIN = 0; ///< Start of lock orientation bitfield.
 static constexpr uint8_t WBL_LOCK_ORIENT_COUNT = 2; ///< Length of lock orientation bitfield.
