bea6f90a7d4a5480dce9a6f565c2471a08d6d065
Tyler Trahan <tyler@tylertrahan.com>
2025-01-13 16:34:06 -0500

    Feature: Draw infinite water when all borders are water

--- src/saveload/saveload.h
+++ src/saveload/saveload.h
@@ -405,6 +405,7 @@ enum SaveLoadVersion : uint16_t {
 
 	SLV_FACE_STYLES,                        ///< 355  PR#14319 Addition of face styles, replacing gender and ethnicity.
 	SLV_INDUSTRY_NUM_VALID_HISTORY,         ///< 356  PR#14416 Store number of valid history records for industries.
+	SLV_INFINITE_WATER_BORDERS,             ///< 357  PR#13289 Draw infinite water when all borders are water.
 
 	SL_MAX_VERSION,                         ///< Highest possible saveload version
 };
