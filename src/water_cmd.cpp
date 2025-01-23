d76254e6d84350bd1435aae6d3b7e0993c052c9b
SamuXarick <43006711+SamuXarick@users.noreply.github.com>
2025-01-23 18:12:57 +0000

    Fix: Remove unwanted river artifacts after generation
    This requires postponing ModifyDesertZoneAroundRiver to after river generation.

--- src/water_cmd.cpp
+++ src/water_cmd.cpp
@@ -461,6 +461,18 @@ void MakeRiverAndModifyDesertZoneAround(TileIndex tile)
 }
 
 /**
+ * Remove desert directly around a river tile.
+ * @param tile The tile to create non-desert around
+ */
+void ModifyDesertZoneAroundRiver(TileIndex tile)
+{
+	assert(IsTileType(tile, MP_WATER) && IsRiver(tile));
+
+	/* Remove desert directly around the river tile. */
+	CircularTileSearch(&tile, RIVER_OFFSET_DESERT_DISTANCE, RiverModifyDesertZone, nullptr);
+}
+
+/**
  * Build a piece of canal.
  * @param flags type of operation
  * @param tile end tile of stretch-dragging
