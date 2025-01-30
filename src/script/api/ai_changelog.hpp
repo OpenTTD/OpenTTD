0627bec61e47e9863fb1f06de22d84532e297a4e
SamuXarick <43006711+SamuXarick@users.noreply.github.com>
2025-01-30 13:56:07 +0000

    Add: [Script] GetBaseStationID
    Added ScriptBaseStation::GetBaseStationID to get the StationID of a tile, if there is a basestation (Station or Waypoint).

--- src/script/api/ai_changelog.hpp
+++ src/script/api/ai_changelog.hpp
@@ -30,6 +30,7 @@
  * \li AIVehicleList_Waypoint
  * \li AIError::ERR_BRIDGE_TOO_LOW
  * \li AIEngine::GetAllRailTypes
+ * \li AIBaseStation::GetBaseStationID
  *
  * Other changes:
  * \li AIBridge::GetBridgeID renamed to AIBridge::GetBridgeType
