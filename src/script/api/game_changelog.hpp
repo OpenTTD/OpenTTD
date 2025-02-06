bde26365c1112f6e62366bdb149225874c081a34
SamuXarick <43006711+SamuXarick@users.noreply.github.com>
2025-02-06 17:05:49 +0000

    Add: [Script] GetEconomyAge and GetWagonEconomyAge

--- src/script/api/game_changelog.hpp
+++ src/script/api/game_changelog.hpp
@@ -27,6 +27,8 @@
  * \li GSCargo::CC_NON_POURABLE
  * \li GSCargo::CC_POTABLE
  * \li GSCargo::CC_NON_POTABLE
+ * \li GSVehicle::GetEconomyAge
+ * \li GSVehicle::GetWagonEconomyAge
  *
  * Other changes:
  * \li GSBridge::GetBridgeID renamed to GSBridge::GetBridgeType
