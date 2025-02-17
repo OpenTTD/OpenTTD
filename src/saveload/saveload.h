3c9b62cbf14f8f6835dee3c45973b5cb81534c3f
SamuXarick <43006711+SamuXarick@users.noreply.github.com>
2025-02-17 22:03:18 +0000

    Change: [Script] Save exact version selected in savegames

--- src/saveload/saveload.h
+++ src/saveload/saveload.h
@@ -403,6 +403,8 @@ enum SaveLoadVersion : uint16_t {
 	SLV_FIX_SCC_ENCODED_NEGATIVE,           ///< 353  PR#14049 Fix encoding of negative parameters.
 	SLV_ORDERS_OWNED_BY_ORDERLIST,          ///< 354  PR#13948 Orders stored in OrderList, pool removed.
 
+	SLV_SCRIPT_FORCE_EXACT_MATCH,           ///< 355  PR#13565 Store script force exact match in savegames.
+
 	SL_MAX_VERSION,                         ///< Highest possible saveload version
 };
 
