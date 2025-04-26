84e7e62509b022a52e5325c20800861a9836e2b7
Rubidium <rubidium@openttd.org>
2025-04-26 16:34:24 +0200

    Codefix: checking unsigned int >= 0 is pointless, check before subtraction

--- src/roadstop.cpp
+++ src/roadstop.cpp
@@ -268,8 +268,8 @@ bool RoadStop::Enter(RoadVehicle *rv)
  */
 void RoadStop::Entry::Leave(const RoadVehicle *rv)
 {
+	assert(this->occupied >= rv->gcache.cached_total_length);
 	this->occupied -= rv->gcache.cached_total_length;
-	assert(this->occupied >= 0);
 }
 
 /**
