b4d4d730a5ec713b6615433c989d693be56f83a9
Rubidium <rubidium@openttd.org>
2025-04-26 14:25:58 +0200

    Codechange: use std::string_view for town name generation

--- src/townname.cpp
+++ src/townname.cpp
@@ -702,11 +702,11 @@ static void MakeCzechTownName(StringBuilder &builder, uint32_t seed)
 	if (dynamic_subst) {
 		builder += _name_czech_subst_stem[stem].name;
 		if (postfix < std::size(_name_czech_subst_postfix)) {
-			const char *poststr = _name_czech_subst_postfix[postfix];
-			const char *endstr = _name_czech_subst_ending[ending].name;
+			std::string_view poststr = _name_czech_subst_postfix[postfix];
+			std::string_view endstr = _name_czech_subst_ending[ending].name;
 
-			size_t postlen = strlen(poststr);
-			size_t endlen = strlen(endstr);
+			size_t postlen = poststr.size();
+			size_t endlen = endstr.size();
 			assert(postlen > 0 && endlen > 0);
 
 			/* Kill the "avava" and "Jananna"-like cases */
@@ -884,7 +884,7 @@ static void MakeItalianTownName(StringBuilder &builder, uint32_t seed)
 		return;
 	}
 
-	static const char * const mascul_femin_italian[] = {
+	static std::string_view const mascul_femin_italian[] = {
 		"o",
 		"a",
 	};
