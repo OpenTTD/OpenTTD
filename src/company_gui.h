156f98ba066d576b6b14cf125eca44f6757e1e8b
Peter Nelson <peter1138@openttd.org>
2025-05-13 01:23:27 +0100

    Fix #14256, dfd9fbf873: Company finance windows not updated when paused. (#14258)
    Refresh company finance windows via a WindowTimer instead of in game loop.
    
    As the invalidation affects multiple windows this is a global timer instead of window-specific.

--- src/company_gui.h
+++ src/company_gui.h
@@ -23,7 +23,6 @@ void ShowCompanyFinances(CompanyID company);
 void ShowCompany(CompanyID company);
 
 void InvalidateCompanyWindows(const Company *c);
-void InvalidateCompanyWindows();
 void CloseCompanyWindows(CompanyID company);
 void DirtyCompanyInfrastructureWindows(CompanyID company);
 
