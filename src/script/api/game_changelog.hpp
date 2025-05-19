ad3a34e9ef139e4589bef902bf153c7e518c5ea5
SamuXarick <43006711+SamuXarick@users.noreply.github.com>
2025-05-19 13:48:17 +0100

    Add: [Script] ScriptVehicleList_Waypoint (#13456)
    Creates a list of vehicles that have orders to a given waypoint.

--- src/script/api/game_changelog.hpp
+++ src/script/api/game_changelog.hpp
@@ -27,6 +27,7 @@
  * \li GSCargo::CC_NON_POURABLE
  * \li GSCargo::CC_POTABLE
  * \li GSCargo::CC_NON_POTABLE
+ * \li GSVehicleList_Waypoint
  *
  * Other changes:
  * \li GSBridge::GetBridgeID renamed to GSBridge::GetBridgeType
