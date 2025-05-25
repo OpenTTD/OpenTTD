d1ad6f4b999978870b641841a7475e0f3f60a1e3
Peter Nelson <peter1138@openttd.org>
2025-05-25 09:51:49 +0100

    Add: Badge dropdown filters to picker window.

--- src/picker_gui.h
+++ src/picker_gui.h
@@ -146,6 +146,7 @@ public:
 struct PickerFilterData : StringFilter {
 	const PickerCallbacks *callbacks; ///< Callbacks for filter functions to access to callbacks.
 	std::optional<BadgeTextFilter> btf;
+	std::optional<BadgeDropdownFilter> bdf;
 };
 
 using PickerClassList = GUIList<int, std::nullptr_t, PickerFilterData &>; ///< GUIList holding classes to display.
@@ -183,6 +184,7 @@ public:
 	void OnInit() override;
 	void Close(int data = 0) override;
 	void UpdateWidgetSize(WidgetID widget, Dimension &size, const Dimension &padding, Dimension &fill, Dimension &resize) override;
+	std::string GetWidgetString(WidgetID widget, StringID stringid) const override;
 	void DrawWidget(const Rect &r, WidgetID widget) const override;
 	void OnDropdownSelect(WidgetID widget, int index, int click_result) override;
 	void OnResize() override;
@@ -222,6 +224,8 @@ private:
 	void EnsureSelectedTypeIsVisible();
 
 	GUIBadgeClasses badge_classes;
+	std::pair<WidgetID, WidgetID> badge_filters{};
+	BadgeFilterChoices badge_filter_choices{};
 
 	const IntervalTimer<TimerGameCalendar> yearly_interval = {{TimerGameCalendar::YEAR, TimerGameCalendar::Priority::NONE}, [this](auto) {
 		this->SetDirty();
