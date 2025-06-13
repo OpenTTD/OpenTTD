8cfb741e1b6feab78ab15f01b2fcbbe84cfcac2b
Michael Lutz <michi@icosahedron.de>
2025-06-13 23:00:51 +0200

    Add: Show all railtypes in the build vehicle and engine preview dialogs.

--- src/engine_gui.cpp
+++ src/engine_gui.cpp
@@ -182,6 +182,18 @@ static std::string GetTrainEngineInfoString(const Engine &e)
 	res << GetString(STR_ENGINE_PREVIEW_COST_WEIGHT, e.GetCost(), e.GetDisplayWeight());
 	res << '\n';
 
+	if (e.u.rail.railtypes.Count() > 1) {
+		std::string railtypes{};
+		std::string_view list_separator = GetListSeparator();
+
+		for (RailType rt : e.u.rail.railtypes) {
+			if (!railtypes.empty()) railtypes += list_separator;
+			AppendStringInPlace(railtypes, GetRailTypeInfo(rt)->strings.name);
+		}
+		res << GetString(STR_ENGINE_PREVIEW_RAILTYPES, railtypes);
+		res << '\n';
+	}
+
 	bool is_maglev = true;
 	for (RailType rt : e.u.rail.railtypes) {
 		is_maglev &= GetRailTypeInfo(rt)->acceleration_type == 2;
