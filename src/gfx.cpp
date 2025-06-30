d4399f0095f0dd2796430d14434b843ba47cb057
Peter Nelson <peter1138@openttd.org>
2025-06-30 17:08:10 +0100

    Fix: Don't draw truncation ellipsis if it's too wide.

--- src/gfx.cpp
+++ src/gfx.cpp
@@ -540,6 +540,9 @@ static int DrawLayoutLine(const ParagraphLayouter::Line &line, int y, int left, 
 		dot_width = fc->GetGlyphWidth(dot_glyph);
 		dot_sprite = fc->GetGlyph(dot_glyph);
 
+		/* Is there enough space even for an ellipsis? */
+		if (max_w < dot_width * 3) return (_current_text_dir == TD_RTL) ? left : right;
+
 		if (_current_text_dir == TD_RTL) {
 			min_x += 3 * dot_width;
 			offset_x = w - max_w;
