d09dfd843c7ae7f923ce14caff25f15c9d2eca70
Peter Nelson <peter1138@openttd.org>
2025-07-18 22:28:09 +0100

    Codechange: Extend industry cargo history to 24 years.
    Monthly data is stored for the current 24 months.
    Quarterly data is stored for a further 2-6 years.
    Yearly data is stored for a further 6-24 years.

--- src/misc/history_type.hpp
+++ src/misc/history_type.hpp
@@ -10,7 +10,37 @@
 #ifndef HISTORY_TYPE_HPP
 #define HISTORY_TYPE_HPP
 
-static constexpr uint8_t HISTORY_RECORDS = 25;
+struct HistoryRange {
+	const HistoryRange *hr;
+	const uint8_t periods; ///< Number of periods for this range.
+	const uint8_t records; ///< Number of records needed for this range.
+	const uint8_t first; ///< Index of first element in history data.
+	const uint8_t last; ///< Index of last element in history data.
+	const uint8_t division; ///< Number of divisions of the previous history range.
+	const uint8_t total_division; ///< Number of divisions of the initial history range.
+
+	explicit constexpr HistoryRange(uint8_t periods) :
+		hr(nullptr), periods(periods), records(this->periods), first(1), last(this->first + this->records), division(1), total_division(1)
+	{
+	}
+
+	constexpr HistoryRange(const HistoryRange &hr, uint8_t division, uint8_t periods) :
+		hr(&hr), periods(periods), records(this->periods - ((hr.periods / division) - 1)), first(hr.last), last(this->first + this->records),
+		division(division), total_division(division * hr.total_division)
+	{
+	}
+};
+
+static constexpr uint8_t HISTORY_PERIODS = 24;
+static constexpr HistoryRange HISTORY_MONTH{HISTORY_PERIODS};
+static constexpr HistoryRange HISTORY_QUARTER{HISTORY_MONTH, 3, HISTORY_PERIODS};
+static constexpr HistoryRange HISTORY_YEAR{HISTORY_QUARTER, 4, HISTORY_PERIODS};
+
+/** Maximum number of divisions from previous history range. */
+static constexpr uint8_t HISTORY_MAX_DIVISION = std::max({HISTORY_MONTH.division, HISTORY_QUARTER.division, HISTORY_YEAR.division});
+
+/** Total number of records require for all history data. */
+static constexpr uint8_t HISTORY_RECORDS = HISTORY_YEAR.last;
 
 static constexpr uint8_t THIS_MONTH = 0;
 static constexpr uint8_t LAST_MONTH = 1;
