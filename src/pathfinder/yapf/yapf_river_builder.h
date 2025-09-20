bff850128c2b9ad737a212d1091fa092bcb1e79d
Kuhnovic <68320206+Kuhnovic@users.noreply.github.com>
2025-09-20 21:42:06 +0200

    Codechange: Use YAPF for river builder pathfinder. (#14606)

---
+++ src/pathfinder/yapf/yapf_river_builder.h
@@ -0,0 +1,17 @@
+/*
+ * This file is part of OpenTTD.
+ * OpenTTD is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
+ * OpenTTD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OpenTTD. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file yapf_river_builder.h Pathfinder for river building. */
+
+#ifndef YAPF_RIVER_BUILDER_H
+#define YAPF_RIVER_BUILDER_H
+
+#include "../../tile_type.h"
+
+void YapfBuildRiver(TileIndex start_tile, TileIndex end_tile, TileIndex spring_tile, bool main_river);
+
+#endif /* YAPF_RIVER_BUILDER_H */
