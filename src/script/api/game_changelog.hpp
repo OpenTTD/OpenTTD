bd0e27ea5b72b59d20254535d4ec99fa5a971717
SamuXarick <43006711+SamuXarick@users.noreply.github.com>
2025-10-30 15:28:49 +0000

    Doc 313c6c4: [Script] GetAllRailTypes and GetRailType are from ScriptEngine

--- src/script/api/game_changelog.hpp
+++ src/script/api/game_changelog.hpp
@@ -30,7 +30,7 @@
  * \li GSVehicleList_Waypoint
  * \li GSBaseStation::GetOwner
  * \li GSError::ERR_BRIDGE_TOO_LOW
- * \li GSRail::GetAllRailTypes
+ * \li GSEngine::GetAllRailTypes
  *
  * Other changes:
  * \li GSBridge::GetBridgeID renamed to GSBridge::GetBridgeType
@@ -38,7 +38,7 @@
  * \li GSList instances can now be saved
  * \li GSVehicleList_Station accepts an optional GSVehicle::VehicleType parameter
  * \li GSList instances can now be cloned
- * \li GSRail::GetRailType will only return the first RailType of an engine, use GSRail::GetAllRailTypes instead
+ * \li GSEngine::GetRailType will only return the first RailType of an engine, use GSEngine::GetAllRailTypes instead
  *
  * \b 14.0
  *
