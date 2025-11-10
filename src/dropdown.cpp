45311c178ba4a0b0e9c2f3a77a0841ee9c3dd822
Rito12 <kapitan.cypek@gmail.com>
2025-11-10 16:25:56 +0100

    Codechange: Create dropdown list item that allows to change bg colours.

--- src/dropdown.cpp
+++ src/dropdown.cpp
@@ -21,15 +21,11 @@
 #include "widgets/dropdown_widget.h"
 
 #include "table/strings.h"
+#include "table/sprites.h"
 
 #include "dropdown_common_type.h"
 
 #include "safeguards.h"
-
-std::unique_ptr<DropDownListItem> MakeDropDownListDividerItem()
-{
-	return std::make_unique<DropDownListDividerItem>(-1);
-}
 
 std::unique_ptr<DropDownListItem> MakeDropDownListStringItem(StringID str, int value, bool masked, bool shaded)
 {
@@ -85,6 +81,8 @@ struct DropdownWindow : Window {
 	int scrolling = 0; ///< If non-zero, auto-scroll the item list (one time).
 	Point position{}; ///< Position of the topleft corner of the window.
 	Scrollbar *vscroll = nullptr;
+	bool last_shift_state; ///< Whether the shift button was pressed during last frame.
+	bool last_ctrl_state; ///< Whether the ctrl button was pressed during last frame.
 
 	Dimension items_dim{}; ///< Calculated cropped and padded dimension for the items widget.
 
@@ -105,6 +103,8 @@ struct DropdownWindow : Window {
 			, list(std::move(list))
 			, selected_result(selected)
 			, options(options)
+			, last_shift_state(_shift_pressed)
+			, last_ctrl_state(_ctrl_pressed)
 	{
 		assert(!this->list.empty());
 
@@ -274,7 +274,7 @@ struct DropdownWindow : Window {
 				Rect full = ir.WithY(y, y + item_height - 1);
 
 				bool selected = (this->selected_result == item->result) && item->Selectable();
-				if (selected) GfxFillRect(full, PC_BLACK);
+				if (selected) GfxFillRect(full, item->GetSelectedBGColour(colour));
 
 				item->Draw(full, full.Shrink(WidgetDimensions::scaled.dropdowntext, RectPadding::zero), selected, selected ? this->selected_click_result : -1, colour);
 			}
@@ -305,6 +305,13 @@ struct DropdownWindow : Window {
 
 	void OnMouseLoop() override
 	{
+		if (this->last_ctrl_state != _ctrl_pressed || this->last_shift_state != _shift_pressed) {
+			/* Dropdown might contain an item with specified custom bg colours, allow it to update. */
+			this->SetDirty();
+			this->last_ctrl_state = _ctrl_pressed;
+			this->last_shift_state = _shift_pressed;
+		}
+
 		if (this->click_delay != 0 && --this->click_delay == 0) {
 			/* Close the dropdown, so it doesn't affect new window placement.
 			 * Also mark it dirty in case the callback deals with the screen. (e.g. screenshots). */
