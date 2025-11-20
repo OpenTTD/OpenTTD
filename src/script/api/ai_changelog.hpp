75aee10e1e639c1cf7d94613feef7e381f015f56
TheDude-gh <thedude@novapolis.net>
2025-11-20 12:54:33 +0100

    Add: [Script] ScriptTile::IsHouseTile

--- src/script/api/ai_changelog.hpp
+++ src/script/api/ai_changelog.hpp
@@ -30,6 +30,7 @@
  * \li AIVehicleList_Waypoint
  * \li AIError::ERR_BRIDGE_TOO_LOW
  * \li AIEngine::GetAllRailTypes
+ * \li AITile::IsHouseTile
  *
  * Other changes:
  * \li AIBridge::GetBridgeID renamed to AIBridge::GetBridgeType
--- src/script/api/game_changelog.hpp
+++ src/script/api/game_changelog.hpp
@@ -31,6 +31,7 @@
  * \li GSBaseStation::GetOwner
  * \li GSError::ERR_BRIDGE_TOO_LOW
  * \li GSEngine::GetAllRailTypes
+ * \li GSTile::IsHouseTile
  *
  * Other changes:
  * \li GSBridge::GetBridgeID renamed to GSBridge::GetBridgeType
