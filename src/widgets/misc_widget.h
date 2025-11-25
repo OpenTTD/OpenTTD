c3f9cdf84f8e0075ad12afcad67e4038175deb22
mmtunligit <156685720+mmtunligit@users.noreply.github.com>
2025-11-25 12:05:24 +0100

    Codechange 7a0814c: Use SetDisplayedPlane instead of UpdateWidgetSize (#14823)

--- src/widgets/misc_widget.h
+++ src/widgets/misc_widget.h
@@ -33,6 +33,7 @@ enum QueryStringWidgets : WidgetID {
 	WID_QS_CAPTION, ///< Caption of the window.
 	WID_QS_TEXT,    ///< Text of the query.
 	WID_QS_DEFAULT, ///< Default button.
+	WID_QS_DEFAULT_SEL, ///< Container for default button, which can be hidden.
 	WID_QS_CANCEL,  ///< Cancel button.
 	WID_QS_OK,      ///< OK button.
 	WID_QS_MOVE,    ///< Move button.
