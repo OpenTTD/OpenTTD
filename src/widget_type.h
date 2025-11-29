881e12d7fa6e5b5d96089b4cc1147bc5f53a80bd
Peter Nelson <peter1138@openttd.org>
2025-11-29 11:59:23 +0000

    Codechange: Allow vertical container direction to be flipped.
    Allows widgets to be laid out from bottom to top instead of top to bottom.

--- src/widget_type.h
+++ src/widget_type.h
@@ -592,6 +592,8 @@ public:
 
 	void SetupSmallestSize(Window *w) override;
 	void AssignSizePosition(SizingType sizing, int x, int y, uint given_width, uint given_height, bool rtl) override;
+
+	bool bottom_up = false;
 };
 
 /**
