99f04f27ded8e26feda084a0c73a73022e376747
Cyprian Klimaszewski <111280526+Rito13@users.noreply.github.com>
2025-12-07 12:25:08 +0100

    Doc: Update information for receiving a copy of GPL. (#14869)

--- src/widgets/timetable_widget.h
+++ src/widgets/timetable_widget.h
@@ -2,7 +2,7 @@
  * This file is part of OpenTTD.
  * OpenTTD is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
  * OpenTTD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OpenTTD. If not, see <http://www.gnu.org/licenses/>.
+ * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OpenTTD. If not, see <https://www.gnu.org/licenses/old-licenses/gpl-2.0>.
  */
 
 /** @file timetable_widget.h Types related to the timetable widgets. */
