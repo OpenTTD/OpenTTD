88a52b60f86efb90da3d41f5ee95c642ed2fd0e2
Peter Nelson <peter1138@openttd.org>
2025-12-15 16:42:50 +0000

    Fix #14917: Crash when opening house picker with no houses available. (#14920)

--- src/picker_gui.cpp
+++ src/picker_gui.cpp
@@ -357,9 +357,11 @@ void PickerWindow::DrawWidget(const Rect &r, WidgetID widget) const
 			break;
 		}
 
-		case WID_PW_TYPE_NAME:
-			DrawString(r, this->callbacks.GetTypeName(this->callbacks.GetSelectedClass(), this->callbacks.GetSelectedType()), TC_GOLD, SA_CENTER);
+		case WID_PW_TYPE_NAME: {
+			StringID str = this->callbacks.GetTypeName(this->callbacks.GetSelectedClass(), this->callbacks.GetSelectedType());
+			if (str != INVALID_STRING_ID) DrawString(r, str, TC_GOLD, SA_CENTER);
 			break;
+		}
 	}
 }
 
