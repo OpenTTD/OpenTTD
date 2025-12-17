5d702a96d6f17b74aa0f765ab368b386adf1c277
Loïc Guilloux <glx22@users.noreply.github.com>
2025-12-17 19:19:29 +0100

    Codefix 2b24444: Don't run regression tests in parallel (#14935)

--- cmake/CreateRegression.cmake
+++ cmake/CreateRegression.cmake
@@ -54,5 +54,7 @@ macro(create_regression)
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
 
+    set_property(TEST regression_${REGRESSION_TEST_NAME} PROPERTY RUN_SERIAL TRUE)
+
     add_dependencies(regression regression_${REGRESSION_TEST_NAME})
 endmacro()
