d41bc87f5d2ccadb729b83afc4d432d229bad192
ahhhj <86386745+ahhhj@users.noreply.github.com>
2025-12-21 14:32:42 -0800

    Update src/tree_cmd.cpp
    expand from ternary and correct behavior on TROPICZONE_NORMAL
    
    Co-authored-by: Tyler Trahan <tyler@tylertrahan.com>

--- src/tree_cmd.cpp
+++ src/tree_cmd.cpp
@@ -811,8 +811,20 @@ static void TileLoopTreesAlps(TileIndex tile)
  */
 static bool CanPlantExtraTrees(TileIndex tile)
 {
-	return ((_settings_game.game_creation.landscape == LandscapeType::Tropic) ?
-		(GetTropicZone(tile) == TROPICZONE_RAINFOREST && (_settings_game.construction.extra_tree_placement == ETP_SPREAD_ALL || _settings_game.construction.extra_tree_placement == ETP_SPREAD_RAINFOREST)) :
+/* Desert and rainforest trees need special handling. */
+if (_settings_game.game_creation.landscape == LandscapeType::Tropic) {
+	switch (GetTropicZone(tile)) {
+		case TROPICZONE_DESERT:
+			/* Cacti never spread. */
+			return false;
+		case TROPICZONE_RAINFOREST:
+			return (_settings_game.construction.extra_tree_placement == ETP_SPREAD_ALL || _settings_game.construction.extra_tree_placement == ETP_SPREAD_RAINFOREST);
+		default:
+			return _settings_game.construction.extra_tree_placement == ETP_SPREAD_ALL;
+	}
+}
+
+return (_settings_game.construction.extra_tree_placement == ETP_SPREAD_ALL);
 		_settings_game.construction.extra_tree_placement == ETP_SPREAD_ALL);
 }
 
