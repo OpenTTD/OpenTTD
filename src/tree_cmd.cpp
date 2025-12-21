4ce1ecac0a745fdcc044987e350d0c5790e9c388
ahhhj <86386745+ahhhj@users.noreply.github.com>
2025-12-21 14:46:23 -0800

    Remove extra line and indent

--- src/tree_cmd.cpp
+++ src/tree_cmd.cpp
@@ -810,21 +810,21 @@ static void TileLoopTreesAlps(TileIndex tile)
  */
 static bool CanPlantExtraTrees(TileIndex tile)
 {
-/* Desert and rainforest trees need special handling. */
-if (_settings_game.game_creation.landscape == LandscapeType::Tropic) {
-	switch (GetTropicZone(tile)) {
-		case TROPICZONE_DESERT:
-			/* Cacti never spread. */
-			return false;
-		case TROPICZONE_RAINFOREST:
-			return (_settings_game.construction.extra_tree_placement == ETP_SPREAD_ALL || _settings_game.construction.extra_tree_placement == ETP_SPREAD_RAINFOREST);
-		default:
-			return _settings_game.construction.extra_tree_placement == ETP_SPREAD_ALL;
+	/* Desert and rainforest trees need special handling. */
+	if (_settings_game.game_creation.landscape == LandscapeType::Tropic) {
+		switch (GetTropicZone(tile)) {
+			case TROPICZONE_DESERT:
+				/* Cacti never spread. */
+				return false;
+			case TROPICZONE_RAINFOREST:
+				return (_settings_game.construction.extra_tree_placement == ETP_SPREAD_ALL || _settings_game.construction.extra_tree_placement == ETP_SPREAD_RAINFOREST);
+			default:
+				return _settings_game.construction.extra_tree_placement == ETP_SPREAD_ALL;
+		}
 	}
-}
 
 return (_settings_game.construction.extra_tree_placement == ETP_SPREAD_ALL);
-		_settings_game.construction.extra_tree_placement == ETP_SPREAD_ALL);
+	
 }
 
 static void TileLoop_Trees(TileIndex tile)
