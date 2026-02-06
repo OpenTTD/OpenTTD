cb27f9471038426d990de2b901c44fb66230adb7
ronvid <leon888.lb@gmail.com>
2026-02-06 15:18:28 +0100

    Fix 4cb637c: Eliminating unwanted cases for Czech townnames

--- src/townname.cpp
+++ src/townname.cpp
@@ -697,14 +697,24 @@ static void MakeCzechTownName(StringBuilder &builder, uint32_t seed)
 
 	if (dynamic_subst) {
 		builder += _name_czech_subst_stem[stem].name;
+		std::string_view endstr = _name_czech_subst_ending[ending].name;
 		if (postfix < std::size(_name_czech_subst_postfix)) {
 			std::string_view poststr = _name_czech_subst_postfix[postfix];
-			std::string_view endstr = _name_czech_subst_ending[ending].name;
 
 			/* Kill the "avava" and "Jananna"-like cases */
 			if ((poststr[1] != 'v' || poststr[1] != endstr[1]) &&
 					poststr[1] != endstr[0]) {
 				builder += poststr;
+			}
+		}
+
+		/* k-i -> c-i, h-i -> z-i */
+		if (endstr[0] == 'i') {
+			std::string &str = builder.GetString();
+			switch (str.back()) {
+				case 'k': str.back() = 'c'; break;
+				case 'h': str.back() = 'z'; break;
+				default: break;
 			}
 		}
 		builder += _name_czech_subst_ending[ending].name;
