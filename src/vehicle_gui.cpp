d685fc97f16cb72105043dd7e3a8b9c882a9fa74
Peter Nelson <peter1138@openttd.org>
2026-03-08 15:33:30 +0000

    Change: Enable dropdown text filter for various dropdown lists.

--- src/vehicle_gui.cpp
+++ src/vehicle_gui.cpp
@@ -2138,7 +2138,7 @@ public:
 				return;
 
 			case WID_VL_FILTER_BY_CARGO: // Cargo filter dropdown
-				ShowDropDownList(this, this->BuildCargoDropDownList(false), this->cargo_filter_criteria, widget);
+				ShowDropDownList(this, this->BuildCargoDropDownList(false), this->cargo_filter_criteria, widget, 0, DropDownOption::Filterable);
 				break;
 
 			case WID_VL_LIST: { // Matrix to show vehicles
