c18b3a8a0480eabb9ba9224ec4ea7f8daf4650fd
livingBEEF <sruz25cz@gmail.com>
2026-07-03 14:21:23 +0200

    Fix traffic side inconsistencies (#15661)
    * Update: change wording for driving side setting
    
    * Fix: [NewGRF] Make 'signals on traffic side' consistent with explicit settings
    
    Previously this TTDPatchFlag returned 'true' when the 'train signal side'
    setting was 'road vehicle driving side'. Now it will also return 'true' when
    both the settings are the same, i.e. both are 'left' or both are 'right'.

--- src/newgrf/newgrf_act7_9.cpp
+++ src/newgrf/newgrf_act7_9.cpp
@@ -24,6 +24,21 @@
 /** 32 * 8 = 256 flags. Apparently TTDPatch uses this many.. */
 static std::array<uint32_t, 8> _ttdpatch_flags;
 
+/**
+ * Checks whether the train signals are on the same side as the road vehicles are driving.
+ * This is a flag that can be checked by NewGRFs (TTDPatchFlags bit 0x3B).
+ * @return \c true iff the road vehicles drive on the same side as signals are drawn.
+ */
+static bool IsSignalSideOnTrafficSide()
+{
+	switch (_settings_game.construction.train_signal_side) {
+		case TrainSignalSide::RoadVehicleDrivingSide: return true;
+		case TrainSignalSide::Left: return _settings_game.vehicle.road_side == RoadVehicleDrivingSide::Left;
+		case TrainSignalSide::Right: return _settings_game.vehicle.road_side == RoadVehicleDrivingSide::Right;
+		default: NOT_REACHED();
+	}
+}
+
 /** Initialize the TTDPatch flags */
 void InitializePatchFlags()
 {
@@ -52,7 +67,7 @@ void InitializePatchFlags()
 	                   |                                                       (1U << 0x18)  // newrvs
 	                   |                                                       (1U << 0x19)  // newships
 	                   |                                                       (1U << 0x1A)  // newplanes
-	                   | ((_settings_game.construction.train_signal_side == TrainSignalSide::RoadVehicleDrivingSide ? 1U : 0U) << 0x1B)  // signalsontrafficside
+	                   |                  ((IsSignalSideOnTrafficSide() ? 1U : 0U) << 0x1B)  // signalsontrafficside
 	                   |       ((_settings_game.vehicle.disable_elrails ? 0U : 1U) << 0x1C); // electrifiedrailway
 
 	_ttdpatch_flags[2] =                                                       (1U << 0x01)  // loadallgraphics - obsolete
