8c0804b5c68c090e69937fc38be83e85e6c79560
Peter Nelson <peter1138@openttd.org>
2026-08-17 23:36:20 +0100

    Codechange: [NewGRF] Remove unneeded check for valid mapped entity IDs

--- src/newgrf_commons.cpp
+++ src/newgrf_commons.cpp
@@ -115,7 +115,7 @@ uint16_t OverrideManagerBase::AddEntityID(uint16_t grf_local_id, GrfID grfid, ui
 	for (id = this->max_offset; id < this->max_entities; id++) {
 		EntityIDMapping *map = &this->mappings[id];
 
-		if (CheckValidNewID(id) && map->entity_id == 0 && map->grfid.Empty()) {
+		if (map->entity_id == 0 && map->grfid.Empty()) {
 			map->entity_id     = grf_local_id;
 			map->grfid         = grfid;
 			map->substitute_id = substitute_id;
