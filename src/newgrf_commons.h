9d19eb62fd9b553167bf570bace21ad875962804
Peter Nelson <peter1138@openttd.org>
2026-08-17 23:35:10 +0100

    Change: [NewGRF] Remove airport tile ID hole at 0xFF
    Airport tiles have no special values that need to be avoided.

--- src/newgrf_commons.h
+++ src/newgrf_commons.h
@@ -278,8 +278,6 @@ public:
 
 struct AirportTileSpec;
 class AirportTileOverrideManager : public OverrideManagerBase {
-protected:
-	bool CheckValidNewID(uint16_t testid) override { return testid != 0xFF; }
 public:
 	AirportTileOverrideManager(uint16_t offset, uint16_t maximum, uint16_t invalid) :
 			OverrideManagerBase(offset, maximum, invalid) {}
