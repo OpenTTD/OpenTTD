d8509d9871866991bf3827f63a95e264db37bddd
Peter Nelson <peter1138@openttd.org>
2026-08-31 14:08:41 +0100

    Codechange: Avoid redundant UpdateNearestTownForRoadTiles calls for JSON town import
    Avoids two full map scans for every town placed, which is now done once per import.
    
    Based on changes in JGRPP.

--- src/town_cmd.cpp
+++ src/town_cmd.cpp
@@ -2140,7 +2140,7 @@ static CommandCost TownCanBePlacedHere(TileIndex tile, bool check_surrounding)
  * @param name The name to check.
  * @return true if the name is unique
  */
-static bool IsUniqueTownName(const std::string &name)
+static bool IsUniqueTownName(std::string_view name)
 {
 	for (const Town *t : Town::Iterate()) {
 		if (!t->name.empty() && t->name == name) return false;
@@ -2471,6 +2471,36 @@ bool GenerateTowns(TownLayout layout, std::optional<uint> number)
 	return false;  // we are still without a town? we failed, simply
 }
 
+/**
+ * Try to generate a named town around a tile.
+ * @param target_tile Target tile.
+ * @param size The preset size of the town.
+ * @param city Should we create a city?
+ * @param layout The road layout of the town.
+ * @param name Town name.
+ * @return town or nullptr.
+ */
+Town *TryGenerateNamedTownAroundTile(TileIndex target_tile, TownSize size, bool city, TownLayout layout, std::string_view name)
+{
+	if (!Town::CanAllocateItem() || name.empty() || Utf8StringLength(name) >= MAX_LENGTH_TOWN_NAME_CHARS || !IsUniqueTownName(name)) {
+		return nullptr;
+	}
+
+	/* Try founding on the target tile, and if that doesn't work, find the nearest suitable tile up to 16 tiles away.
+	 * The target might be on water, blocked somehow, or on a steep slope that can't be terraformed by the founding command. */
+	for (TileIndex tile : SpiralTileSequence(target_tile, 16, 0, 0)) {
+		CommandCost ret = TownCanBePlacedHere(tile, false);
+		if (ret.Failed()) continue;
+
+		Town *t = Town::Create(tile);
+		t->name = name;
+		DoCreateTown(t, tile, 0, size, city, layout, true);
+
+		return t;
+	}
+
+	return nullptr;
+}
 
 /**
  * Returns the bit corresponding to the town zone of the specified tile.
